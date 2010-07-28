b75e523ab190d89596632787c605221905b47ae9
daniel@transgaming.com <daniel@transgaming.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2010-07-28 19:13:47 +0000

    Use TestCooperativeLevel instead of ValidateDevice to check for device loss.
    TRAC #12547
    
    ValidateDevice checks for correct rendering state as well, which we don't need.
    
    Signed-off-by: Nicolas Capens
    Signed-off-by: Daniel Koch
    
    Author:    Andrew Lewycky
    
    git-svn-id: https://angleproject.googlecode.com/svn/trunk@354 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/libEGL/libEGL.cpp
+++ src/libEGL/libEGL.cpp
@@ -824,9 +824,8 @@ EGLBoolean __stdcall eglMakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface 
         egl::Display *display = static_cast<egl::Display*>(dpy);
         gl::Context *context = static_cast<gl::Context*>(ctx);
         IDirect3DDevice9 *device = display->getDevice();
-        DWORD passes;
 
-        if (!device || device->ValidateDevice(&passes) == D3DERR_DEVICELOST)
+        if (!device || device->TestCooperativeLevel() != D3D_OK)
         {
             return error(EGL_CONTEXT_LOST, EGL_FALSE);
         }
