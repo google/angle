00d02a28d6f0b832908f1d8165bac975d8c676e8
daniel@transgaming.com <daniel@transgaming.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2011-08-02 15:29:07 +0000

    update version number
    git-svn-id: https://angleproject.googlecode.com/svn/trunk@714 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/common/version.h
+++ src/common/version.h
@@ -1,7 +1,7 @@
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 0
 #define BUILD_VERSION 0
-#define BUILD_REVISION 712
+#define BUILD_REVISION 714
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
