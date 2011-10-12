eef0e27c12ecb29c5583dfe41b41682e483b4a74
daniel@transgaming.com <daniel@transgaming.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2011-10-12 16:19:36 +0000

    Fix link errors caused by static use of perf related functions even when ANGLE_DISABLE_PERF is defined.
    Bug=225
    Author: Mark Callow
    
    git-svn-id: https://angleproject.googlecode.com/svn/trunk@788 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/common/version.h
+++ src/common/version.h
@@ -1,7 +1,7 @@
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 0
 #define BUILD_VERSION 0
-#define BUILD_REVISION 787
+#define BUILD_REVISION 788
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
