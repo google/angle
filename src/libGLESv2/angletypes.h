c8b8f2832435209bc3b8969eaf0eec2ccf3d1432
daniel@transgaming.com <daniel@transgaming.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2012-11-28 19:35:28 +0000

    Added structures for rasterizer, blend and depth stencil state.
    TRAC #22041
    
    Signed-off-by: Shannon Woods
    Signed-off-by: Daniel Koch
    
    Author:    Geoff Lang
    
    git-svn-id: https://angleproject.googlecode.com/svn/branches/dx11proto@1419 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/libGLESv2/angletypes.h
+++ src/libGLESv2/angletypes.h
@@ -9,6 +9,9 @@
 #ifndef LIBGLESV2_ANGLETYPES_H_
 #define LIBGLESV2_ANGLETYPES_H_
 
+#define GL_APICALL
+#include <GLES2/gl2.h>
+
 namespace gl
 {
 
@@ -34,6 +37,63 @@ struct Color
     float blue;
     float alpha;
 };
+
+struct RasterizerState
+{
+    bool cullFace;
+    GLenum cullMode;
+    GLenum frontFace;
+
+    bool polygonOffsetFill;
+    GLfloat polygonOffsetFactor;
+    GLfloat polygonOffsetUnits;
+
+    bool scissorTest;
+};
+
+struct BlendState
+{
+    bool blend;
+    GLenum sourceBlendRGB;
+    GLenum destBlendRGB;
+    GLenum sourceBlendAlpha;
+    GLenum destBlendAlpha;
+    GLenum blendEquationRGB;
+    GLenum blendEquationAlpha;
+
+    bool colorMaskRed;
+    bool colorMaskGreen;
+    bool colorMaskBlue;
+    bool colorMaskAlpha;
+
+    bool sampleAlphaToCoverage;
+
+    bool dither;
+};
+
+struct DepthStencilState
+{
+    bool depthTest;
+    GLenum depthFunc;
+    bool depthMask;
+
+    bool stencilTest;
+    GLenum stencilFunc;
+    GLint stencilRef;
+    GLuint stencilMask;
+    GLenum stencilFail;
+    GLenum stencilPassDepthFail;
+    GLenum stencilPassDepthPass;
+    GLuint stencilWritemask;
+    GLenum stencilBackFunc;
+    GLint stencilBackRef;
+    GLuint stencilBackMask;
+    GLenum stencilBackFail;
+    GLenum stencilBackPassDepthFail;
+    GLenum stencilBackPassDepthPass;
+    GLuint stencilBackWritemask;
+};
+
 }
 
 #endif // LIBGLESV2_ANGLETYPES_H_
