9aa6fe1ce670d78956eaf7c2715d79ff3f936218
daniel@transgaming.com <daniel@transgaming.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2012-12-20 21:13:39 +0000

    Ensured that the mUsesPointSprites flag is properly preserved when loading shaders from binary cache.
    BUG=https://code.google.com/p/angleproject/issues/detail?id=389
    Review URL: https://codereview.appspot.com/6884053
    
    git-svn-id: https://angleproject.googlecode.com/svn/branches/dx11proto@1636 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/libGLESv2/ProgramBinary.cpp
+++ src/libGLESv2/ProgramBinary.cpp
@@ -1564,6 +1564,7 @@ bool ProgramBinary::load(InfoLog &infoLog, const void *binary, GLsizei length)
 
     stream.read(&mUsedVertexSamplerRange);
     stream.read(&mUsedPixelSamplerRange);
+    stream.read(&mUsesPointSize);
 
     unsigned int size;
     stream.read(&size);
@@ -1689,6 +1690,7 @@ bool ProgramBinary::save(void* binary, GLsizei bufSize, GLsizei *length)
 
     stream.write(mUsedVertexSamplerRange);
     stream.write(mUsedPixelSamplerRange);
+    stream.write(mUsesPointSize);
 
     stream.write(mUniforms.size());
     for (unsigned int i = 0; i < mUniforms.size(); ++i)
