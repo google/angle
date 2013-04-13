8132dae98a89e025d1fd53319bbab1c25ef58f96
shannon.woods%transgaming.com@gtempaccount.com <shannon.woods%transgaming.com@gtempaccount.com@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2013-04-13 03:39:44 +0000

    Fixed a bug where BufferStorage11 was unable to handle NULL data being set.
    TRAC #22811
    
    Signed-off-by: Jamie Madill
    Signed-off-by: Shannon Woods
    Author: Geoff Lang
    
    git-svn-id: https://angleproject.googlecode.com/svn/branches/es3proto@2135 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/libGLESv2/renderer/BufferStorage11.cpp
+++ src/libGLESv2/renderer/BufferStorage11.cpp
@@ -160,12 +160,20 @@ void BufferStorage11::setData(const void* data, unsigned int size, unsigned int 
             bufferDesc.MiscFlags = 0;
             bufferDesc.StructureByteStride = 0;
 
-            D3D11_SUBRESOURCE_DATA initialData;
-            initialData.pSysMem = data;
-            initialData.SysMemPitch = size;
-            initialData.SysMemSlicePitch = 0;
+            if (data)
+            {
+                D3D11_SUBRESOURCE_DATA initialData;
+                initialData.pSysMem = data;
+                initialData.SysMemPitch = size;
+                initialData.SysMemSlicePitch = 0;
 
-            result = device->CreateBuffer(&bufferDesc, &initialData, &mStagingBuffer);
+                result = device->CreateBuffer(&bufferDesc, &initialData, &mStagingBuffer);
+            }
+            else
+            {
+                result = device->CreateBuffer(&bufferDesc, NULL, &mStagingBuffer);
+            }
+
             if (FAILED(result))
             {
                 return gl::error(GL_OUT_OF_MEMORY);
@@ -211,12 +219,20 @@ void BufferStorage11::setData(const void* data, unsigned int size, unsigned int 
                 mBufferSize = 0;
             }
 
-            D3D11_SUBRESOURCE_DATA initialData;
-            initialData.pSysMem = data;
-            initialData.SysMemPitch = size;
-            initialData.SysMemSlicePitch = 0;
+            if (data)
+            {
+                D3D11_SUBRESOURCE_DATA initialData;
+                initialData.pSysMem = data;
+                initialData.SysMemPitch = size;
+                initialData.SysMemSlicePitch = 0;
 
-            result = device->CreateBuffer(&bufferDesc, &initialData, &mBuffer);
+                result = device->CreateBuffer(&bufferDesc, &initialData, &mBuffer);
+            }
+            else
+            {
+                result = device->CreateBuffer(&bufferDesc, NULL, &mBuffer);
+            }
+
             if (FAILED(result))
             {
                 return gl::error(GL_OUT_OF_MEMORY);
