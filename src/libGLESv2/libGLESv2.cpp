ee148562c70f62b57868aaaddb42fd16c4f2b81d
shannonwoods@chromium.org <shannonwoods@chromium.org@736b8ea6-26fd-11df-bfd4-992fa37f6226>
2013-05-30 00:17:21 +0000

    Implement glBlitFramebuffer using glBlitFramebufferANGLE.
    TRAC #23055
    Signed-off-by: Jamie Madill
    Signed-off-by: Shannon Woods
    Author: Nicolas Capens
    
    git-svn-id: https://angleproject.googlecode.com/svn/branches/es3proto@2383 736b8ea6-26fd-11df-bfd4-992fa37f6226

--- src/libGLESv2/libGLESv2.cpp
+++ src/libGLESv2/libGLESv2.cpp
@@ -8693,7 +8693,7 @@ void __stdcall glBlitFramebuffer(GLint srcX0, GLint srcY0, GLint srcX1, GLint sr
                 return gl::error(GL_INVALID_OPERATION);
             }
 
-            UNIMPLEMENTED();
+            glBlitFramebufferANGLE(srcX0, srcY0, srcX1, srcY1, dstX0, dstY0, dstX1, dstY1, mask, filter);
         }
     }
     catch(std::bad_alloc&)
