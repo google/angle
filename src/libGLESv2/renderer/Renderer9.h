121945b174d1f63758e205851b33097dda73c58a
Jamie Madill <jmadill@chromium.org>
2013-11-18 15:10:07 -0500

    Proper handling of D3D9Ex device removed.
    BUG=313210
    
    Change-Id: I6b7e34466e89e7854c8797cbdcffc4a24c2c32f5

--- src/libGLESv2/renderer/Renderer9.h
+++ src/libGLESv2/renderer/Renderer9.h
@@ -198,6 +198,8 @@ class Renderer9 : public Renderer
   private:
     DISALLOW_COPY_AND_ASSIGN(Renderer9);
 
+    void deinitialize();
+
     void applyUniformnfv(gl::Uniform *targetUniform, const GLfloat *v);
     void applyUniformniv(gl::Uniform *targetUniform, const GLint *v);
     void applyUniformnbv(gl::Uniform *targetUniform, const GLint *v);
@@ -217,6 +219,9 @@ class Renderer9 : public Renderer
     void initializeDevice();
     D3DPRESENT_PARAMETERS getDefaultPresentParameters();
     void releaseDeviceResources();
+
+    HRESULT getDeviceStatusCode();
+    HRESULT resetRemovedDevice();
 
     UINT mAdapter;
     D3DDEVTYPE mDeviceType;
