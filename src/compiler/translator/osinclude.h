f831e3dd196e496edfb9b5380b2f0ab14eb18e31
Shannon Woods <shannonwoods@chromium.org>
2014-01-06 12:06:46 -0500

    Add NetBSD and DragonFly to posix-compliant OS list
    BUG=angle:428
    
    Change-Id: I282a6bf138b4e2a2c17baf428cbe1e636f43bdd6
    Reviewed-on: https://chromium-review.googlesource.com/181640
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Nicolas Capens <nicolascapens@chromium.org>
    Commit-Queue: Nicolas Capens <nicolascapens@chromium.org>
    Tested-by: Shannon Woods <shannonwoods@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/181670
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Tested-by: Jamie Madill <jmadill@chromium.org>

--- src/compiler/translator/osinclude.h
+++ src/compiler/translator/osinclude.h
@@ -16,6 +16,7 @@
 #define ANGLE_OS_WIN
 #elif defined(__APPLE__) || defined(__linux__) || \
       defined(__FreeBSD__) || defined(__OpenBSD__) || \
+      defined(__NetBSD__) || defined(__DragonFly__) || \
       defined(__sun) || defined(ANDROID) || \
       defined(__GLIBC__) || defined(__GNU__) || \
       defined(__QNX__)
