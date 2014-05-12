2e660d5627c8ef47daa2be82fa359baee13ab640
Vladimir Vukicevic <vladimirv@gmail.com>
2014-05-12 16:43:19 -0700

    Use lowercase d3d header includes to fix ANGLE compilation on case sensitive OSes
    The actual D3D header filenames are all lowercase.
    
    Change-Id: I8f78d618d44360d34b5b01eb5a1972b35f2f15b7
    Reviewed-on: https://chromium-review.googlesource.com/199692
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Shannon Woods <shannonwoods@chromium.org>
    Tested-by: Shannon Woods <shannonwoods@chromium.org>

--- src/libGLESv2/precompiled.h
+++ src/libGLESv2/precompiled.h
@@ -39,13 +39,13 @@
 
 #if defined(ANGLE_ENABLE_D3D9)
 #include <d3d9.h>
-#include <D3Dcompiler.h>
+#include <d3dcompiler.h>
 #endif // ANGLE_ENABLE_D3D9
 
 #if defined(ANGLE_ENABLE_D3D11)
-#include <D3D10_1.h>
-#include <D3D11.h>
+#include <d3d10_1.h>
+#include <d3d11.h>
 #include <dxgi.h>
 #include <dxgi1_2.h>
-#include <D3Dcompiler.h>
+#include <d3dcompiler.h>
 #endif // ANGLE_ENABLE_D3D11
