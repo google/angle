cd83a2f21084065eee991c89af6ec864a3572a4f
Geoff Lang <geofflang@chromium.org>
2015-01-23 12:50:20 -0500

    Disable the MipMapTests on D3D11 FL 9.3.
    Change-Id: Ibcb72f7b1aabcd98c4bebd91c3a57f5ba57eac3a
    Reviewed-on: https://chromium-review.googlesource.com/242850
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Tested-by: Geoff Lang <geofflang@chromium.org>

--- tests/angle_tests/MipmapTest.cpp
+++ tests/angle_tests/MipmapTest.cpp
@@ -1,7 +1,7 @@
 #include "ANGLETest.h"
 
 // Use this to select which configurations (e.g. which renderer, which GLES major version) these tests should be run against.
-ANGLE_TYPED_TEST_CASE(MipmapTest, ES2_D3D9, ES2_D3D11, ES2_D3D11_FL9_3);
+ANGLE_TYPED_TEST_CASE(MipmapTest, ES2_D3D9, ES2_D3D11);
 ANGLE_TYPED_TEST_CASE(MipmapTestES3, ES3_D3D11);
 
 template<typename T>
