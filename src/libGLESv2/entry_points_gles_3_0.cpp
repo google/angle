592ab9dd91e653f5c2d4f7395267046d229e20c1
Jamie Madill <jmadill@chromium.org>
2015-02-02 10:56:12 -0500

    Fix indentation in entry_points_gles_3_0.cpp.
    Change-Id: Iba3d8d51f5fb04958c5f917d56dbc01df70da041
    Reviewed-on: https://chromium-review.googlesource.com/245323
    Tested-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Gregoire Payen de La Garanderie <Gregory.Payen@imgtec.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libGLESv2/entry_points_gles_3_0.cpp
+++ src/libGLESv2/entry_points_gles_3_0.cpp
@@ -1096,7 +1096,7 @@ void GL_APIENTRY BindBufferRange(GLenum target, GLuint index, GLuint buffer, GLi
 
         switch (target)
         {
-        case GL_TRANSFORM_FEEDBACK_BUFFER:
+          case GL_TRANSFORM_FEEDBACK_BUFFER:
             {
                 // size and offset must be a multiple of 4
                 if (buffer != 0 && ((offset % 4) != 0 || (size % 4) != 0))
@@ -1177,7 +1177,7 @@ void GL_APIENTRY BindBufferBase(GLenum target, GLuint index, GLuint buffer)
 
         switch (target)
         {
-        case GL_TRANSFORM_FEEDBACK_BUFFER:
+          case GL_TRANSFORM_FEEDBACK_BUFFER:
             {
                 // Cannot bind a transform feedback buffer if the current transform feedback is active (3.0.4 pg 91 section 2.15.2)
                 TransformFeedback *curTransformFeedback = context->getState().getCurrentTransformFeedback();
