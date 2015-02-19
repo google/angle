7c1f6f190e904b977db91e581f2b2d12f9482e43
Jamie Madill <jmadill@chromium.org>
2015-02-19 18:56:09 +0000

    Revert "Implement missing variants of abs/sign"
    This for some reason is failing angle_unittests:
    
    http://build.chromium.org/p/chromium.gpu.fyi/builders/Win7%20Release%20%28NVIDIA%29/builds/13296
    
    Happens across platforms.
    
    This reverts commit 3e142e47cc5b40564bcdba4252fd326bc28ca343.
    
    Change-Id: Ideb20457546c0dec10c9584182cc8d5ead42309d
    Reviewed-on: https://chromium-review.googlesource.com/251160
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Tested-by: Jamie Madill <jmadill@chromium.org>

--- src/compiler/translator/IntermNode.cpp
+++ src/compiler/translator/IntermNode.cpp
@@ -338,8 +338,6 @@ bool TIntermUnary::promote(TInfoSink &)
         break;
 
       // operators for built-ins are already type checked against their prototype
-      case EOpAbs:
-      case EOpSign:
       case EOpAny:
       case EOpAll:
       case EOpVectorLogicalNot:
