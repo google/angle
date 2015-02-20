2fa660ba26f475d2b6762102739f8499a4c39b85
Jamie Madill <jmadill@chromium.org>
2015-02-20 13:01:34 -0500

    Add missing Display::getImplementation.
    Change-Id: I220ffefcbf9bc988d986e729961c9a893d6b5159
    Reviewed-on: https://chromium-review.googlesource.com/251580
    Tested-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/Display.h
+++ src/libANGLE/Display.h
@@ -84,6 +84,8 @@ class Display final
     const AttributeMap &getAttributeMap() const { return mAttributeMap; }
     EGLNativeDisplayType getNativeDisplayId() const { return mDisplayId; }
 
+    rx::DisplayImpl *getImplementation() { return mImplementation; }
+
   private:
     DISALLOW_COPY_AND_ASSIGN(Display);
 
