4904a79b40a167f66e23f095ce650c497d7a0a5b
Geoff Lang <geofflang@chromium.org>
2015-03-20 11:54:37 -0400

    Use the GL_COPY_READ_BUFFER binding for copy sources.
    Using the ELEMENT_ARRAY binding can cause issues such as rebinding the
    current VAO's index buffer unexpectedly.
    
    BUG=angleproject:881
    
    Change-Id: Ieeed52ac81c23a46bfe9fc4365239c2b177ff6ac
    Reviewed-on: https://chromium-review.googlesource.com/261563
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Tested-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/gl/BufferGL.cpp
+++ src/libANGLE/renderer/gl/BufferGL.cpp
@@ -16,7 +16,7 @@
 namespace rx
 {
 
-static const GLenum SourceBufferOperationTarget = GL_ELEMENT_ARRAY_BUFFER;
+static const GLenum SourceBufferOperationTarget = GL_COPY_READ_BUFFER;
 static const GLenum DestBufferOperationTarget = GL_ARRAY_BUFFER;
 
 BufferGL::BufferGL(const FunctionsGL *functions, StateManagerGL *stateManager)
@@ -61,7 +61,7 @@ gl::Error BufferGL::copySubData(BufferImpl* source, GLintptr sourceOffset, GLint
     mStateManager->bindBuffer(DestBufferOperationTarget, mBufferID);
     mStateManager->bindBuffer(SourceBufferOperationTarget, sourceGL->getBufferID());
 
-    mFunctions->copyBufferSubData(SourceBufferOperationTarget, GL_ARRAY_BUFFER, sourceOffset, destOffset, size);
+    mFunctions->copyBufferSubData(SourceBufferOperationTarget, DestBufferOperationTarget, sourceOffset, destOffset, size);
 
     return gl::Error(GL_NO_ERROR);
 }
