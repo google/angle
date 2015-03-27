5f3f372abf66230752c44089e173792dfa19afa9
Jamie Madill <jmadill@chromium.org>
2015-03-27 15:48:55 -0400

    Remove unused tests folder.
    This is now located in src/ and Chromium no longer refers to it.
    
    BUG=None
    
    Change-Id: I64fd302629ca28aa367d8714bfbac2657774fc3b
    Reviewed-on: https://chromium-review.googlesource.com/262920
    Tested-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- tests/angle_tests/ANGLETest.h
+++
@@ -1,9 +0,0 @@
-//
-// Copyright (c) 2012 The ANGLE Project Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//
-
-// TODO(jmadill): Remove this file once we're ready on the Chromium side.
-
-#include "../../src/tests/end2end_tests/ANGLETest.h"
