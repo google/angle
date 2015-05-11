4f3b8f6381ef49d53749b1b72654593b23459064
Olli Etuaho <oetuaho@nvidia.com>
2015-05-11 16:56:21 +0300

    Add missing EvqPointCoord to getQualifierString
    Also sort enums in getQualifierString in the same order as in the enum
    definition.
    
    BUG=angleproject:999
    TEST=WebGL conformance tests with --gl-shader-interm-output
    
    Change-Id: I31aa2e4b2405a090f560bf4140d9ee66271e526c
    Reviewed-on: https://chromium-review.googlesource.com/269941
    Tested-by: Olli Etuaho <oetuaho@nvidia.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/compiler/translator/BaseTypes.h
+++ src/compiler/translator/BaseTypes.h
@@ -386,7 +386,6 @@ inline const char* getQualifierString(TQualifier q)
     case EvqTemporary:      return "Temporary";      break;
     case EvqGlobal:         return "Global";         break;
     case EvqConst:          return "const";          break;
-    case EvqConstReadOnly:  return "const";          break;
     case EvqAttribute:      return "attribute";      break;
     case EvqVaryingIn:      return "varying";        break;
     case EvqVaryingOut:     return "varying";        break;
@@ -398,22 +397,24 @@ inline const char* getQualifierString(TQualifier q)
     case EvqIn:             return "in";             break;
     case EvqOut:            return "out";            break;
     case EvqInOut:          return "inout";          break;
+    case EvqConstReadOnly:  return "const";          break;
     case EvqInstanceID:     return "InstanceID";     break;
     case EvqPosition:       return "Position";       break;
     case EvqPointSize:      return "PointSize";      break;
     case EvqFragCoord:      return "FragCoord";      break;
     case EvqFrontFacing:    return "FrontFacing";    break;
+    case EvqPointCoord:     return "PointCoord";     break;
     case EvqFragColor:      return "FragColor";      break;
     case EvqFragData:       return "FragData";       break;
     case EvqFragDepth:      return "FragDepth";      break;
+    case EvqLastFragColor:  return "LastFragColor";  break;
+    case EvqLastFragData:   return "LastFragData";   break;
     case EvqSmoothOut:      return "smooth out";     break;
     case EvqCentroidOut:    return "centroid out";   break;
     case EvqFlatOut:        return "flat out";       break;
     case EvqSmoothIn:       return "smooth in";      break;
-    case EvqCentroidIn:     return "centroid in";    break;
     case EvqFlatIn:         return "flat in";        break;
-    case EvqLastFragColor:  return "LastFragColor";  break;
-    case EvqLastFragData:   return "LastFragData";   break;
+    case EvqCentroidIn:     return "centroid in";    break;
     default: UNREACHABLE(); return "unknown qualifier";
     }
 }
