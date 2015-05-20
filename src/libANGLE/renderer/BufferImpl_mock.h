2f9455e338c01e9274e366aa9d8a5a7cf1176fa9
Corentin Wallez <cwallez@chromium.org>
2015-05-20 16:43:57 -0400

    Fix -Winconsistent-override in Mock classes
    Gmock defines virtual functions without the override keyword so the
    constructors using override here caused a warning.
    
    BUG=angleproject:892
    
    Change-Id: I2a8f0445aa4a1e09694b7f98a6da07f8abaa47fb
    Reviewed-on: https://chromium-review.googlesource.com/272551
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Tested-by: Corentin Wallez <cwallez@chromium.org>

--- src/libANGLE/renderer/BufferImpl_mock.h
+++ src/libANGLE/renderer/BufferImpl_mock.h
@@ -19,7 +19,7 @@ namespace rx
 class MockBufferImpl : public BufferImpl
 {
   public:
-    ~MockBufferImpl() override { destructor(); }
+    ~MockBufferImpl() { destructor(); }
 
     MOCK_METHOD3(setData, gl::Error(const void*, size_t, GLenum));
     MOCK_METHOD3(setSubData, gl::Error(const void*, size_t, size_t));
