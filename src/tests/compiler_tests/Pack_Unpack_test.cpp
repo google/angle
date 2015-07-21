01c796ac8e363ae953797f9cf316ec17dc477e87
Geoff Lang <geofflang@chromium.org>
2015-07-21 18:53:58 +0000

    Revert "Emulate the pack/unpack functions for unorms."
    Causing MSAN failures on Linux.
    
    This reverts commit 1915652ee82109d7756dc8349562c3c11ea39b77.
    
    Change-Id: Ib23bec16eab22288930be0b41186e54cd8d1f921
    Reviewed-on: https://chromium-review.googlesource.com/287127
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Tested-by: Geoff Lang <geofflang@chromium.org>

--- src/tests/compiler_tests/Pack_Unpack_test.cpp
+++ src/tests/compiler_tests/Pack_Unpack_test.cpp
@@ -4,7 +4,7 @@
 // found in the LICENSE file.
 //
 // Pack_Unpack_test.cpp:
-//   Tests for the emulating pack_unpack functions for GLSL.
+//   Tests for the emulating pack_unpack functions for GLSL 4.1.
 //
 
 #include "angle_gl.h"
@@ -15,7 +15,7 @@
 namespace
 {
 
-class PackUnpackTest : public testing::TestWithParam<ShShaderOutput>
+class PackUnpackTest : public testing::Test
 {
   public:
     PackUnpackTest() {}
@@ -25,15 +25,15 @@ class PackUnpackTest : public testing::TestWithParam<ShShaderOutput>
     {
         std::string infoLog;
         bool compilationSuccess = compileTestShader(GL_FRAGMENT_SHADER, SH_GLES3_SPEC,
-                                                    GetParam(),
+                                                    SH_GLSL_410_CORE_OUTPUT,
                                                     shaderString, &mGLSLCode, &infoLog);
         if (!compilationSuccess)
         {
-            FAIL() << "Shader compilation into GLSL failed " << infoLog;
+            FAIL() << "Shader compilation into GLSL 4.1 failed " << infoLog;
         }
     }
 
-    bool foundInGLSLCode(const char* stringToFind) const
+    bool foundInGLSLCode(const char* stringToFind)
     {
         return mGLSLCode.find(stringToFind) != std::string::npos;
     }
@@ -42,8 +42,8 @@ class PackUnpackTest : public testing::TestWithParam<ShShaderOutput>
     std::string mGLSLCode;
 };
 
-//Check if PackSnorm2x16 Emulation for GLSL < 4.2 compile correctly.
-TEST_P(PackUnpackTest, PackSnorm2x16Emulation)
+//Check if PackSnorm2x16 Emulation for GLSL 4.1 compile correctly.
+TEST_F(PackUnpackTest, PackSnorm2x16Emulation)
 {
     const std::string &shaderString =
         "#version 300 es\n"
@@ -55,11 +55,11 @@ TEST_P(PackUnpackTest, PackSnorm2x16Emulation)
         "   fragColor = vec4(0.0);\n"
         "}\n";
     compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_420_CORE_OUTPUT, foundInGLSLCode("uint webgl_packSnorm2x16_emu(vec2 v)"));
+    ASSERT_TRUE(foundInGLSLCode("uint webgl_packSnorm2x16_emu(vec2 v)"));
 }
 
-//Check if UnpackSnorm2x16 Emulation for GLSL < 4.2 compile correctly.
-TEST_P(PackUnpackTest, UnpackSnorm2x16Emulation)
+//Check if UnpackSnorm2x16 Emulation for GLSL 4.1 compile correctly.
+TEST_F(PackUnpackTest, UnpackSnorm2x16Emulation)
 {
     const std::string &shaderString =
         "#version 300 es\n"
@@ -71,43 +71,11 @@ TEST_P(PackUnpackTest, UnpackSnorm2x16Emulation)
         "   fragColor = vec4(0.0);\n"
         "}\n";
     compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_420_CORE_OUTPUT, foundInGLSLCode("vec2 webgl_unpackSnorm2x16_emu(uint u)"));
+    ASSERT_TRUE(foundInGLSLCode("vec2 webgl_unpackSnorm2x16_emu(uint u)"));
 }
 
-//Check if PackUnorm2x16 Emulation for GLSL < 4.1 compiles correctly.
-TEST_P(PackUnpackTest, PackUnorm2x16Emulation)
-{
-    const std::string &shaderString =
-        "#version 300 es\n"
-        "precision mediump float;\n"
-        "layout(location = 0) out mediump vec4 fragColor;"
-        "void main() {\n"
-        "   vec2 v;\n"
-        "   uint u = packUnorm2x16(v);\n"
-        "   fragColor = vec4(0.0);\n"
-        "}\n";
-    compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_410_CORE_OUTPUT, foundInGLSLCode("uint webgl_packUnorm2x16_emu(vec2 v)"));
-}
-
-//Check if UnpackSnorm2x16 Emulation for GLSL < 4.1 compiles correctly.
-TEST_P(PackUnpackTest, UnpackUnorm2x16Emulation)
-{
-    const std::string &shaderString =
-        "#version 300 es\n"
-        "precision mediump float;\n"
-        "layout(location = 0) out mediump vec4 fragColor;"
-        "void main() {\n"
-        "   uint u;\n"
-        "   vec2 v=unpackUnorm2x16(u);\n"
-        "   fragColor = vec4(0.0);\n"
-        "}\n";
-    compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_410_CORE_OUTPUT, foundInGLSLCode("vec2 webgl_unpackUnorm2x16_emu(uint u)"));
-}
-
-//Check if PackHalf2x16 Emulation for GLSL < 4.2 compiles correctly.
-TEST_P(PackUnpackTest, PackHalf2x16Emulation)
+//Check if PackHalf2x16 Emulation for GLSL 4.1 compile correctly.
+TEST_F(PackUnpackTest, PackHalf2x16Emulation)
 {
     const std::string &shaderString =
         "#version 300 es\n"
@@ -119,11 +87,11 @@ TEST_P(PackUnpackTest, PackHalf2x16Emulation)
         "   fragColor = vec4(0.0);\n"
         "}\n";
     compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_420_CORE_OUTPUT, foundInGLSLCode("uint webgl_packHalf2x16_emu(vec2 v)"));
+    ASSERT_TRUE(foundInGLSLCode("uint webgl_packHalf2x16_emu(vec2 v)"));
 }
 
-//Check if UnpackHalf2x16 Emulation for GLSL < 4.2 compiles correctly.
-TEST_P(PackUnpackTest, UnpackHalf2x16Emulation)
+//Check if UnpackHalf2x16 Emulation for GLSL 4.1 compile correctly.
+TEST_F(PackUnpackTest, UnpackHalf2x16Emulation)
 {
     const std::string &shaderString =
         "#version 300 es\n"
@@ -135,13 +103,7 @@ TEST_P(PackUnpackTest, UnpackHalf2x16Emulation)
         "   fragColor = vec4(0.0);\n"
         "}\n";
     compile(shaderString);
-    ASSERT_EQ(GetParam() < SH_GLSL_420_CORE_OUTPUT, foundInGLSLCode("vec2 webgl_unpackHalf2x16_emu(uint u)"));
+    ASSERT_TRUE(foundInGLSLCode("vec2 webgl_unpackHalf2x16_emu(uint u)"));
 }
-
-INSTANTIATE_TEST_CASE_P(, PackUnpackTest,
-                        testing::Values(SH_GLSL_330_CORE_OUTPUT, SH_GLSL_400_CORE_OUTPUT,
-                                        SH_GLSL_410_CORE_OUTPUT, SH_GLSL_420_CORE_OUTPUT,
-                                        SH_GLSL_430_CORE_OUTPUT, SH_GLSL_440_CORE_OUTPUT,
-                                        SH_GLSL_450_CORE_OUTPUT));
 
 } // namespace
