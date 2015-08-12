a0633a7e4124473d67538053cba3ebdc44f71687
Geoff Lang <geofflang@chromium.org>
2015-08-12 14:10:36 -0400

    In TextureD3D_2D, don't call Image::redefine directly.
    Call the TextureD3D_2D::redefine member which will not make unnecessary
    modifications to the image and lets the member serve as a callback for
    redefined images.
    
    BUG=angleproject:970
    
    Change-Id: I280c18b993ef4dfac2efe1b28bf28c34aa4b6bcc
    Reviewed-on: https://chromium-review.googlesource.com/293222
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Tested-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/d3d/TextureD3D.h
+++ src/libANGLE/renderer/d3d/TextureD3D.h
@@ -169,7 +169,10 @@ class TextureD3D_2D : public TextureD3D
 
     gl::Error updateStorageLevel(int level);
 
-    void redefineImage(GLint level, GLenum internalformat, const gl::Extents &size);
+    void redefineImage(GLint level,
+                       GLenum internalformat,
+                       const gl::Extents &size,
+                       bool forceRelease);
 
     ImageD3D *mImageArray[gl::IMPLEMENTATION_MAX_TEXTURE_LEVELS];
 };
