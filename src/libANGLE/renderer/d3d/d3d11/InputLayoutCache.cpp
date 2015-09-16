7a04151879dd97275d954e7f3f2473776deae136
Cooper Partin <coopp@microsoft.com>
2015-09-16 08:58:47 -0700

    Fixed FL9_3 shaders that use glPointSize without GL_POINTS mode
    This change fixes rendering on FL9_3 with shaders that reference
    glPointSize and are not used with GL_POINTS rendering mode. A TDR
    occurs on some hardware because of missing vertex buffers specified by
    the vertex layout.
    
    Change-Id: Ie49b75f7177c32c7f021a0409d15c27b18140c4b
    Reviewed-on: https://chromium-review.googlesource.com/299719
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Tested-by: Cooper Partin <coopp@microsoft.com>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/d3d/d3d11/InputLayoutCache.cpp
+++ src/libANGLE/renderer/d3d/d3d11/InputLayoutCache.cpp
@@ -456,7 +456,10 @@ gl::Error InputLayoutCache::applyVertexBuffers(const std::vector<TranslatedAttri
     // PointSprite quad.
     // An index buffer also needs to be created and applied because rendering instanced
     // data on D3D11 FL9_3 requires DrawIndexedInstanced() to be used.
-    if (instancedPointSpritesActive)
+    // Shaders that contain gl_PointSize and used without the GL_POINTS rendering mode
+    // require a vertex buffer because some drivers cannot handle missing vertex data
+    // and will TDR the system.
+    if (programUsesInstancedPointSprites)
     {
         HRESULT result = S_OK;
         const UINT pointSpriteVertexStride = sizeof(float) * 5;
@@ -491,7 +494,10 @@ gl::Error InputLayoutCache::applyVertexBuffers(const std::vector<TranslatedAttri
         }
 
         mCurrentBuffers[nextAvailableIndex] = mPointSpriteVertexBuffer;
-        mCurrentVertexStrides[nextAvailableIndex] = pointSpriteVertexStride;
+        // Set the stride to 0 if GL_POINTS mode is not being used to instruct the driver
+        // to avoid indexing into the vertex buffer.
+        mCurrentVertexStrides[nextAvailableIndex] =
+            instancedPointSpritesActive ? pointSpriteVertexStride : 0;
         mCurrentVertexOffsets[nextAvailableIndex] = 0;
 
         if (!mPointSpriteIndexBuffer)
@@ -519,11 +525,16 @@ gl::Error InputLayoutCache::applyVertexBuffers(const std::vector<TranslatedAttri
             }
         }
 
-        // The index buffer is applied here because Instanced PointSprite emulation uses
-        // the a non-indexed rendering path in ANGLE (DrawArrays).  This means that applyIndexBuffer()
-        // on the renderer will not be called and setting this buffer here ensures that the rendering
-        // path will contain the correct index buffers.
-        mDeviceContext->IASetIndexBuffer(mPointSpriteIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+        if (instancedPointSpritesActive)
+        {
+            // The index buffer is applied here because Instanced PointSprite emulation uses
+            // the a non-indexed rendering path in ANGLE (DrawArrays).  This means that
+            // applyIndexBuffer()
+            // on the renderer will not be called and setting this buffer here ensures that the
+            // rendering
+            // path will contain the correct index buffers.
+            mDeviceContext->IASetIndexBuffer(mPointSpriteIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+        }
     }
 
     if (moveFirstIndexedIntoSlotZero)
