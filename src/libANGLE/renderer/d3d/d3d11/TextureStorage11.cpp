0ecb18b9b98c576699ff85218888c577117cdd59
John Bauman <jbauman@chromium.org>
2016-02-03 13:40:37 -0800

    Avoid a copy in TextureStorage11::setData
    This is essentially a reland of 9cf9bcbecaa1e54a47a92eb784e7f591f7b65c57
    
    This code always allocates a new memory buffer for the texture memory,
    picks a load function to copy/convert the input data into it, and the
    uploads.
    
    In the case where the input format matches the upload format we should
    be able to skip the allocation and copy and be much happier.
    
    Change-Id: If4281aeb4cd7bbbebba60122a10610a916833052
    Reviewed-on: https://chromium-review.googlesource.com/326852
    Tryjob-Request: John Bauman <jbauman@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: John Bauman <jbauman@chromium.org>

--- src/libANGLE/renderer/d3d/d3d11/TextureStorage11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/TextureStorage11.cpp
@@ -664,17 +664,29 @@ gl::Error TextureStorage11::setData(const gl::ImageIndex &index, ImageD3D *image
     UINT bufferDepthPitch = bufferRowPitch * height;
 
     size_t neededSize = bufferDepthPitch * depth;
-    MemoryBuffer *conversionBuffer = NULL;
-    error = mRenderer->getScratchMemoryBuffer(neededSize, &conversionBuffer);
-    if (error.isError())
-    {
-        return error;
-    }
+    MemoryBuffer *conversionBuffer = nullptr;
+    const uint8_t *data            = nullptr;
 
-    // TODO: fast path
-    LoadImageFunction loadFunction = d3d11Format.loadFunctions.at(type);
-    loadFunction(width, height, depth, pixelData + srcSkipBytes, srcRowPitch, srcDepthPitch,
-                 conversionBuffer->data(), bufferRowPitch, bufferDepthPitch);
+    d3d11::LoadImageFunctionInfo loadFunctionInfo = d3d11Format.loadFunctions.at(type);
+    if (loadFunctionInfo.requiresConversion)
+    {
+        error = mRenderer->getScratchMemoryBuffer(neededSize, &conversionBuffer);
+        if (error.isError())
+        {
+            return error;
+        }
+
+        loadFunctionInfo.loadFunction(width, height, depth, pixelData + srcSkipBytes, srcRowPitch,
+                                      srcDepthPitch, conversionBuffer->data(), bufferRowPitch,
+                                      bufferDepthPitch);
+        data = conversionBuffer->data();
+    }
+    else
+    {
+        data             = pixelData + srcSkipBytes;
+        bufferRowPitch   = srcRowPitch;
+        bufferDepthPitch = srcDepthPitch;
+    }
 
     ID3D11DeviceContext *immediateContext = mRenderer->getDeviceContext();
 
@@ -690,15 +702,13 @@ gl::Error TextureStorage11::setData(const gl::ImageIndex &index, ImageD3D *image
         destD3DBox.front = destBox->z;
         destD3DBox.back = destBox->z + destBox->depth;
 
-        immediateContext->UpdateSubresource(resource, destSubresource,
-                                            &destD3DBox, conversionBuffer->data(),
+        immediateContext->UpdateSubresource(resource, destSubresource, &destD3DBox, data,
                                             bufferRowPitch, bufferDepthPitch);
     }
     else
     {
-        immediateContext->UpdateSubresource(resource, destSubresource,
-                                            NULL, conversionBuffer->data(),
-                                            bufferRowPitch, bufferDepthPitch);
+        immediateContext->UpdateSubresource(resource, destSubresource, NULL, data, bufferRowPitch,
+                                            bufferDepthPitch);
     }
 
     return gl::Error(GL_NO_ERROR);
