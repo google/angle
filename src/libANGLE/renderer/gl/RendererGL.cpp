930fefca127fb16c8a65172139ee2fa55a87c2ef
Corentin Wallez <cwallez@chromium.org>
2016-09-14 15:54:18 -0400

    RendererGL: enable GL_DEBUG_OUTPUT on Debug builds
    The intent was already to enable it but without glEnable(GL_DEBUG_OUTPUT)
    the Linux NVIDIA driver doesn't output anything.
    
    BUG=
    
    Change-Id: Ia9a500c2e792ee7c4e2a7be60d478419eda617e1
    Reviewed-on: https://chromium-review.googlesource.com/385278
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/gl/RendererGL.cpp
+++ src/libANGLE/renderer/gl/RendererGL.cpp
@@ -122,6 +122,7 @@ RendererGL::RendererGL(const FunctionsGL *functions, const egl::AttributeMap &at
 #ifndef NDEBUG
     if (mHasDebugOutput)
     {
+        mFunctions->enable(GL_DEBUG_OUTPUT);
         mFunctions->enable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
         mFunctions->debugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_HIGH, 0, nullptr, GL_TRUE);
         mFunctions->debugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_MEDIUM, 0, nullptr, GL_TRUE);
