edc89d62aa1a7adbc3dfbfa471d906363a21cb1f
Jamie Madill <jmadill@chromium.org>
2016-11-17 16:40:20 -0500

    Fix missing file license in HLSLCompiler.h.
    BUG=angleproject:1576
    
    Change-Id: I9fdd7a27944880383df53aa8468884ea2e99e670
    Reviewed-on: https://chromium-review.googlesource.com/412422
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/d3d/HLSLCompiler.h
+++ src/libANGLE/renderer/d3d/HLSLCompiler.h
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2016 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// HLSLCompiler: Wrapper for the D3DCompiler DLL.
+//
+
 #ifndef LIBANGLE_RENDERER_D3D_HLSLCOMPILER_H_
 #define LIBANGLE_RENDERER_D3D_HLSLCOMPILER_H_
 
@@ -53,4 +61,4 @@ class HLSLCompiler : angle::NonCopyable
 
 }
 
-#endif // LIBANGLE_RENDERER_D3D_HLSLCOMPILER_H_
+#endif  // LIBANGLE_RENDERER_D3D_HLSLCOMPILER_H_
