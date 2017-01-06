63d826267778cf2ec4c0e51fdcb3a6b98123aba7
Brian Osman <brianosman@google.com>
2017-01-06 14:05:18 -0500

    Fix preprocessor typo for enabling Windows Runtime Library
    BUG=angleproject:530
    
    Change-Id: Id93bd4f3f82bc0a6a0cbe4b55bc310d11266a2bd
    Reviewed-on: https://chromium-review.googlesource.com/425938
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/common/angleutils.h
+++ src/common/angleutils.h
@@ -23,9 +23,9 @@
 namespace angle
 {
 
-#if defined(ANBLE_ENABLE_D3D9) || defined(ANGLE_ENABLE_D3D11)
+#if defined(ANGLE_ENABLE_D3D9) || defined(ANGLE_ENABLE_D3D11)
 using Microsoft::WRL::ComPtr;
-#endif  // defined(ANBLE_ENABLE_D3D9) || defined(ANGLE_ENABLE_D3D11)
+#endif  // defined(ANGLE_ENABLE_D3D9) || defined(ANGLE_ENABLE_D3D11)
 
 class NonCopyable
 {
