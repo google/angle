bb1db48ed5c7607fbd18e35b57080a28386d8d7e
Jamie Madill <jmadill@chromium.org>
2017-01-10 10:48:32 -0500

    Suppress two new failing tests on OSX 10.12 Intel.
    BUG=chromium:679607
    
    Change-Id: Iacba3958dc942d7830caec8e053148533bff4069
    Reviewed-on: https://chromium-review.googlesource.com/425872
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/tests/gl_tests/TextureTest.cpp
+++ src/tests/gl_tests/TextureTest.cpp
@@ -2751,6 +2751,13 @@ TEST_P(Texture2DTestES3, TextureRGB9E5ImplicitAlpha1)
 // ES 3.0.4 table 3.24
 TEST_P(Texture2DTestES3, TextureCOMPRESSEDRGB8ETC2ImplicitAlpha1)
 {
+    if (IsOSX() && IsIntel() && IsOpenGL())
+    {
+        // Seems to fail on OSX 10.12 Intel.
+        std::cout << "Test skipped on OSX Intel." << std::endl;
+        return;
+    }
+
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, mTexture2D);
     glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_COMPRESSED_RGB8_ETC2, 1, 1, 0, 8, nullptr);
@@ -2765,10 +2772,11 @@ TEST_P(Texture2DTestES3, TextureCOMPRESSEDRGB8ETC2ImplicitAlpha1)
 // ES 3.0.4 table 3.24
 TEST_P(Texture2DTestES3, TextureCOMPRESSEDSRGB8ETC2ImplicitAlpha1)
 {
-    if (IsIntel() && IsLinux())
+    if (IsIntel() && IsOpenGL() && (IsLinux() || IsOSX()))
     {
         // TODO(cwallez): Fix on Linux Intel drivers (http://anglebug.com/1346)
-        std::cout << "Test disabled on Linux Intel OpenGL." << std::endl;
+        // Also seems to fail on OSX 10.12 Intel.
+        std::cout << "Test disabled on Linux and OSX Intel OpenGL." << std::endl;
         return;
     }
 
