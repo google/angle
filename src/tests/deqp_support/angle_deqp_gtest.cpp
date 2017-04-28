bb514004fc5ff1d817b0cb6fefced5f2915f99bd
Geoff Lang <geofflang@chromium.org>
2017-04-28 14:08:38 -0400

    Request RGBA8 configs by default for gtest dEQP.
    Previously, dEQP would just use the first config returned which was
    RGBA4 which is not very representitive of what our users would request.
    
    BUG=angleproject:2337
    BUG=angleproject:1095
    BUG=angleproject:2222
    
    Change-Id: I5c9defc7ce1feaa49407557b88d9bb6b98571771
    Reviewed-on: https://chromium-review.googlesource.com/490726
    Reviewed-by: Luc Ferron <lucferron@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>

--- src/tests/deqp_support/angle_deqp_gtest.cpp
+++ src/tests/deqp_support/angle_deqp_gtest.cpp
@@ -61,6 +61,11 @@ const char *g_angleEGLString = "--use-angle=";
 
 const APIInfo *g_initAPI = nullptr;
 
+constexpr const char *g_deqpEGLConfigNameString = "--deqp-gl-config-name=";
+
+// Default the config to RGBA8
+const char *g_eglConfigName = "rgba8888";
+
 // Returns the default API for a platform.
 const char *GetDefaultAPIName()
 {
@@ -332,6 +337,11 @@ void dEQPTest<TestModuleIndex>::SetUpTestCase()
     std::string apiArgString = std::string(g_deqpEGLString) + targetApi;
     argv.push_back(apiArgString.c_str());
 
+    // Add config name
+    const char *targetConfigName = g_eglConfigName;
+    std::string configArgString  = std::string(g_deqpEGLConfigNameString) + targetConfigName;
+    argv.push_back(configArgString.c_str());
+
     // Init the platform.
     if (!deqp_libtester_init_platform(static_cast<int>(argv.size()), argv.data()))
     {
@@ -416,6 +426,11 @@ void HandleDisplayType(const char *displayTypeString)
     }
 }
 
+void HandleEGLConfigName(const char *configNameString)
+{
+    g_eglConfigName = configNameString;
+}
+
 void DeleteArg(int *argc, int argIndex, char **argv)
 {
     (*argc)--;
@@ -443,6 +458,12 @@ void InitTestHarness(int *argc, char **argv)
         else if (strncmp(argv[argIndex], g_angleEGLString, strlen(g_angleEGLString)) == 0)
         {
             HandleDisplayType(argv[argIndex] + strlen(g_angleEGLString));
+            DeleteArg(argc, argIndex, argv);
+        }
+        else if (strncmp(argv[argIndex], g_deqpEGLConfigNameString,
+                         strlen(g_deqpEGLConfigNameString)) == 0)
+        {
+            HandleEGLConfigName(argv[argIndex] + strlen(g_deqpEGLConfigNameString));
             DeleteArg(argc, argIndex, argv);
         }
         else
