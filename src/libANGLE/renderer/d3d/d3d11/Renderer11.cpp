5788d24bb00c8bd148981b5a6680d0aadd8b8fc2
Geoff Lang <geofflang@chromium.org>
2017-07-10 11:13:36 -0400

    D3D11: Support stencil-only EGL configs.
    WebGL can request contexts with a stencil buffer but no depth buffer.
    
    BUG=angleproject:2075
    
    Change-Id: I46b272089e44d5f1725fa6252b68d34c9e9f37f6
    Reviewed-on: https://chromium-review.googlesource.com/565052
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -949,7 +949,7 @@ egl::ConfigSet Renderer11::generateConfigs()
     }
 
     static const GLenum depthStencilBufferFormats[] = {
-        GL_NONE, GL_DEPTH24_STENCIL8_OES, GL_DEPTH_COMPONENT16,
+        GL_NONE, GL_DEPTH24_STENCIL8_OES, GL_DEPTH_COMPONENT16, GL_STENCIL_INDEX8,
     };
 
     const gl::Caps &rendererCaps                  = getNativeCaps();
