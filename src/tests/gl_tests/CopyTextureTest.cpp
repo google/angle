3dfaf265b48cefd7af8c863893073dc0c3d846ae
Jamie Madill <jmadill@chromium.org>
2017-08-18 12:32:14 -0400

    Add a Linux/Intel suppression for CopyTextureTestES3.ES3UintFormats.
    BUG=chromium:756087
    
    Change-Id: If7d318d58ce46b52a810b1a96fcdbff57a787f86
    Reviewed-on: https://chromium-review.googlesource.com/621408
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/tests/gl_tests/CopyTextureTest.cpp
+++ src/tests/gl_tests/CopyTextureTest.cpp
@@ -1239,6 +1239,8 @@ TEST_P(CopyTextureTestES3, ES3FloatFormats)
 // Test the newly added ES3 unsigned integer formats
 TEST_P(CopyTextureTestES3, ES3UintFormats)
 {
+    ANGLE_SKIP_TEST_IF(IsLinux() && IsOpenGL() && IsIntel());
+
     if (!checkExtensions())
     {
         return;
