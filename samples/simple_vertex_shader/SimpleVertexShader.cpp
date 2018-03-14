2fe36e456a24f1e63b2a630324b0f8e9e63e295c
Jamie Madill <jmadill@chromium.org>
2018-03-14 16:14:26 -0400

    Vulkan: Enable SimpleVertexShader.
    Required correct depth range implementation, matrix uniforms, etc.
    
    Bug: angleproject:2403
    
    Change-Id: I84d12aae0c9eb760645d6b953461122bce7d31d1
    Reviewed-on: https://chromium-review.googlesource.com/967265
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- samples/simple_vertex_shader/SimpleVertexShader.cpp
+++ samples/simple_vertex_shader/SimpleVertexShader.cpp
@@ -25,8 +25,8 @@
 class SimpleVertexShaderSample : public SampleApplication
 {
   public:
-    SimpleVertexShaderSample()
-        : SampleApplication("SimpleVertexShader", 1280, 720)
+    SimpleVertexShaderSample(EGLint displayType)
+        : SampleApplication("SimpleVertexShader", 1280, 720, 2, 0, displayType)
     {
     }
 
@@ -144,6 +144,13 @@ class SimpleVertexShaderSample : public SampleApplication
 
 int main(int argc, char **argv)
 {
-    SimpleVertexShaderSample app;
+    EGLint displayType = EGL_PLATFORM_ANGLE_TYPE_DEFAULT_ANGLE;
+
+    if (argc > 1)
+    {
+        displayType = GetDisplayTypeFromArg(argv[1]);
+    }
+
+    SimpleVertexShaderSample app(displayType);
     return app.run();
 }
