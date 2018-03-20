ed8d5ec7333070baddc731babbca895537a73cf4
Courtney Goeltzenleuchter <courtneygo@google.com>
2018-03-20 10:08:05 -0600

    Add json build file generation
    gen_angle_gn_info_json.py can be used to generate a json description
    of the build for given targets. This information can then be used
    to generate build files for other tool chains (e.g. Android blueprint)
    
    Bug: angleproject:2418
    Change-Id: Ief8f43e30ae1f469e3fcfa795306675e29a90a2f
    Reviewed-on: https://chromium-review.googlesource.com/967225
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/commit_id.py
+++ src/commit_id.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+#  Copyright 2018 The ANGLE Project Authors. All rights reserved.
+#  Use of this source code is governed by a BSD-style license that can be
+#  found in the LICENSE file.
+
+# Generate commit.h with git commit hash.
+#
+
 import subprocess as sp
 import sys
 import os
