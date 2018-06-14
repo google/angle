57707e5f451f08f32d1483c4e9a7332a0347f9e5
Luc Ferron <lucferron@chromium.org>
2018-06-14 14:50:36 -0400

    Vulkan: Fix a state notification change that was wrong
    That state change should really be in the frontend and not in each
    backend.
    
    Bug: angleproject:2594
    Change-Id: I9d9396563ce42a41587b954892d6b4c6002c5072
    Reviewed-on: https://chromium-review.googlesource.com/1101252
    Commit-Queue: Luc Ferron <lucferron@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/BufferVk.cpp
+++ src/libANGLE/renderer/vulkan/BufferVk.cpp
@@ -84,7 +84,6 @@ gl::Error BufferVk::setData(const gl::Context *context,
         ANGLE_TRY(setDataImpl(contextVk, static_cast<const uint8_t *>(data), size, 0));
     }
 
-    onStateChange(context, angle::SubjectMessage::STORAGE_CHANGED);
     return gl::NoError();
 }
 
@@ -100,7 +99,6 @@ gl::Error BufferVk::setSubData(const gl::Context *context,
     ContextVk *contextVk = vk::GetImpl(context);
     ANGLE_TRY(setDataImpl(contextVk, static_cast<const uint8_t *>(data), size, offset));
 
-    onStateChange(context, angle::SubjectMessage::STORAGE_CHANGED);
     return gl::NoError();
 }
 
@@ -124,7 +122,6 @@ gl::Error BufferVk::map(const gl::Context *context, GLenum access, void **mapPtr
     ANGLE_TRY(
         mBufferMemory.map(device, 0, mState.getSize(), 0, reinterpret_cast<uint8_t **>(mapPtr)));
 
-    onStateChange(context, angle::SubjectMessage::STORAGE_CHANGED);
     return gl::NoError();
 }
 
@@ -141,7 +138,6 @@ gl::Error BufferVk::mapRange(const gl::Context *context,
 
     ANGLE_TRY(mBufferMemory.map(device, offset, length, 0, reinterpret_cast<uint8_t **>(mapPtr)));
 
-    onStateChange(context, angle::SubjectMessage::STORAGE_CHANGED);
     return gl::NoError();
 }
 
@@ -154,7 +150,6 @@ gl::Error BufferVk::unmap(const gl::Context *context, GLboolean *result)
 
     mBufferMemory.unmap(device);
 
-    onStateChange(context, angle::SubjectMessage::STORAGE_CHANGED);
     return gl::NoError();
 }
 
