743899de3f2d04826d33c0886bac8132f7d58610
Qin Jiajia <jiajia.qin@intel.com>
2018-10-19 13:53:28 +0800

    ES31: Implement MAX_SHADER_STORAGE_BLOCK_SIZE
    Bug: angleproject:1951
    
    Change-Id: I6353e6ed10a8b441bfbebc10fa6a07cde1cae7d8
    Reviewed-on: https://chromium-review.googlesource.com/c/1317377
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jiajia Qin <jiajia.qin@intel.com>

--- src/libANGLE/renderer/d3d/d3d11/renderer11_utils.cpp
+++ src/libANGLE/renderer/d3d/d3d11/renderer11_utils.cpp
@@ -972,6 +972,13 @@ void SetUAVRelatedResourceLimits(D3D_FEATURE_LEVEL featureLevel, gl::Caps *caps)
     caps->maxCombinedAtomicCounters = reservedUAVsForAtomicCounterBuffers * 1024;
     caps->maxShaderAtomicCounters[gl::ShaderType::Compute] = caps->maxCombinedAtomicCounters;
 
+    // See
+    // https://docs.microsoft.com/en-us/windows/desktop/direct3d11/overviews-direct3d-11-resources-limits
+    // Resource size (in MB) for any of the preceding resources is min(max(128,0.25f * (amount of
+    // dedicated VRAM)), 2048) MB. So we set it to 128MB to keep same with GL backend.
+    caps->maxShaderStorageBlockSize =
+        D3D11_REQ_RESOURCE_SIZE_IN_MEGABYTES_EXPRESSION_A_TERM * 1024 * 1024;
+
     // Allocate the remaining slots for images and shader storage blocks.
     // The maximum number of fragment shader outputs depends on the current context version, so we
     // will not set it here. See comments in Context11::initialize().
