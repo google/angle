9a79c8893bfc2905e14d51e8256660aba853ff7b
Shahbaz Youssefi <syoussefi@chromium.org>
2018-10-29 17:08:42 -0400

    Run DifferentStencilMasks tests
    This test was added and referenced, but was actually never added to the
    list of files to be compiled.  The most recent validation work has been
    regarding WebGL.
    
    This test verifies the behavior when different stencil masks are set for
    front and back faces, which is unsupported in D3D and disallowed in
    WebGL.  In the interest of running the test on all back ends, and that
    the validation was modified last to improve WebGL support, the test runs
    in WebGL compatibility mode.
    
    Bug: chromium:806557
    Change-Id: I7615b9fc18d4203ed342e23881bea6bdd9b3864c
    Reviewed-on: https://chromium-review.googlesource.com/c/1306256
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/tests/gl_tests/DifferentStencilMasksTest.cpp
+++ src/tests/gl_tests/DifferentStencilMasksTest.cpp
@@ -8,6 +8,7 @@
 //
 
 #include "test_utils/ANGLETest.h"
+#include "test_utils/gl_raii.h"
 
 using namespace angle;
 
@@ -26,13 +27,15 @@ class DifferentStencilMasksTest : public ANGLETest
         setConfigAlphaBits(8);
         setConfigDepthBits(24);
         setConfigStencilBits(8);
+
+        setWebGLCompatibilityEnabled(true);
     }
 
     void SetUp() override
     {
         ANGLETest::SetUp();
 
-        mProgram = CompileProgram(essl1_shaders::vs::Simple(), essl1_shaders::fs::Blue());
+        mProgram = CompileProgram(essl1_shaders::vs::Zero(), essl1_shaders::fs::Blue());
         ASSERT_NE(0u, mProgram);
 
         glEnable(GL_STENCIL_TEST);
@@ -60,7 +63,7 @@ TEST_P(DifferentStencilMasksTest, DrawWithSameEffectiveMask)
 
     glUseProgram(mProgram);
 
-    drawQuad(mProgram, essl1_shaders::PositionAttrib(), 0.5f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     EXPECT_GL_NO_ERROR();
 }
@@ -73,64 +76,39 @@ TEST_P(DifferentStencilMasksTest, DrawWithDifferentMask)
 
     glUseProgram(mProgram);
 
-    drawQuad(mProgram, essl1_shaders::PositionAttrib(), 0.5f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     EXPECT_GL_ERROR(GL_INVALID_OPERATION);
 }
 
-class DifferentStencilMasksWithoutStencilBufferTest : public ANGLETest
-{
-  protected:
-    DifferentStencilMasksWithoutStencilBufferTest() : mProgram(0)
-    {
-        setWindowWidth(128);
-        setWindowHeight(128);
-        setConfigRedBits(8);
-        setConfigGreenBits(8);
-        setConfigBlueBits(8);
-        setConfigAlphaBits(8);
-        setConfigDepthBits(0);
-        setConfigStencilBits(0);
-    }
-
-    void SetUp() override
-    {
-        ANGLETest::SetUp();
-
-        mProgram = CompileProgram(essl1_shaders::vs::Simple(), essl1_shaders::fs::Blue());
-        ASSERT_NE(0u, mProgram);
-
-        glEnable(GL_STENCIL_TEST);
-        ASSERT_GL_NO_ERROR();
-    }
-
-    void TearDown() override
-    {
-        glDisable(GL_STENCIL_TEST);
-        if (mProgram != 0)
-            glDeleteProgram(mProgram);
-
-        ANGLETest::TearDown();
-    }
-
-    GLuint mProgram;
-};
-
 // Tests that effectively different front and back masks, without stencil bits, are legal.
-TEST_P(DifferentStencilMasksWithoutStencilBufferTest, DrawWithDifferentMask)
+TEST_P(DifferentStencilMasksTest, DrawWithDifferentMask_NoStencilBuffer)
 {
+    GLTexture texture;
+    glBindTexture(GL_TEXTURE_2D, texture.get());
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 64, 64, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+
+    GLFramebuffer framebuffer;
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.get());
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture.get(), 0);
+
     glStencilMaskSeparate(GL_FRONT, 0x0001);
     glStencilMaskSeparate(GL_BACK, 0x0002);
 
     glUseProgram(mProgram);
 
-    drawQuad(mProgram, essl1_shaders::PositionAttrib(), 0.5f);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     EXPECT_GL_NO_ERROR();
 }
 
 // Use this to select which configurations (e.g. which renderer, which GLES major version) these
 // tests should be run against.
-ANGLE_INSTANTIATE_TEST(DifferentStencilMasksTest, ES2_D3D9(), ES2_D3D11(), ES3_D3D11(), ES2_OPENGL(), ES3_OPENGL());
-ANGLE_INSTANTIATE_TEST(DifferentStencilMasksWithoutStencilBufferTest, ES2_D3D9(), ES2_D3D11(), ES3_D3D11(), ES2_OPENGL(), ES3_OPENGL());
+ANGLE_INSTANTIATE_TEST(DifferentStencilMasksTest,
+                       ES2_D3D9(),
+                       ES2_D3D11(),
+                       ES3_D3D11(),
+                       ES2_OPENGL(),
+                       ES3_OPENGL(),
+                       ES2_VULKAN());
 } // anonymous namespace
