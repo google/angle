902ceed3998617e97aad25d6b632f9e34dff2644
Tim Van Patten <timvp@google.com>
2019-02-20 16:49:22 +0000

    Revert "Create Container ANGLE APK"
    This reverts commit e82c857d38549da44de2c055e9e677a368e8793f.
    
    Reason for revert:
    This is breaking the ANGLE build in the Chromium build process:
    
    https://bugs.chromium.org/p/chromium/issues/detail?id=933783#c6
    
    Original change's description:
    > Create Container ANGLE APK
    > 
    > The ANGLE APK needs to be reverted back to just being a container
    > for the ANGLE shared object libraries, since the rest of the APK
    > contents are in the Android source tree.
    > 
    > Bug: angleproject:3154
    > Test: 'adb sync' the APK on to a device
    > Change-Id: Id245f1e3d269c71054d9759e8ba8be0532afe659
    > Reviewed-on: https://chromium-review.googlesource.com/c/1474341
    > Commit-Queue: Tim Van Patten <timvp@google.com>
    > Reviewed-by: Courtney Goeltzenleuchter <courtneygo@google.com>
    
    TBR=ynovikov@chromium.org,jmadill@google.com,cnorthrop@google.com,courtneygo@google.com,timvp@google.com
    
    Change-Id: I71a739faa2052a707a06bce616ab43a621d5a4ff
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: angleproject:3154
    Reviewed-on: https://chromium-review.googlesource.com/c/1478333
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Commit-Queue: Tim Van Patten <timvp@google.com>

--- android/src/com/google/android/angle/MainActivity.java
+++ android/src/com/google/android/angle/MainActivity.java
@@ -4,10 +4,11 @@
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
-package com.android.angle;
+package com.google.android.angle;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.preference.PreferenceManager;
 
 import com.android.angle.common.*;
 
