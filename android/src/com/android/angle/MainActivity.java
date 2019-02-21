ee4fa0785090f7e78884322397e0c978b6648404
Tim Van Patten <timvp@google.com>
2019-02-21 19:21:09 +0000

    Reland "Create Container ANGLE APK"
    This reverts commit 902ceed3998617e97aad25d6b632f9e34dff2644.
    
    Reason for revert:
    
    Re-landing this since the Chromium build fix has landed:
    https://bugs.chromium.org/p/chromium/issues/detail?id=933783#c29
      Comment 29 by gbeaty@chromium.org, Today (13 minutes ago)
      Tim, you should be able to re-land your change now.
    
    Original change's description:
    > Revert "Create Container ANGLE APK"
    > 
    > This reverts commit e82c857d38549da44de2c055e9e677a368e8793f.
    > 
    > Reason for revert:
    > This is breaking the ANGLE build in the Chromium build process:
    > 
    > https://bugs.chromium.org/p/chromium/issues/detail?id=933783#c6
    > 
    > Original change's description:
    > > Create Container ANGLE APK
    > > 
    > > The ANGLE APK needs to be reverted back to just being a container
    > > for the ANGLE shared object libraries, since the rest of the APK
    > > contents are in the Android source tree.
    > > 
    > > Bug: angleproject:3154
    > > Test: 'adb sync' the APK on to a device
    > > Change-Id: Id245f1e3d269c71054d9759e8ba8be0532afe659
    > > Reviewed-on: https://chromium-review.googlesource.com/c/1474341
    > > Commit-Queue: Tim Van Patten <timvp@google.com>
    > > Reviewed-by: Courtney Goeltzenleuchter <courtneygo@google.com>
    > 
    > TBR=ynovikov@chromium.org,jmadill@google.com,cnorthrop@google.com,courtneygo@google.com,timvp@google.com
    > 
    > Change-Id: I71a739faa2052a707a06bce616ab43a621d5a4ff
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: angleproject:3154
    > Reviewed-on: https://chromium-review.googlesource.com/c/1478333
    > Reviewed-by: Tim Van Patten <timvp@google.com>
    > Commit-Queue: Tim Van Patten <timvp@google.com>
    
    TBR=ynovikov@chromium.org,jmadill@google.com,cnorthrop@google.com,courtneygo@google.com,timvp@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: angleproject:3154
    Change-Id: If3a9abfb1c00aab3278437a233bef29d5febe5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/1480865
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Commit-Queue: Tim Van Patten <timvp@google.com>

--- android/src/com/android/angle/MainActivity.java
+++ android/src/com/android/angle/MainActivity.java
@@ -9,16 +9,11 @@ package com.android.angle;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.android.angle.common.*;
-
 public class MainActivity extends Activity
 {
-    private final String TAG = this.getClass().getSimpleName();
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment);
     }
 }
