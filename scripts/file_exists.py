a9e06db8301143f5d3f742bae9047aba7a7c8fda
Raul Tambre <raul@tambre.ee>
2019-03-13 22:09:25 +0200

    Support Python 3 in //scripts/file_exists.py
    The changes are to allow build files to be generated using Python 3 on Windows.
    
    The scripts still work with Python 2.
    There are no intended behaviour changes.
    
    Bug: 941669
    Change-Id: Ia9d8d61373ee37b4470fd5479e9db01e17994ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1520649
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- scripts/file_exists.py
+++ scripts/file_exists.py
@@ -5,18 +5,20 @@
 #
 # Simple helper for use in 'gn' files to check if a file exists.
 
+from __future__ import print_function
+
 import os, shutil, sys
 
 def main():
-	if len(sys.argv) != 2:
-		print "Usage: %s <path>" % sys.argv[0]
-		sys.exit(1)
+  if len(sys.argv) != 2:
+    print("Usage: %s <path>" % sys.argv[0])
+    sys.exit(1)
 
-	if os.path.exists(sys.argv[1]):
-		print "true"
-	else:
-		print "false"
-	sys.exit(0)
+  if os.path.exists(sys.argv[1]):
+    print("true")
+  else:
+    print("false")
+  sys.exit(0)
 
 if __name__ == '__main__':
   main()
