9cce3cd9e376ff5889bad52fb197774ca6b3bc52
Jamie Madill <jmadill@chromium.org>
2019-03-27 15:24:12 -0400

    Update texure cache after teleting bound texture.
    The texture cache could become out of sync. And we could end up
    dereferencing an invalid pointer.
    
    Bug: chromium:943538
    Change-Id: I6a99a04e80fc551b6177e25b7bee09c6ae226340
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1541718
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Jonah Ryan-Davis <jonahr@google.com>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -538,10 +538,19 @@ ANGLE_INLINE void State::updateActiveTextureState(const Context *context,
         }
     }
 
-    mTexturesIncompatibleWithSamplers[textureIndex] =
-        !texture->getTextureState().compatibleWithSamplerFormat(
-            mProgram->getState().getSamplerFormatForTextureUnitIndex(textureIndex),
-            sampler ? sampler->getSamplerState() : texture->getSamplerState());
+    if (mProgram)
+    {
+        const SamplerState &samplerState =
+            sampler ? sampler->getSamplerState() : texture->getSamplerState();
+        mTexturesIncompatibleWithSamplers[textureIndex] =
+            !texture->getTextureState().compatibleWithSamplerFormat(
+                mProgram->getState().getSamplerFormatForTextureUnitIndex(textureIndex),
+                samplerState);
+    }
+    else
+    {
+        mTexturesIncompatibleWithSamplers[textureIndex] = false;
+    }
 
     mDirtyBits.set(DIRTY_BIT_TEXTURE_BINDINGS);
 }
@@ -1124,15 +1133,17 @@ void State::detachTexture(const Context *context, const TextureMap &zeroTextures
     for (TextureType type : angle::AllEnums<TextureType>())
     {
         TextureBindingVector &textureVector = mSamplerTextures[type];
-        for (BindingPointer<Texture> &binding : textureVector)
+
+        for (size_t bindingIndex = 0; bindingIndex < textureVector.size(); ++bindingIndex)
         {
+            BindingPointer<Texture> &binding = textureVector[bindingIndex];
             if (binding.id() == texture)
             {
+                // Zero textures are the "default" textures instead of NULL
                 Texture *zeroTexture = zeroTextures[type].get();
                 ASSERT(zeroTexture != nullptr);
-                // Zero textures are the "default" textures instead of NULL
                 binding.set(context, zeroTexture);
-                mDirtyBits.set(DIRTY_BIT_TEXTURE_BINDINGS);
+                updateActiveTexture(context, bindingIndex, zeroTexture);
             }
         }
     }
