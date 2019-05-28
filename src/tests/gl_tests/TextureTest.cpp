29ac274f065e2b6983d3d02668e7955edbcf21a7
Jamie Madill <jmadill@chromium.org>
2019-05-28 15:53:00 -0400

    D3D11: Suppress failing Texture test on AMD.
    Bug: chromium:967796
    Change-Id: I48280909db9c927c12d3c0d97f1ad451dffb849e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1632949
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/tests/gl_tests/TextureTest.cpp
+++ src/tests/gl_tests/TextureTest.cpp
@@ -4722,6 +4722,9 @@ TEST_P(TextureCubeIntegerEdgeTestES3, IntegerCubeTextureCorner)
 // texture is output.
 TEST_P(Texture2DIntegerProjectiveOffsetTestES3, NonZeroBaseLevel)
 {
+    // Fails on AMD: http://crbug.com/967796
+    ANGLE_SKIP_TEST_IF(IsAMD() && IsWindows() && IsD3D11());
+
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, mTexture2D);
     int width     = getWindowWidth();
