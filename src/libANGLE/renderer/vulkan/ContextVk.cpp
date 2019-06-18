71ad2a732e412d6d6f5b2b21540938cc890d3edb
Jamie Madill <jmadill@chromium.org>
2019-06-18 21:10:28 +0000

    Revert "Vulkan: Add missing command pool reset."
    This reverts commit 9aa865a6f9885fce602e9b637a18fb124ce9d372.
    
    Reason for revert: Seems to be causing crashes on Android in glmark2.
    Might also be responsible for a perf regression.
    
    Bug: angleproject:3542
    
    Original change's description:
    > Vulkan: Add missing command pool reset.
    > 
    > We were still calling destroy in checkCompletedCommands. We can
    > instead call reset.
    > 
    > May not significantly impact performance.
    > 
    > Bug: angleproject:3489
    > Change-Id: I61a2cb154faac56dc78a49678dd822d81ce16ad2
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1647029
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Reviewed-by: Tobin Ehlis <tobine@google.com>
    > Commit-Queue: Jamie Madill <jmadill@chromium.org>
    
    TBR=tobine@google.com,jmadill@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: angleproject:3489
    Change-Id: Icb0e99f18ecab09798654605973958bd2b263002
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1665748
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -2166,8 +2166,7 @@ angle::Result ContextVk::checkCompletedCommands()
 
         batch.fence.reset(device);
         TRACE_EVENT0("gpu.angle", "commandPool.destroy");
-        batch.commandPool.reset(device, 0);
-        mCommandPoolFreeList.emplace_back(std::move(batch.commandPool));
+        batch.commandPool.destroy(device);
         ++finishedCount;
     }
 
