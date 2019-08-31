93bb092f88c982ef4c6d4e0db72afc5d7c178b42
Shahbaz Youssefi <syoussefi@chromium.org>
2019-08-31 13:44:28 -0400

    Fix build error
    Result of simultaneous submission of two CLs.
    
    Bug: angleproject:3757
    Change-Id: I0e11678633587421f83d337958a1df28a694cf2e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1778967
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/SurfaceVk.h
+++ src/libANGLE/renderer/vulkan/SurfaceVk.h
@@ -244,8 +244,7 @@ class WindowSurfaceVk : public SurfaceVk
                           EGLint n_rects,
                           bool *presentOutOfDate);
 
-    struct SwapchainImage;
-    angle::Result updateAndDrawOverlay(ContextVk *contextVk, SwapchainImage *image) const;
+    angle::Result updateAndDrawOverlay(ContextVk *contextVk, impl::SwapchainImage *image) const;
 
     angle::Result swapImpl(const gl::Context *context, EGLint *rects, EGLint n_rects);
 
