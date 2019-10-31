edc560fec25fd3b01979f236ea4c3bb139df4874
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-10-31 17:18:29 -0600

    Vulkan: Add 565 and validate pixel formats: Windows
    Add support for 565 to Windows. Also includes code that verifies
    generated configs can actually be supported by Vulkan.
    Test:
    --use-angle=swiftshader --deqp-gl-config-name=rgb565d0s0ms0 --gtest_filter=dEQP.GLES3/functional_fragment_ops_interaction_basic_shader_80
    
    Bug: angleproject:4016
    Change-Id: Idcfd4dd368c4a30ef177aa89e1035d2f00570c99
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1885378
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Courtney Goeltzenleuchter <courtneygo@google.com>

--- src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.h
+++ src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.h
@@ -18,6 +18,10 @@ class DisplayVkWin32 : public DisplayVk
 {
   public:
     DisplayVkWin32(const egl::DisplayState &state);
+    ~DisplayVkWin32() override;
+
+    egl::Error initialize(egl::Display *display) override;
+    void terminate() override;
 
     bool isValidNativeWindow(EGLNativeWindowType window) const override;
 
@@ -28,6 +32,11 @@ class DisplayVkWin32 : public DisplayVk
     bool checkConfigSupport(egl::Config *config) override;
 
     const char *getWSIExtension() const override;
+
+  private:
+    ATOM mWindowClass;
+    HWND mDummyWindow;
+    std::vector<VkSurfaceFormatKHR> mSurfaceFormats;
 };
 
 }  // namespace rx
