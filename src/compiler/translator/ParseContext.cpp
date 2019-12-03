4ad8b55ef4274c730788fe3d42e6ebc27095ca6e
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-12-03 10:07:41 -0700

    Return valid qualifier after error
    Using EvqLast for the variable causes us to hit an assert in
    sh::getQualifierString when we try to print a later error message.
    Just use the appropriate qualifier regardless that it's not valid
    for a compute shader, we've output the error.
    
    Bug: angleproject:4187
    Change-Id: If6fa5dea07d7b6e0d3307485f41d59c5ea7f1c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/1948525
    Commit-Queue: Courtney Goeltzenleuchter <courtneygo@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -4731,7 +4731,7 @@ TStorageQualifierWrapper *TParseContext::parseOutQualifier(const TSourceLoc &loc
         case GL_COMPUTE_SHADER:
         {
             error(loc, "storage qualifier isn't supported in compute shaders", "out");
-            return new TStorageQualifierWrapper(EvqLast, loc);
+            return new TStorageQualifierWrapper(EvqOut, loc);
         }
         case GL_GEOMETRY_SHADER_EXT:
         {
