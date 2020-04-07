90e07c7342d0fac3f483d0910b4103367f2b6c9c
shrekshao <shrekshao@google.com>
2020-04-06 17:04:32 -0700

    Hook up several drawElements variants in Context11
    Turns out the issue for the skia bug is not related to index range
    cache but simply several new drawElemnts variants are not properly
    hooked up.
    
    Change-Id: I2d1ae15c74233185e4fee8a1443ffe0477f9c444
    Bug: angleproject:3402, angleproject:4536
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2137943
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/d3d/d3d11/Context11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Context11.cpp
@@ -341,9 +341,7 @@ angle::Result Context11::drawElementsBaseVertex(const gl::Context *context,
                                                 const void *indices,
                                                 GLint baseVertex)
 {
-    // TODO: angleproject:3402
-    UNIMPLEMENTED();
-    return angle::Result::Stop;
+    return drawElementsImpl(context, mode, count, type, indices, 0, baseVertex, 0);
 }
 
 angle::Result Context11::drawElementsInstanced(const gl::Context *context,
@@ -364,9 +362,7 @@ angle::Result Context11::drawElementsInstancedBaseVertex(const gl::Context *cont
                                                          GLsizei instances,
                                                          GLint baseVertex)
 {
-    // TODO: angleproject:3402
-    UNIMPLEMENTED();
-    return angle::Result::Stop;
+    return drawElementsImpl(context, mode, count, type, indices, instances, baseVertex, 0);
 }
 
 angle::Result Context11::drawElementsInstancedBaseVertexBaseInstance(const gl::Context *context,
@@ -402,9 +398,7 @@ angle::Result Context11::drawRangeElementsBaseVertex(const gl::Context *context,
                                                      const void *indices,
                                                      GLint baseVertex)
 {
-    // TODO: angleproject:3402
-    UNIMPLEMENTED();
-    return angle::Result::Stop;
+    return drawElementsImpl(context, mode, count, type, indices, 0, baseVertex, 0);
 }
 
 angle::Result Context11::drawArraysIndirect(const gl::Context *context,
