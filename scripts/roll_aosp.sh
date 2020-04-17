6cea4405d1d10959ce77f12c393890180fcc3fc7
Geoff Lang <geofflang@google.com>
2020-04-17 12:29:07 -0400

    Remove more bad files before rolling into AOSP.
    third_party/VulkanMemoryAllocator/OWNERS also includes owners that block
    uploading.
    
    Move the deletion of the bad files earlier in the script and rm them
    instead of git rm. They may or may not be committed git files but rm
    always works.
    
    BUG=b/150892231
    
    Change-Id: Iee60fba504ed1c16e994ed1ba83d2b7c58c0a01c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2154537
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Tim Van Patten <timvp@google.com>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -70,17 +70,19 @@ for dep in ${deps[@]}; do
     rm -rf $dep/.git
 done
 
-for dep in ${deps[@]}; do
-    git add -f $dep
-done
-
 extra_removal_files=(
-    # The jsoncpp OWNERS file contains users that have not logged into the Android gerrit so it fails to upload.
+    # The jsoncpp OWNERS and VulkanMemoryAllocator file contains users that have not logged into
+    # the Android gerrit so it fails to upload.
     "third_party/jsoncpp/OWNERS"
+    "third_party/VulkanMemoryAllocator/OWNERS"
 )
 
 for removal_file in ${extra_removal_files[@]}; do
-    git rm $removal_file
+    rm $removal_file
+done
+
+for dep in ${deps[@]}; do
+    git add -f $dep
 done
 
 git commit --amend --no-edit
