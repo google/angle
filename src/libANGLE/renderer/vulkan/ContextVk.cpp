a5e465524dd7816eab13363916abea6e5c6d089e
Ian Elliott <ianelliott@google.com>
2020-04-20 09:33:21 -0600

    Vulkan: glDrawElements used old offset into index buffer
    When the vertex array binding is changed, the value of
    ContextVk::mLastIndexBufferOffset must be reset so that an
    old/cached offset (from a previous draw call) is not used for a
    new draw call.
    
    This addresses rendering artifacts and Vulkan validation errors
    seen with running NBA 2K20 on Android (or the ANGLE trace on
    Windows).
    
    Bug: b/153682968
    Bug: angleproject:4575
    Bug: angleproject:4576
    Change-Id: I69089964b8d278c7fc8610ab003548864a215dab
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2152850
    Commit-Queue: Ian Elliott <ianelliott@google.com>
    Reviewed-by: Courtney Goeltzenleuchter <courtneygo@google.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -2901,6 +2901,7 @@ angle::Result ContextVk::syncState(const gl::Context *context,
                 mVertexArray = vk::GetImpl(glState.getVertexArray());
                 invalidateDefaultAttributes(context->getStateCache().getActiveDefaultAttribsMask());
                 mVertexArray->updateActiveAttribInfo(this);
+                setIndexBufferDirty();
                 break;
             }
             case gl::State::DIRTY_BIT_DRAW_INDIRECT_BUFFER_BINDING:
