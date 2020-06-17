fa3d64bf72d69e94f8754e52fcf1d2b5ed65a671
Ian Elliott <ianelliott@google.com>
2020-06-17 15:14:02 -0600

    Minor edits to top of EGLPreRotationTest.cpp
    Bug: b/158337857
    Change-Id: Id415496fbf38b63975b29834cd93c3b3c56defcb
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2250940
    Reviewed-by: Ian Elliott <ianelliott@google.com>
    Commit-Queue: Ian Elliott <ianelliott@google.com>

--- src/tests/egl_tests/EGLPreRotationTest.cpp
+++ src/tests/egl_tests/EGLPreRotationTest.cpp
@@ -1,10 +1,10 @@
 //
-// Copyright 2015 The ANGLE Project Authors. All rights reserved.
+// Copyright 2020 The ANGLE Project Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-// EGLSurfaceTest:
-//   Tests pertaining to egl::Surface.
+// EGLPreRotationTest:
+//   Tests pertaining to Android pre-rotation.
 //
 
 #include <gtest/gtest.h>
