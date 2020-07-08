514c8f64ab2a5bbb52e366394f907d36a4d00804
Jonah Ryan-Davis <jonahr@google.com>
2020-07-08 13:48:54 -0400

    Skip MultithreadingTest.MultiCreateContext on Ozone
    This tests crashes on Ozone, skip for now.
    
    Bug: chromium:1103009
    Change-Id: Ie686e69d937710b43b615372251801e83b7bde20
    TBR=geofflang@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2288334
    Reviewed-by: Jonah Ryan-Davis <jonahr@google.com>
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>

--- src/tests/gl_tests/MultithreadingTest.cpp
+++ src/tests/gl_tests/MultithreadingTest.cpp
@@ -205,7 +205,8 @@ TEST_P(MultithreadingTest, MultiContextDraw)
 TEST_P(MultithreadingTest, MultiCreateContext)
 {
     // Supported by CGL, GLX, and WGL (https://anglebug.com/4725)
-    ANGLE_SKIP_TEST_IF(!(IsWindows() || IsLinux() || IsOSX()));
+    // Not supported on Ozone (https://crbug.com/1103009)
+    ANGLE_SKIP_TEST_IF(!(IsWindows() || IsLinux() || IsOSX()) || IsOzone());
 
     EGLWindow *window  = getEGLWindow();
     EGLDisplay dpy     = window->getDisplay();
