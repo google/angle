b89e9611baea44d4682ef8d42c43a4cc7fa173f4
Jonah Ryan-Davis <jonahr@google.com>
2020-08-19 11:19:35 -0400

    Fix SwANGLE suppressions of EGLIOSurfaceClientBufferTest
    Suppressions were missing for several texture formats. This slipped
    through  because angle_end2end_tests are not run on SwANGLE on Mac.
    
    Bug: angleproject:4369
    Change-Id: I0cbafe5daab591da15f86f2658c4edc724ae2561
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2364190
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>

--- src/tests/egl_tests/EGLIOSurfaceClientBufferTest.cpp
+++ src/tests/egl_tests/EGLIOSurfaceClientBufferTest.cpp
@@ -389,6 +389,9 @@ TEST_P(IOSurfaceClientBufferTest, RenderToBGRA8888IOSurface)
 {
     ANGLE_SKIP_TEST_IF(!hasIOSurfaceExt());
 
+    // TODO(http://anglebug.com/4369)
+    ANGLE_SKIP_TEST_IF(isSwiftshader());
+
     ScopedIOSurfaceRef ioSurface = CreateSinglePlaneIOSurface(1, 1, 'BGRA', 4);
 
     GLColor color(3, 2, 1, 4);
@@ -439,6 +442,9 @@ TEST_P(IOSurfaceClientBufferTest, RenderToRG88IOSurface)
 {
     ANGLE_SKIP_TEST_IF(!hasIOSurfaceExt());
 
+    // TODO(http://anglebug.com/4369)
+    ANGLE_SKIP_TEST_IF(isSwiftshader());
+
     ScopedIOSurfaceRef ioSurface = CreateSinglePlaneIOSurface(1, 1, '2C08', 2);
 
     std::array<uint8_t, 2> color{1, 2};
@@ -460,6 +466,9 @@ TEST_P(IOSurfaceClientBufferTest, ReadFromRG88IOSurface)
 TEST_P(IOSurfaceClientBufferTest, RenderToR8IOSurface)
 {
     ANGLE_SKIP_TEST_IF(!hasIOSurfaceExt());
+
+    // TODO(http://anglebug.com/4369)
+    ANGLE_SKIP_TEST_IF(isSwiftshader());
 
     ScopedIOSurfaceRef ioSurface = CreateSinglePlaneIOSurface(1, 1, 'L008', 1);
 
