eb1df14b18a0dd67ae416e9b426dca3a18c3fc5c
Jonah Ryan-Davis <jonahr@google.com>
2020-10-01 16:25:33 -0400

    Suppress MultisampledRenderToTextureBenchmark on Pixel2/GLES
    This test started failing, but we don't care about performance on
    GLES so we can suppress it for now.
    
    Bug: angleproject:5120
    Change-Id: If5eb865956ee5fb6f798d110d42e3493294bc18e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2443590
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>

--- src/tests/perf_tests/MultisampledRenderToTexturePerf.cpp
+++ src/tests/perf_tests/MultisampledRenderToTexturePerf.cpp
@@ -106,6 +106,12 @@ MultisampledRenderToTextureBenchmark::MultisampledRenderToTextureBenchmark()
     {
         mSkipTest = true;
     }
+
+    // Fails on Pixel 2 GLES: http://anglebug.com/5120
+    if (IsPixel2() && GetParam().eglParameters.renderer == EGL_PLATFORM_ANGLE_TYPE_OPENGLES_ANGLE)
+    {
+        mSkipTest = true;
+    }
 }
 
 void MultisampledRenderToTextureBenchmark::initializeBenchmark()
