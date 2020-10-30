04d7be3fd729810bf7e1b4f3f3454dc3bda52a76
Yuly Novikov <ynovikov@chromium.org>
2020-10-29 20:40:48 -0400

    Skip InstancingTest.NonIndexed* with non-zero Offset on Mac Mini 8.1
    Bug: angleproject:5271
    Change-Id: I644b28670ce0501407797f7ae7e749a66a63d84e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2508975
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- src/tests/gl_tests/InstancingTest.cpp
+++ src/tests/gl_tests/InstancingTest.cpp
@@ -122,6 +122,10 @@ class InstancingTest : public ANGLETest
         // Unknown problem.  FL9_3 is not officially supported anyway.
         ANGLE_SKIP_TEST_IF(IsD3D11_FL93() && geometry == Quad && draw == NonIndexed);
 
+        // http://anglebug.com/5271
+        ANGLE_SKIP_TEST_IF(IsOSX() && IsIntelUHD630Mobile() && IsDesktopOpenGL() &&
+                           draw == NonIndexed && offset != 0);
+
         // The window is divided into kMaxDrawn slices of size kDrawSize.
         // The slice drawn into is determined by the instance datum.
         // The instance data array selects all the slices in order.
