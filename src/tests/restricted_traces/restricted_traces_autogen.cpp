e51c9068a84f14e42157c097eb764ec3009e0e54
Cody Northrop <cnorthrop@google.com>
2020-11-10 20:57:07 -0700

    Tests: Add Google Maps trace
    TBR: jmadill@chromium.org,courtneygo@google.com
    Test: angle_perftests --gtest_filter="*google_maps*"
    Bug: b/172977194
    Change-Id: I1e2a2ed7af95069d30fd4d0fede036555688e98b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2530456
    Reviewed-by: Cody Northrop <cnorthrop@google.com>
    Commit-Queue: Cody Northrop <cnorthrop@google.com>

--- src/tests/restricted_traces/restricted_traces_autogen.cpp
+++ src/tests/restricted_traces/restricted_traces_autogen.cpp
@@ -21,6 +21,7 @@
 #include "egypt_1500/egypt_1500_capture_context1.h"
 #include "fate_grand_order/fate_grand_order_capture_context1.h"
 #include "free_fire/free_fire_capture_context1.h"
+#include "google_maps/google_maps_capture_context3.h"
 #include "kartrider_rush/kartrider_rush_capture_context1.h"
 #include "lego_legacy/lego_legacy_capture_context2.h"
 #include "manhattan_10/manhattan_10_capture_context1.h"
@@ -74,6 +75,9 @@ constexpr angle::PackedEnumMap<RestrictedTraceID, TraceInfo> kTraceInfos = {
     {RestrictedTraceID::free_fire,
      {free_fire::kReplayFrameStart, free_fire::kReplayFrameEnd, free_fire::kReplayDrawSurfaceWidth,
       free_fire::kReplayDrawSurfaceHeight, "free_fire"}},
+    {RestrictedTraceID::google_maps,
+     {google_maps::kReplayFrameStart, google_maps::kReplayFrameEnd,
+      google_maps::kReplayDrawSurfaceWidth, google_maps::kReplayDrawSurfaceHeight, "google_maps"}},
     {RestrictedTraceID::kartrider_rush,
      {kartrider_rush::kReplayFrameStart, kartrider_rush::kReplayFrameEnd,
       kartrider_rush::kReplayDrawSurfaceWidth, kartrider_rush::kReplayDrawSurfaceHeight,
@@ -157,6 +161,9 @@ void ReplayFrame(RestrictedTraceID traceID, uint32_t frameIndex)
         case RestrictedTraceID::free_fire:
             free_fire::ReplayContext1Frame(frameIndex);
             break;
+        case RestrictedTraceID::google_maps:
+            google_maps::ReplayContext3Frame(frameIndex);
+            break;
         case RestrictedTraceID::kartrider_rush:
             kartrider_rush::ReplayContext1Frame(frameIndex);
             break;
@@ -230,6 +237,9 @@ void ResetReplay(RestrictedTraceID traceID)
             break;
         case RestrictedTraceID::free_fire:
             free_fire::ResetContext1Replay();
+            break;
+        case RestrictedTraceID::google_maps:
+            google_maps::ResetContext3Replay();
             break;
         case RestrictedTraceID::kartrider_rush:
             kartrider_rush::ResetContext1Replay();
@@ -305,6 +315,9 @@ void SetupReplay(RestrictedTraceID traceID)
         case RestrictedTraceID::free_fire:
             free_fire::SetupContext1Replay();
             break;
+        case RestrictedTraceID::google_maps:
+            google_maps::SetupContext3Replay();
+            break;
         case RestrictedTraceID::kartrider_rush:
             kartrider_rush::SetupContext1Replay();
             break;
@@ -379,6 +392,9 @@ void SetBinaryDataDir(RestrictedTraceID traceID, const char *dataDir)
         case RestrictedTraceID::free_fire:
             free_fire::SetBinaryDataDir(dataDir);
             break;
+        case RestrictedTraceID::google_maps:
+            google_maps::SetBinaryDataDir(dataDir);
+            break;
         case RestrictedTraceID::kartrider_rush:
             kartrider_rush::SetBinaryDataDir(dataDir);
             break;
@@ -452,6 +468,9 @@ void SetBinaryDataDecompressCallback(RestrictedTraceID traceID, DecompressCallba
             break;
         case RestrictedTraceID::free_fire:
             free_fire::SetBinaryDataDecompressCallback(callback);
+            break;
+        case RestrictedTraceID::google_maps:
+            google_maps::SetBinaryDataDecompressCallback(callback);
             break;
         case RestrictedTraceID::kartrider_rush:
             kartrider_rush::SetBinaryDataDecompressCallback(callback);
