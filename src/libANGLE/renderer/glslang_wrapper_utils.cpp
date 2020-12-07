56f4a5011bcbac20834eb845b943dc522b86296d
Jamie Madill <jmadill@chromium.org>
2020-12-07 14:41:32 -0500

    Vulkan: Add debugging code for SPIRV transformer.
    Requires a custom define for output.
    
    Bug: angleproject:3572
    Change-Id: I3721c4067c61f3e22ce7c047054b74d3fe468d8f
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2578058
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/glslang_wrapper_utils.cpp
+++ src/libANGLE/renderer/glslang_wrapper_utils.cpp
@@ -50,6 +50,11 @@ ANGLE_REENABLE_EXTRA_SEMI_WARNING
                                                  \
     } while (0)
 
+// Enable this for debug logging of pre-transform SPIR-V:
+#if !defined(ANGLE_DEBUG_SPIRV_TRANSFORMER)
+#    define ANGLE_DEBUG_SPIRV_TRANSFORMER 0
+#endif  // !defined(ANGLE_DEBUG_SPIRV_TRANSFORMER)
+
 namespace rx
 {
 namespace
@@ -3740,6 +3745,14 @@ angle::Result GlslangTransformSpirvCode(const GlslangErrorCallback &callback,
     {
         return angle::Result::Continue;
     }
+
+#if defined(ANGLE_DEBUG_SPIRV_TRANSFORMER) && ANGLE_DEBUG_SPIRV_TRANSFORMER
+    spvtools::SpirvTools spirvTools(SPV_ENV_VULKAN_1_1);
+    spirvTools.SetMessageConsumer(ValidateSpirvMessage);
+    std::string readableSpirv;
+    spirvTools.Disassemble(initialSpirvBlob, &readableSpirv, 0);
+    fprintf(stderr, "%s\n", readableSpirv.c_str());
+#endif  // defined(ANGLE_DEBUG_SPIRV_TRANSFORMER) && ANGLE_DEBUG_SPIRV_TRANSFORMER
 
     // Transform the SPIR-V code by assigning location/set/binding values.
     SpirvTransformer transformer(initialSpirvBlob, removeEarlyFragmentTestsOptimization,
