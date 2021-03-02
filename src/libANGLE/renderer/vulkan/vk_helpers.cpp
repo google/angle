58c35d4215210b3904a9a06322edd23e0f72bd62
Mohan Maiya <m.maiya@samsung.com>
2021-03-02 09:00:55 -0800

    Vulkan: Unmap buffer when adding to mBufferFreeList
    The commit b5af8bde13 moved the unmap call on a buffer to
    when it is placed in mBufferFreeList. There was one place
    that was missed, rectifying that oversight here.
    
    Bug: angleproject:5689
    Change-Id: Iaed2916de374d55ab59276e2679c23ed7ef9c4e8
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2733476
    Commit-Queue: Mohan Maiya <m.maiya@samsung.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1947,6 +1947,7 @@ void DynamicBuffer::releaseInFlightBuffers(ContextVk *contextVk)
         }
         else
         {
+            toRelease->unmap(contextVk->getRenderer());
             mBufferFreeList.push_back(std::move(toRelease));
         }
     }
