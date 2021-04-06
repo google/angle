b4cb63a5bea74186c23a6ac32e49424182786d4c
Jamie Madill <jmadill@chromium.org>
2021-04-06 19:22:32 -0400

    Handle trace prerequisites.
    real_gangster_crime: external images
    avakin_life: external images
    hill_climb_racing: skip on Adreno
    
    Bug: angleproject:5822
    Bug: angleproject:5823
    Change-Id: I7c6a27eab72ba69464748928c93dda57fa8094f4
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2809653
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -309,6 +309,15 @@ TracePerfTest::TracePerfTest()
     {
         // Linux+Nvidia doesn't support GL_KHR_texture_compression_astc_ldr (possibly others also)
         addExtensionPrerequisite("GL_KHR_texture_compression_astc_ldr");
+
+        // Intel doesn't support external images.
+        addExtensionPrerequisite("GL_OES_EGL_image_external");
+
+        // Failing on Linux Intel due to invalid enum. http://anglebug.com/5822
+        if (IsLinux() && IsIntel() && param.driver != GLESDriverType::AngleEGL)
+        {
+            mSkipTest = true;
+        }
     }
 
     if (param.testID == RestrictedTraceID::asphalt_8)
@@ -493,6 +502,20 @@ TracePerfTest::TracePerfTest()
         {
             mSkipTest = true;
         }
+    }
+
+    // Adreno gives a driver error with empty/small draw calls. http://anglebug.com/5823
+    if (param.testID == RestrictedTraceID::hill_climb_racing)
+    {
+        if (IsAndroid() && IsPixel2() && param.driver == GLESDriverType::SystemEGL)
+        {
+            mSkipTest = true;
+        }
+    }
+
+    if (param.testID == RestrictedTraceID::avakin_life)
+    {
+        addExtensionPrerequisite("GL_OES_EGL_image_external");
     }
 
     // We already swap in TracePerfTest::drawBenchmark, no need to swap again in the harness.
