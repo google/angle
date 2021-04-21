95e75ec86ff3b84751b201dabb1362be68bac798
Jamie Madill <jmadill@chromium.org>
2021-04-21 09:26:57 -0400

    Remove 'six' from roll_chromium_deps.
    This dep was causing the script to fail. It may be because the repo
    currently has a single commit. Work around this by removing the
    entry from the auto-roller for now.
    
    Bug: angleproject:2344
    Change-Id: Id4f61124aae7ab43aa2343faf19416ff676314d7
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2842224
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- scripts/roll_chromium_deps.py
+++ scripts/roll_chromium_deps.py
@@ -67,7 +67,6 @@ ANGLE_CHROMIUM_DEPS = [
     'third_party/qemu-linux-x64',
     'third_party/qemu-mac-x64',
     'third_party/r8',
-    'third_party/six',
     'third_party/turbine',
     'third_party/zlib',
     'tools/clang',
