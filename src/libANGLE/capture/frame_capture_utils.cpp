966c6a9aa0d4531169236aa480073d4f806f164f
Jamie Madill <jmadill@chromium.org>
2021-07-15 12:50:09 -0400

    Capture/Replay: Don't try to serialize compressed textures.
    Instead we write out a simple string indicating compressed texel
    contents. We in the future could implement a GetCompressedTexImage
    API.
    
    Bug: angleproject:6176
    Change-Id: I6673ec85ac94d5268b24bda4d510247a63fa97e4
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3032022
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Tim Van Patten <timvp@google.com>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- src/libANGLE/capture/frame_capture_utils.cpp
+++ src/libANGLE/capture/frame_capture_utils.cpp
@@ -1228,10 +1228,18 @@ Result SerializeTextureData(JsonSerializer *json,
 
         if (texture->getState().getInitState() == gl::InitState::Initialized)
         {
-            ANGLE_TRY(texture->getTexImage(context, packState, nullptr, index.getTarget(),
-                                           index.getLevelIndex(), getFormat, getType,
-                                           texelsPtr->data()));
-            json->addBlob("Texels", texelsPtr->data(), texelsPtr->size());
+            if (format.compressed)
+            {
+                // TODO: Read back compressed data. http://anglebug.com/6177
+                json->addCString("Texels", "compressed texel data");
+            }
+            else
+            {
+                ANGLE_TRY(texture->getTexImage(context, packState, nullptr, index.getTarget(),
+                                               index.getLevelIndex(), getFormat, getType,
+                                               texelsPtr->data()));
+                json->addBlob("Texels", texelsPtr->data(), texelsPtr->size());
+            }
         }
         else
         {
