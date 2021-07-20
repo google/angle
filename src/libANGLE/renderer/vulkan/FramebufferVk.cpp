0be285c6ee727a2fd7bb0e239e46693bf1958c7d
Jason Macnak <natsu@google.com>
2021-07-20 11:36:22 -0700

    Vulkan: Flush read RT if color attachment is the read buffer
    Prior to this change, the following sequence:
    
     * eglMakeCurrent(..., draw=surface1, read=surface1, ...);
     * glClear(...);
     * eglMakeCurrent(..., draw=surface2, read=surface1, ...);
     * glBlitFramebuffer(...);
     * eglMakeCurrent(..., draw=surface1, read=surface1, ...);
     * glReadPixels(...);
    
    would end up with the `vkCmdClearColorImage()` on surface1
    occuring after the `vkCmdBlitImage()`.
    
    This CL updates flushColorAttachmentUpdates() to flush any staged
    updates to both the read and draw attachments, since they can be
    different if different read and draw surfaces are bound.
    
    Adds a test which is a small repro of
    android.opengl.cts.FramebufferTest#testBlitFramebuffer failure.
    
    Bug: b/192327017
    Test: EGLSurfaceTest.BlitBetweenSurfaces/*
    Change-Id: Iabad26dfcd8633e9dcfcee2fb16ba352bc3931d5
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3078980
    Commit-Queue: Tim Van Patten <timvp@google.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -1735,22 +1735,47 @@ angle::Result FramebufferVk::flushColorAttachmentUpdates(const gl::Context *cont
                                                          bool deferClears,
                                                          uint32_t colorIndexGL)
 {
-    ContextVk *contextVk = vk::GetImpl(context);
+    ContextVk *contextVk             = vk::GetImpl(context);
+    RenderTargetVk *readRenderTarget = nullptr;
+    RenderTargetVk *drawRenderTarget = nullptr;
 
-    RenderTargetVk *renderTarget = mRenderTargetCache.getColors()[colorIndexGL];
-    if (renderTarget == nullptr)
+    // It's possible for the read and draw color attachments to be different if different surfaces
+    // are bound, so we need to flush any staged updates to both.
+
+    // Read
+    if (mState.getReadBufferState() != GL_NONE && mState.getReadIndex() == colorIndexGL)
+    {
+        readRenderTarget = mRenderTargetCache.getColorRead(mState);
+        if (readRenderTarget)
+        {
+            if (deferClears && mState.getEnabledDrawBuffers().test(colorIndexGL))
+            {
+                ANGLE_TRY(
+                    readRenderTarget->flushStagedUpdates(contextVk, &mDeferredClears, colorIndexGL,
+                                                         mCurrentFramebufferDesc.getLayerCount()));
+            }
+            else
+            {
+                ANGLE_TRY(readRenderTarget->flushStagedUpdates(
+                    contextVk, nullptr, 0, mCurrentFramebufferDesc.getLayerCount()));
+            }
+        }
+    }
+
+    // Draw
+    drawRenderTarget = mRenderTargetCache.getColorDraw(mState, colorIndexGL);
+    if (drawRenderTarget == nullptr || readRenderTarget == drawRenderTarget)
     {
         return angle::Result::Continue;
     }
-
     if (deferClears && mState.getEnabledDrawBuffers().test(colorIndexGL))
     {
-        return renderTarget->flushStagedUpdates(contextVk, &mDeferredClears, colorIndexGL,
-                                                mCurrentFramebufferDesc.getLayerCount());
+        return drawRenderTarget->flushStagedUpdates(contextVk, &mDeferredClears, colorIndexGL,
+                                                    mCurrentFramebufferDesc.getLayerCount());
     }
 
-    return renderTarget->flushStagedUpdates(contextVk, nullptr, 0,
-                                            mCurrentFramebufferDesc.getLayerCount());
+    return drawRenderTarget->flushStagedUpdates(contextVk, nullptr, 0,
+                                                mCurrentFramebufferDesc.getLayerCount());
 }
 
 angle::Result FramebufferVk::flushDepthStencilAttachmentUpdates(const gl::Context *context,
--- src/tests/egl_tests/EGLSurfaceTest.cpp
+++ src/tests/egl_tests/EGLSurfaceTest.cpp
@@ -898,6 +898,11 @@ TEST_P(EGLSurfaceTest3, MakeCurrentDifferentSurfaces)
 
     // Use the same surface for both draw and read
     EXPECT_EGL_TRUE(eglMakeCurrent(mDisplay, firstPbufferSurface, firstPbufferSurface, mContext));
+
+    // TODO(http://www.anglebug.com/6284): Failing with OpenGL ES backend on Android.
+    // Must be after the eglMakeCurrent() so the renderer string is initialized.
+    ANGLE_SKIP_TEST_IF(IsOpenGLES() && IsAndroid());
+
     glClearColor(GLColor::red.R, GLColor::red.G, GLColor::red.B, GLColor::red.A);
     glClear(GL_COLOR_BUFFER_BIT);
     ASSERT_GL_NO_ERROR();
@@ -1202,6 +1207,56 @@ TEST_P(EGLSurfaceTestD3D11, CreateSurfaceWithMSAA)
 
 #endif  // ANGLE_ENABLE_D3D11
 
+// Verify bliting between two surfaces works correctly.
+TEST_P(EGLSurfaceTest3, BlitBetweenSurfaces)
+{
+    initializeDisplay();
+    ASSERT_NE(mDisplay, EGL_NO_DISPLAY);
+
+    initializeSurfaceWithDefaultConfig(true);
+    ASSERT_NE(mWindowSurface, EGL_NO_SURFACE);
+    ASSERT_NE(mContext, EGL_NO_CONTEXT);
+
+    EGLSurface surface1;
+    EGLSurface surface2;
+
+    const EGLint surfaceAttributes[] = {
+        EGL_WIDTH, 64, EGL_HEIGHT, 64, EGL_NONE,
+    };
+
+    surface1 = eglCreatePbufferSurface(mDisplay, mConfig, surfaceAttributes);
+    ASSERT_EGL_SUCCESS();
+    surface2 = eglCreatePbufferSurface(mDisplay, mConfig, surfaceAttributes);
+    ASSERT_EGL_SUCCESS();
+
+    // Clear surface1.
+    EXPECT_EGL_TRUE(eglMakeCurrent(mDisplay, surface1, surface1, mContext));
+
+    // TODO(http://www.anglebug.com/6284): Failing with OpenGL ES backend on Android and Windows.
+    // Must be after the eglMakeCurrent() so the renderer string is initialized.
+    ANGLE_SKIP_TEST_IF(IsOpenGLES() && (IsAndroid() || IsWindows()));
+
+    glClearColor(GLColor::red.R, GLColor::red.G, GLColor::red.B, GLColor::red.A);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ASSERT_GL_NO_ERROR();
+
+    // Blit from surface1 to surface2.
+    EXPECT_EGL_TRUE(eglMakeCurrent(mDisplay, surface2, surface1, mContext));
+    glBlitFramebuffer(0, 0, 64, 64, 0, 0, 64, 64, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+    ASSERT_GL_NO_ERROR();
+
+    // Confirm surface1 has the clear color.
+    EXPECT_EGL_TRUE(eglMakeCurrent(mDisplay, surface1, surface1, mContext));
+    EXPECT_PIXEL_COLOR_EQ(32, 32, GLColor::red);
+
+    // Confirm surface2 has the blited clear color.
+    EXPECT_EGL_TRUE(eglMakeCurrent(mDisplay, surface2, surface2, mContext));
+    EXPECT_PIXEL_COLOR_EQ(32, 32, GLColor::red);
+
+    eglDestroySurface(mDisplay, surface1);
+    eglDestroySurface(mDisplay, surface2);
+}
+
 // Verify switching between a surface with robust resource init and one without still clears alpha.
 TEST_P(EGLSurfaceTest, RobustResourceInitAndEmulatedAlpha)
 {
@@ -1321,7 +1376,11 @@ ANGLE_INSTANTIATE_TEST(EGLFloatSurfaceTest,
                        WithNoFixture(ES3_VULKAN()));
 
 GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(EGLSurfaceTest3);
-ANGLE_INSTANTIATE_TEST(EGLSurfaceTest3, WithNoFixture(ES3_VULKAN()));
+ANGLE_INSTANTIATE_TEST(EGLSurfaceTest3,
+                       WithNoFixture(ES3_D3D11()),
+                       WithNoFixture(ES3_OPENGLES()),
+                       WithNoFixture(ES3_VULKAN()),
+                       WithNoFixture(ES3_VULKAN_SWIFTSHADER()));
 
 #if defined(ANGLE_ENABLE_D3D11)
 ANGLE_INSTANTIATE_TEST(EGLSurfaceTestD3D11, WithNoFixture(ES2_D3D11()), WithNoFixture(ES3_D3D11()));
