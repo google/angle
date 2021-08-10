8f214193940b2e7eda70746b7b42f96b0b4ce73b
Yuly Novikov <ynovikov@chromium.org>
2021-08-10 20:40:36 +0000

    Revert "Properly report xcb_connect failures in DisplayVkXcb"
    This reverts commit 81bee2e7f6deeda15691dd0b7fca90626b66bfb8.
    
    Reason for revert: fails on
    https://ci.chromium.org/ui/p/chromium/builders/try/linux-ozone-rel/918054/overview
    
    Original change's description:
    > Properly report xcb_connect failures in DisplayVkXcb
    >
    > Bug: chromium:1223431
    > Change-Id: Iaefddbda3fef283eed365fe010ee003a3b3beca8
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3083383
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    
    Bug: chromium:1223431
    Change-Id: I1da24861375a29f448e61771a609396e777c2e29
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3087555
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- src/libANGLE/renderer/vulkan/xcb/DisplayVkXcb.cpp
+++ src/libANGLE/renderer/vulkan/xcb/DisplayVkXcb.cpp
@@ -49,13 +49,8 @@ DisplayVkXcb::DisplayVkXcb(const egl::DisplayState &state)
 egl::Error DisplayVkXcb::initialize(egl::Display *display)
 {
     mXcbConnection = xcb_connect(nullptr, nullptr);
-    ASSERT(mXcbConnection != nullptr);
-    int xcb_connection_error = xcb_connection_has_error(mXcbConnection);
-    if (xcb_connection_error)
+    if (mXcbConnection == nullptr)
     {
-        ERR() << "xcb_connect() failed, error " << xcb_connection_error;
-        xcb_disconnect(mXcbConnection);
-        mXcbConnection = nullptr;
         return egl::EglNotInitialized();
     }
     return DisplayVk::initialize(display);
