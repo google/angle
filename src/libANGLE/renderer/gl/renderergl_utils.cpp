851ecf494a63a863cdc3ac125b14c61a92fa8f39
Geoff Lang <geofflang@google.com>
2021-09-08 14:40:41 -0400

    GL: Temporarily disable MSRTT on Android.
    This extension is causing validation errors and graphics corruption
    in Chrome. Disable it until the bugs are resolved.
    
    Bug: chromium:1238327
    Change-Id: I823303f2d75f2d6422cf5680a8751b9b79c8580b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3149573
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -2191,10 +2191,11 @@ void InitializeFeatures(const FunctionsGL *functions, angle::FeaturesGL *feature
     // http://crbug.com/594016
     bool isLinuxVivante = IsLinux() && IsVivante(device);
 
+    // Temporarily disable on all of Android. http://crbug.com/1238327
     ANGLE_FEATURE_CONDITION(features, disableMultisampledRenderToTexture,
-                            isAdreno4xxOnAndroidLessThan51 || isAdreno4xxOnAndroid70 ||
-                                isAdreno5xxOnAndroidLessThan70 || isAdreno5xxOnAndroid71 ||
-                                isLinuxVivante);
+                            IsAndroid() || isAdreno4xxOnAndroidLessThan51 ||
+                                isAdreno4xxOnAndroid70 || isAdreno5xxOnAndroidLessThan70 ||
+                                isAdreno5xxOnAndroid71 || isLinuxVivante);
 
     // http://crbug.com/1181068
     ANGLE_FEATURE_CONDITION(features, uploadTextureDataInChunks, IsApple());
