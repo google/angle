62c70b449d2ef5e195c583f7a7627f14587a51dc
Cody Northrop <cnorthrop@google.com>
2021-09-10 16:54:49 -0600

    Vulkan: Suppress CoreValidation-Shader-InterfaceTypeMismatch
    New VVL error causing test failures.
    
    Bug: angleproject:6382
    Change-Id: I61801828c0879a33502a2d9582e2e7da1bdb9250
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3155571
    Reviewed-by: Cody Northrop <cnorthrop@google.com>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -185,6 +185,8 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdClearAttachments-baseArrayLayer-00018",
     // http://anglebug.com/6355
     "VUID-vkCmdDraw-blendEnable-04727",
+    // http://anglebug.com/6382
+    "UNASSIGNED-CoreValidation-Shader-InterfaceTypeMismatch",
 };
 
 // Suppress validation errors that are known
