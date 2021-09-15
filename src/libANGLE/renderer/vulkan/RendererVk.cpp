fc9457fb45ee3a1ffbc169d4e90ffb82efbfa7b3
Cody Northrop <cnorthrop@google.com>
2021-09-15 10:46:55 -0600

    Revert "Vulkan: Suppress CoreValidation-Shader-InterfaceTypeMismatch"
    This reverts commit 62c70b449d2ef5e195c583f7a7627f14587a51dc
    
    Reason for revert: VVL bug fixed upstream
    https://github.com/KhronosGroup/Vulkan-ValidationLayers/pull/3264
    
    Original change's description:
    > Vulkan: Suppress CoreValidation-Shader-InterfaceTypeMismatch
    >
    > New VVL error causing test failures.
    >
    > Bug: angleproject:6382
    > Change-Id: I61801828c0879a33502a2d9582e2e7da1bdb9250
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3155571
    > Reviewed-by: Cody Northrop <cnorthrop@google.com>
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    
    Bug: angleproject:6382
    Change-Id: I41202a1d577fb6d9204ead32df134e6c5b3a1a86
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3163238
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Cody Northrop <cnorthrop@google.com>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -185,8 +185,6 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdClearAttachments-baseArrayLayer-00018",
     // http://anglebug.com/6355
     "VUID-vkCmdDraw-blendEnable-04727",
-    // http://anglebug.com/6382
-    "UNASSIGNED-CoreValidation-Shader-InterfaceTypeMismatch",
 };
 
 // Suppress validation errors that are known
