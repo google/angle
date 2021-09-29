85de22fde459f45da1854db98587f4b44d3ce9ce
Gregg Tavares <gman@chromium.org>
2021-09-29 13:27:42 -0700

    Set the viewport missing from a few tests
    A few places seem like they should be setting the viewport.
    Bascially any place that creates a framebuffer that's different
    size than the backbuffer (defined as kWidth, kHeight) should
    probably be setting the viewport.
    
    Bug: angleproject:6394
    Change-Id: Iee64f6700e3f93c236ab46d3d5debd47c49d2fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3195054
    Commit-Queue: Gregg Tavares <gman@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/tests/gl_tests/RobustResourceInitTest.cpp
+++ src/tests/gl_tests/RobustResourceInitTest.cpp
@@ -1532,6 +1532,7 @@ void RobustResourceInitTest::maskedDepthClear(ClearFunc clearFunc)
     // Draw red with a depth function that checks for the clear value.
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_EQUAL);
+    glViewport(0, 0, kSize, kSize);
 
     ANGLE_GL_PROGRAM(program, essl1_shaders::vs::Simple(), essl1_shaders::fs::Red());
 
@@ -1614,6 +1615,7 @@ void RobustResourceInitTest::maskedStencilClear(ClearFunc clearFunc)
     glEnable(GL_STENCIL_TEST);
     glStencilFunc(GL_EQUAL, 0x00, 0xFF);
     glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+    glViewport(0, 0, kSize, kSize);
 
     ANGLE_GL_PROGRAM(program, essl1_shaders::vs::Simple(), essl1_shaders::fs::Red());
 
@@ -2311,6 +2313,7 @@ TEST_P(RobustResourceInitTestES3, BlitDepthStencilAfterClearBuffer)
     ANGLE_SKIP_TEST_IF(IsOSX() && IsARM64() && IsMetal());
 
     constexpr GLsizei kSize = 16;
+    glViewport(0, 0, kSize, kSize);
 
     GLFramebuffer readFbo, drawFbo;
     GLRenderbuffer readDepthStencil, drawDepthStencil;
