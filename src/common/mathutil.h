fde87f5d3840de1ae2eb5826a7109f0ae90138ba
Mark Lobodzinski <mark@lunarg.com>
2021-10-06 15:10:09 -0600

    Fix negative fixed-point->float conversions
    The fixed-to-float conversion routine in mathutils only operated
    correctly on positive numbers.  This caused failures in some GLES1
    apps when negative fixed-point values were converted, as for
    glTranslatex().
    
    Bug: b/199919700
    Test: Manual -- Zillow app should show data on map
    Change-Id: I2ac4794478d3ad4c50057527134c9c17c8680338
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3209826
    Reviewed-by: Ian Elliott <ianelliott@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Mark Lobodzinski <mark@lunarg.com>
    Commit-Queue: Ian Elliott <ianelliott@google.com>

--- src/common/mathutil.h
+++ src/common/mathutil.h
@@ -469,9 +469,8 @@ inline float float10ToFloat32(unsigned short fp10)
     }
 }
 
-// Convers to and from float and 16.16 fixed point format.
-
-inline float ConvertFixedToFloat(uint32_t fixedInput)
+// Converts to and from float and 16.16 fixed point format.
+inline float ConvertFixedToFloat(int32_t fixedInput)
 {
     return static_cast<float>(fixedInput) / 65536.0f;
 }
