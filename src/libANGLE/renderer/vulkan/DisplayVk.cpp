26f9041b1f52a3a9367cb88a6dc5455222f0ab66
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2021-10-06 16:50:47 +0200

    DisplayVk: Always expose EGL_KHR_gl_colorspace.
    In the case of the sRGB color space the VK_EXT_swapchain_colorspace is
    actually not required to create a swap chain.
    
    The requirement should be lowered and VK_KHR_get_surface_capabilities2
    should be used to enumerate if the color spaces from
    VK_EXT_swapchain_colorspace are available.
    
    Bug: angleproject:2514
    Bug: b/201993125
    Change-Id: Id0d8be05016d3833d14395f6638b85110615164b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3208689
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/DisplayVk.cpp
+++ src/libANGLE/renderer/vulkan/DisplayVk.cpp
@@ -221,7 +221,7 @@ void DisplayVk::generateExtensions(egl::DisplayExtensions *outExtensions) const
     outExtensions->imageNativeBuffer =
         getRenderer()->getFeatures().supportsAndroidHardwareBuffer.enabled;
     outExtensions->surfacelessContext = true;
-    outExtensions->glColorspace = getRenderer()->getFeatures().supportsSwapchainColorspace.enabled;
+    outExtensions->glColorspace       = true;
     outExtensions->imageGlColorspace =
         outExtensions->glColorspace && getRenderer()->getFeatures().supportsImageFormatList.enabled;
 
