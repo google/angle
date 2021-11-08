b33796c345fb01c24dc54e63c831a4d236a680e5
Shahbaz Youssefi <syoussefi@chromium.org>
2021-11-08 16:27:20 -0500

    Canary: Fix invalidation of GL_FRAMEBUFFER invalidating READ FBO
    Per spec, GL_FRAMEBUFFER means GL_DRAW_FRAMEBUFFER for
    glInvalidateFramebuffer.
    
    Bug: chromium:1267424
    Change-Id: I8c9ab61ecdbd4ccee4262dc8559b2feb02b4837c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3266176
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3270996
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -4144,8 +4144,14 @@ ANGLE_INLINE angle::Result Context::prepareForDispatch()
 
 angle::Result Context::prepareForInvalidate(GLenum target)
 {
-    // Only sync the FBO that's being invalidated
-    ANGLE_TRY(mState.syncDirtyObject(this, target));
+    // Only sync the FBO that's being invalidated.  Per the GLES3 spec, GL_FRAMEBUFFER is equivalent
+    // to GL_DRAW_FRAMEBUFFER for the purposes of invalidation.
+    GLenum effectiveTarget = target;
+    if (effectiveTarget == GL_FRAMEBUFFER)
+    {
+        effectiveTarget = GL_DRAW_FRAMEBUFFER;
+    }
+    ANGLE_TRY(mState.syncDirtyObject(this, effectiveTarget));
     return syncDirtyBits(mInvalidateDirtyBits, Command::Invalidate);
 }
 
@@ -4790,13 +4796,9 @@ void Context::readBuffer(GLenum mode)
 
 void Context::discardFramebuffer(GLenum target, GLsizei numAttachments, const GLenum *attachments)
 {
-    Framebuffer *framebuffer = mState.getTargetFramebuffer(target);
-    ASSERT(framebuffer);
-
     // The specification isn't clear what should be done when the framebuffer isn't complete.
-    // We leave it up to the framebuffer implementation to decide what to do.
-    ANGLE_CONTEXT_TRY(prepareForInvalidate(target));
-    ANGLE_CONTEXT_TRY(framebuffer->discard(this, numAttachments, attachments));
+    // We threat it the same way as GLES3 glInvalidateFramebuffer.
+    invalidateFramebuffer(target, numAttachments, attachments);
 }
 
 void Context::invalidateFramebuffer(GLenum target,
