8f6f5a4bb28d4f9cef8b747c9cd384ebcebb2a6a
Shahbaz Youssefi <syoussefi@chromium.org>
2021-11-30 23:48:30 -0500

    Vulkan: Fix image respecify's usage tracking
    When respecifying an image due to mip level count changes, the previous
    image is staged as an update to the new image.  The resource usage info
    was not being transferred to the image being staged as an update,
    causing it to be prematurely deleted.
    
    Test based on one authored by sugoi@google.com.
    
    Bug: chromium:1270658
    Bug: angleproject:4835
    Change-Id: I215c65ba700d7be608d0910d3cb37fcfdf297a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3308921
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/renderer/vulkan/ResourceVk.cpp
+++ src/libANGLE/renderer/vulkan/ResourceVk.cpp
@@ -61,6 +61,12 @@ Resource::Resource(Resource &&other) : Resource()
     mUse = std::move(other.mUse);
 }
 
+Resource &Resource::operator=(Resource &&rhs)
+{
+    std::swap(mUse, rhs.mUse);
+    return *this;
+}
+
 Resource::~Resource()
 {
     mUse.release();
