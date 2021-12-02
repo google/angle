ce8b70863980d84e103c8d5f3ab6e267ae90701a
Lingfeng Yang <lfy@google.com>
2021-12-01 17:08:01 -0800

    M96: Validate SamplerFormat
    We weren't validating sampler formats in ProgramExecutable validation.
    
    Bug: chromium:1273661
    Change-Id: Ida0c67c0c7169ea3f47ceb2d433bee17012a7e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3312717
    Reviewed-by: Charlie Lao <cclao@google.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Lingfeng Yang <lfy@google.com>
    (cherry picked from commit 6d3435fddd7abd67699c3f020d6b4fa21445d9b3)
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3335173
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/tests/gl_tests/TextureTest.cpp
+++ src/tests/gl_tests/TextureTest.cpp
@@ -9907,8 +9907,6 @@ class TextureChangeStorageUploadTest : public ANGLETest
             FAIL() << "shader compilation failed.";
         }
 
-        mColorLocation = glGetUniformLocation(mProgram, essl1_shaders::ColorUniform());
-
         glUseProgram(mProgram);
 
         glClearColor(0, 0, 0, 0);
@@ -9951,6 +9949,53 @@ TEST_P(TextureChangeStorageUploadTest, Basic)
     glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, kSmallerImageSize / 2, kSmallerImageSize / 2, GL_RGBA,
                     GL_UNSIGNED_BYTE, kColor.data());
     EXPECT_GL_NO_ERROR();
+}
+
+class ExtraSamplerCubeShadowUseTest : public ANGLETest
+{
+  protected:
+    ExtraSamplerCubeShadowUseTest() : ANGLETest() {}
+
+    const char *getVertexShaderSource() { return "#version 300 es\nvoid main() {}"; }
+
+    const char *getFragmentShaderSource()
+    {
+        return R"(#version 300 es
+precision mediump float;
+
+uniform mediump samplerCube var_0002; // this has to be there
+uniform highp samplerCubeShadow var_0004; // this has to be a cube shadow sampler
+out vec4 color;
+void main() {
+
+    vec4 var_0031 = texture(var_0002, vec3(1,1,1));
+    ivec2 size = textureSize(var_0004, 0) ;
+    var_0031.x += float(size.y);
+
+    color = var_0031;
+})";
+    }
+
+    void testSetUp() override
+    {
+        mProgram = CompileProgram(getVertexShaderSource(), getFragmentShaderSource());
+        if (mProgram == 0)
+        {
+            FAIL() << "shader compilation failed.";
+        }
+        glUseProgram(mProgram);
+        ASSERT_GL_NO_ERROR();
+    }
+
+    void testTearDown() override { glDeleteProgram(mProgram); }
+
+    GLuint mProgram;
+};
+
+TEST_P(ExtraSamplerCubeShadowUseTest, Basic)
+{
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
+    EXPECT_GL_ERROR(GL_INVALID_OPERATION);
 }
 
 // Use this to select which configurations (e.g. which renderer, which GLES major version) these
@@ -10067,5 +10112,7 @@ GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(CopyImageTestES31);
 ANGLE_INSTANTIATE_TEST_ES31_AND(CopyImageTestES31, WithDirectSPIRVGeneration(ES31_VULKAN()));
 
 ANGLE_INSTANTIATE_TEST_ES3(TextureChangeStorageUploadTest);
+
+ANGLE_INSTANTIATE_TEST_ES3(ExtraSamplerCubeShadowUseTest);
 
 }  // anonymous namespace
