50d8a2af9721f34330fe80cd34e51fed5072b08c
Brian Sheedy <bsheedy@chromium.org>
2021-12-06 11:35:08 -0800

    Remove Skia Gold master usage
    Removes _GetGitOriginMasterHeadSha1 in the Skia Gold Python
    code since _GetGitOriginMainHeadSha1 is now used in the base
    Chromium code.
    
    Bug: chromium:1276531
    Change-Id: I8f5b793b21ba6ff0e4110a3259becc2aaec06443
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3318581
    Commit-Queue: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>

--- src/tests/skia_gold/angle_skia_gold_properties.py
+++ src/tests/skia_gold/angle_skia_gold_properties.py
@@ -21,15 +21,6 @@ from skia_gold_common import skia_gold_properties
 class ANGLESkiaGoldProperties(skia_gold_properties.SkiaGoldProperties):
 
     @staticmethod
-    def _GetGitOriginMasterHeadSha1():
-        try:
-            return subprocess.check_output(['git', 'rev-parse', 'origin/master'],
-                                           shell=_IsWin(),
-                                           cwd=ANGLE_SRC_DIR).strip()
-        except subprocess.CalledProcessError:
-            return None
-
-    @staticmethod
     def _GetGitOriginMainHeadSha1():
         try:
             return subprocess.check_output(['git', 'rev-parse', 'origin/main'],
