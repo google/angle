83679716694f7403298057a72c204c1db38cfe4a
Lingfeng Yang <lfy@google.com>
2021-12-14 19:02:48 -0800

    Vulkan: Dirty vertex buffers if default attrib values changed
    In the call_break_offline trace, attribute index 4 is disabled then
    enabled before a draw. This redundant operation is currently not blocked
    and makes it to the Vulkan backend, resulting in
    handleDirtyGraphicsVertexBuffers. However, at the same time, we change
    the default attrib value for index 3. Which makes
    handleDirtyGraphicsVertexBuffers not redundant. We'll need to take into
    account cases when other attribute indices are not dirty and yet the
    default attrib value changes.
    
    Bug: angleproject:6802
    Change-Id: I4a3ec411aa42f73a209fd0f06976d7ef2d21e027
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3331412
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Charlie Lao <cclao@google.com>
    Commit-Queue: Lingfeng Yang <lfy@google.com>

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -5785,6 +5785,7 @@ void ContextVk::invalidateDefaultAttributes(const gl::AttributesMask &dirtyMask)
     {
         mDirtyDefaultAttribsMask |= dirtyMask;
         mGraphicsDirtyBits.set(DIRTY_BIT_DEFAULT_ATTRIBS);
+        mGraphicsDirtyBits.set(DIRTY_BIT_VERTEX_BUFFERS);
     }
 }
 
