706824d837a2dd0d2be8407d927f392ec1469781
Ian Elliott <ianelliott@google.com>
2022-03-22 16:13:17 -0600

    Remove "libcxx_abi_unstable" from "roll_aosp.sh"
    This was removed as a gn arg by: crrev/c/3520025.  That broke the
    ANGLE->AOSP autoroller.
    
    Bug: angleproject:7110
    Change-Id: I3d8b4483b75fb168e91078ff5ee0a53298d98efb
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3542990
    Commit-Queue: Ian Elliott <ianelliott@google.com>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -70,9 +70,6 @@ function generate_Android_bp_file() {
             # Disable histogram/protobuf support
             "angle_has_histograms = false"
 
-            # Disable _LIBCPP_ABI_UNSTABLE, since it breaks std::string
-            "libcxx_abi_unstable = false"
-
             # rapidJSON is used for ANGLE's frame capture (among other things), which is unnecessary for AOSP builds.
             "angle_has_rapidjson = false"
         )
