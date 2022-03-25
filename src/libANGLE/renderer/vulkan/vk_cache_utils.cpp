3bdbfbf868e365e740d39aebc3b27465f9ff0991
Mohan Maiya <m.maiya@samsung.com>
2022-03-25 16:34:51 -0700

    Vulkan: Adjust border color
    Some border color tests used to fail due to either unclamped color
    values or not accounting for depth, stencil or luma formats. We now
    adjust the border color value according to the sampler's format.
    
    Test: dEQP-GLES31.functional.texture.border_clamp.*
    Bug: angleproject:3577
    Bug: angleproject:6213
    Change-Id: Ib38ce2374622bfafde69fe3fa2d7227d60043954
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3551895
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -1451,6 +1451,82 @@ void InitializeSpecializationInfo(
     specializationInfoOut->pData         = &specConsts;
 }
 
+// Adjust border color value according to intended format.
+gl::ColorGeneric AdjustBorderColor(const angle::ColorGeneric &borderColorGeneric,
+                                   const angle::Format &format)
+{
+    gl::ColorGeneric adjustedBorderColor = borderColorGeneric;
+
+    // Handle depth formats
+    if (format.hasDepthOrStencilBits())
+    {
+        if (format.depthBits > 0 && format.isUnorm())
+        {
+            // D or D/S formats
+            adjustedBorderColor.colorF.red = gl::clamp01(borderColorGeneric.colorF.red);
+        }
+
+        return adjustedBorderColor;
+    }
+
+    // Handle LUMA formats
+    if (format.isLUMA() && format.alphaBits > 0)
+    {
+        if (format.luminanceBits > 0)
+        {
+            adjustedBorderColor.colorF.green = borderColorGeneric.colorF.alpha;
+        }
+        else
+        {
+            adjustedBorderColor.colorF.red = borderColorGeneric.colorF.alpha;
+        }
+
+        return adjustedBorderColor;
+    }
+
+    // Handle all other formats
+    if (format.isSint())
+    {
+        adjustedBorderColor.colorI.red =
+            gl::clampForBitCount<int>(borderColorGeneric.colorI.red, format.redBits);
+        adjustedBorderColor.colorI.green =
+            gl::clampForBitCount<int>(borderColorGeneric.colorI.green, format.greenBits);
+        adjustedBorderColor.colorI.blue =
+            gl::clampForBitCount<int>(borderColorGeneric.colorI.blue, format.blueBits);
+        adjustedBorderColor.colorI.alpha =
+            gl::clampForBitCount<int>(borderColorGeneric.colorI.alpha, format.alphaBits);
+    }
+    else if (format.isUint())
+    {
+        adjustedBorderColor.colorUI.red =
+            gl::clampForBitCount<unsigned int>(borderColorGeneric.colorUI.red, format.redBits);
+        adjustedBorderColor.colorUI.green =
+            gl::clampForBitCount<unsigned int>(borderColorGeneric.colorUI.green, format.greenBits);
+        adjustedBorderColor.colorUI.blue =
+            gl::clampForBitCount<unsigned int>(borderColorGeneric.colorUI.blue, format.blueBits);
+        adjustedBorderColor.colorUI.alpha =
+            gl::clampForBitCount<unsigned int>(borderColorGeneric.colorUI.alpha, format.alphaBits);
+    }
+    else if (format.isSnorm())
+    {
+        // clamp between -1.0f and 1.0f
+        adjustedBorderColor.colorF.red   = gl::clamp(borderColorGeneric.colorF.red, -1.0f, 1.0f);
+        adjustedBorderColor.colorF.green = gl::clamp(borderColorGeneric.colorF.green, -1.0f, 1.0f);
+        adjustedBorderColor.colorF.blue  = gl::clamp(borderColorGeneric.colorF.blue, -1.0f, 1.0f);
+        adjustedBorderColor.colorF.alpha = gl::clamp(borderColorGeneric.colorF.alpha, -1.0f, 1.0f);
+    }
+    else if (format.isUnorm())
+    {
+        // clamp between 0.0f and 1.0f
+        adjustedBorderColor.colorF.red   = gl::clamp01(borderColorGeneric.colorF.red);
+        adjustedBorderColor.colorF.green = gl::clamp01(borderColorGeneric.colorF.green);
+        adjustedBorderColor.colorF.blue  = gl::clamp01(borderColorGeneric.colorF.blue);
+        adjustedBorderColor.colorF.alpha = gl::clamp01(borderColorGeneric.colorF.alpha);
+    }
+
+    return adjustedBorderColor;
+}
+
 // Utility for setting a value on a packed 4-bit integer array.
 template <typename SrcT>
 void Int4Array_Set(uint8_t *arrayBytes, uint32_t arrayIndex, SrcT value)
@@ -3407,13 +3483,11 @@ void SamplerDesc::update(ContextVk *contextVk,
     mBorderColorType =
         (samplerState.getBorderColor().type == angle::ColorGeneric::Type::Float) ? 0 : 1;
 
+    // Adjust border color according to intended format
     const vk::Format &vkFormat = contextVk->getRenderer()->getFormat(intendedFormatID);
-    mBorderColor               = samplerState.getBorderColor().colorF;
-    if (vkFormat.getIntendedFormatID() != angle::FormatID::NONE)
-    {
-        LoadTextureBorderFunctionInfo loadFunction = vkFormat.getTextureBorderLoadFunctions();
-        loadFunction.loadFunction(mBorderColor);
-    }
+    gl::ColorGeneric adjustedBorderColor =
+        AdjustBorderColor(samplerState.getBorderColor(), vkFormat.getIntendedFormat());
+    mBorderColor = adjustedBorderColor.colorF;
 
     mReserved = 0;
 }
