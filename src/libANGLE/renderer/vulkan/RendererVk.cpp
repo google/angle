657738e04fb114b1409a9d2be746bd4a54d41a5e
Shahbaz Youssefi <syoussefi@chromium.org>
2022-03-29 19:27:34 +0000

    Vulkan: Remove suppression of fixed VVL errors
    This partially reverts commit
    91667dcafacc6d2bec545b776547e8583393a3f9.
    
    Bug: chromium:1310536
    Change-Id: Ifeca55466f74b080274eb7762edce17bcace0aab
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3557816
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -192,11 +192,6 @@ constexpr const char *kSkippedMessages[] = {
     // http://anglebug.com/7105
     "VUID-vkCmdDraw-None-06538",
     "VUID-vkCmdDrawIndexed-None-06538",
-    // http://crbug.com/1310536
-    "VUID-VkPresentInfoKHR-pImageIndices-01296",
-    "UNASSIGNED-CoreValidation-DrawState-QueueForwardProgress",
-    "VUID-vkAcquireNextImageKHR-swapchain-01802",
-    "VUID-VkPresentInfoKHR-pImageIndices-01430",
 };
 
 struct SkippedSyncvalMessage
