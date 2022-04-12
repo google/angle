dd493b5ca1f96e3a0ec1198e3882a92314320f35
Steven Noonan <steven@valvesoftware.com>
2022-04-11 17:49:36 -0700

    EGL: allow eglQueryString(EGL_NO_DISPLAY, EGL_VERSION)
    According to the EGL 1.5 specification, this should be okay:
    
        If dpy is EGL_NO_DISPLAY, then the EGL_VERSION string describes the
        supported client version. If dpy is a valid, initialized display,
        then the EGL_VERSION string describes the supported EGL version for
        dpy.
    
    Bug: angleproject:7194
    Change-Id: I0d851742695293d553465175ce4b59ce7f5d5336
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3583143
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Steven Noonan <steven@valvesoftware.com>
    Auto-Submit: Steven Noonan <steven@valvesoftware.com>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>

--- src/libANGLE/validationEGL.cpp
+++ src/libANGLE/validationEGL.cpp
@@ -6270,7 +6270,11 @@ bool ValidateGetProcAddress(const ValidationContext *val, const char *procname)
 
 bool ValidateQueryString(const ValidationContext *val, const Display *dpyPacked, EGLint name)
 {
-    if (name != EGL_EXTENSIONS || dpyPacked != nullptr)
+    // The only situation where EGL_NO_DISPLAY is allowed is when querying
+    // EGL_EXTENSIONS or EGL_VERSION.
+    const bool canQueryWithoutDisplay = (name == EGL_VERSION || name == EGL_EXTENSIONS);
+
+    if (dpyPacked != nullptr || !canQueryWithoutDisplay)
     {
         ANGLE_VALIDATION_TRY(ValidateDisplay(val, dpyPacked));
     }
