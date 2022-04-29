5b4f180cdbb863853d776e6278de83b6dfd9062d
Antonio Caggiano <antonio.caggiano@collabora.com>
2022-04-29 16:56:34 +0200

    Fix formatting
    Unrelated changes created by running "git cl format"
    
    Bug: None
    Change-Id: I880c7d50aead70353f22502d953efa7aa60ff5d5
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3616882
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Jamie Madill <jmadill@chromium.org>

--- src/libANGLE/Caps.cpp
+++ src/libANGLE/Caps.cpp
@@ -940,9 +940,9 @@ void TypePrecision::get(GLint *returnRange, GLint *returnPrecision) const
     *returnPrecision = precision;
 }
 
-Caps::Caps()                  = default;
-Caps::Caps(const Caps &other) = default;
-Caps::~Caps()                 = default;
+Caps::Caps()                             = default;
+Caps::Caps(const Caps &other)            = default;
+Caps::~Caps()                            = default;
 Caps &Caps::operator=(const Caps &other) = default;
 
 Caps GenerateMinimumCaps(const Version &clientVersion, const Extensions &extensions)
