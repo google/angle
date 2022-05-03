9865ed8b8117a083eb138247bbcb029a6bb0f2e4
Doug Horn <doughorn@google.com>
2022-05-03 16:06:56 -0700

    Check in //build files needed.
    Previously, only //third_party was actually `git add`ed.
    This change checks in the build files needed to access
    test scripting.
    
    Test: ./scripts/roll_aosp.sh
       m -j32 ANGLE
    
    Bug: b/231365398
    Change-Id: Ib4bf75bc96e089bf7fe5d7b4d2304829c293c8ea
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3625833
    Commit-Queue: Doug Horn <doughorn@google.com>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -121,6 +121,11 @@ third_party_deps=(
     "third_party/zlib"
 )
 
+root_add_deps=(
+  "build"
+  "third_party"
+)
+
 # Only add the parts of NDK and vulkan-deps that are required by ANGLE. The entire dep is too large.
 delete_only_deps=(
     "third_party/vulkan-deps"
@@ -158,6 +163,8 @@ for dep in "${third_party_deps[@]}"; do
 done
 
 extra_removal_files=(
+   # build/linux is hundreds of megs that aren't needed.
+   "build/linux"
    # Remove Android.mk files to prevent automated CLs:
    #   "[LSC] Add LOCAL_LICENSE_KINDS to external/angle"
    "Android.mk"
@@ -168,11 +175,13 @@ extra_removal_files=(
 )
 
 for removal_file in "${extra_removal_files[@]}"; do
-   rm -f "$removal_file"
+   rm -rf "$removal_file"
 done
 
 # Add all changes to third_party/ so we delete everything not explicitly allowed.
-git add -f "third_party/*"
+for root_add_dep in "${root_add_deps[@]}"; do
+git add -f "$root_add_dep/*"
+done
 
 # Done with depot_tools
 rm -rf $DEPOT_TOOLS_DIR
