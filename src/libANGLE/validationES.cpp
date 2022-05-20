8291f2d3e72eca3a25826b689a013c292ef4ba94
Alexey Knyazev <lexa.knyazev@gmail.com>
2022-05-20 00:00:00 +0000

    Fix ValidCompressedSubImageSize for 3D uploads
    Add CompressedTexSubImage3DValidation* tests
    
    Bug: angleproject:4056
    Change-Id: I088273b6349729fadf2c21ca7b349e44dc9ce5f3
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3657408
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Alexey Knyazev <lexa.knyazev@gmail.com>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/validationES.cpp
+++ src/libANGLE/validationES.cpp
@@ -1159,9 +1159,12 @@ bool ValidCompressedSubImageSize(const Context *context,
         return false;
     }
 
-    bool fillsEntireMip =
-        xoffset == 0 && yoffset == 0 && static_cast<size_t>(width) == textureWidth &&
-        static_cast<size_t>(height) == textureHeight && static_cast<size_t>(depth) == textureDepth;
+    // ANGLE does not support compressed 3D blocks (provided exclusively by ASTC 3D formats), so
+    // there is no need to check the depth here. Only width and height determine whether a 2D array
+    // element or a 2D slice of a sliced 3D texture fill the entire level.
+    bool fillsEntireMip = xoffset == 0 && yoffset == 0 &&
+                          static_cast<size_t>(width) == textureWidth &&
+                          static_cast<size_t>(height) == textureHeight;
 
     if (CompressedFormatRequiresWholeImage(internalFormat))
     {
