ea3bd30a6c75e1883cf76ba7a477cfe42770d6d3
Gert Wollny <gert.wollny@collabora.com>
2022-06-13 11:44:45 +0200

    Capture/Replay: Don't capture tex levels that are out-of-range
    Bug: angleproject:6334
    Change-Id: I01cb1920154b5fba9554180d2edf5be9dea2b7a2
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3702351
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Gert Wollny <gert.wollny@collabora.com>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- src/libANGLE/capture/frame_capture_utils.cpp
+++ src/libANGLE/capture/frame_capture_utils.cpp
@@ -1207,6 +1207,11 @@ Result SerializeTextureData(JsonSerializer *json,
     {
         gl::ImageIndex index = imageIter.next();
 
+        // Skip serializing level data if the level index is out of range
+        GLuint levelIndex = index.getLevelIndex();
+        if (levelIndex > texture->getMipmapMaxLevel() || levelIndex < texture->getBaseLevel())
+            continue;
+
         const gl::ImageDesc &desc = texture->getTextureState().getImageDesc(index);
 
         if (desc.size.empty())
