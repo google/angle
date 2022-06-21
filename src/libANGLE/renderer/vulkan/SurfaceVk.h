ef60d38ecf12d6663e9ecccacbff9803b1fba7c6
Mohan Maiya <m.maiya@samsung.com>
2022-06-21 12:05:28 -0700

    Vulkan: Support EGL_ANDROID_front_buffer_auto_refresh
    Cache value of EGL_FRONT_BUFFER_AUTO_REFRESH_ANDROID attribute
    and set swapchain present mode accordingly when recreating
    swapchain in single buffer mode.
    
    Bug: angleproject:7224
    Tests: EGLAndroidAutoRefreshTest.Basic*
    Change-Id: I2dbb92ce5c3fa047e0b02ea9011725311f346027
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3707570
    Reviewed-by: Ian Elliott <ianelliott@google.com>
    Commit-Queue: mohan maiya <m.maiya@samsung.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>

--- src/libANGLE/renderer/vulkan/SurfaceVk.h
+++ src/libANGLE/renderer/vulkan/SurfaceVk.h
@@ -270,13 +270,22 @@ class WindowSurfaceVk : public SurfaceVk
         return mPreTransform;
     }
 
+    egl::Error setAutoRefreshEnabled(bool enabled) override;
+
     egl::Error getBufferAge(const gl::Context *context, EGLint *age) override;
 
     egl::Error setRenderBuffer(EGLint renderBuffer) override;
 
     bool isSharedPresentMode() const
     {
-        return (mSwapchainPresentMode == vk::PresentMode::SharedDemandRefreshKHR);
+        return (mSwapchainPresentMode == vk::PresentMode::SharedDemandRefreshKHR ||
+                mSwapchainPresentMode == vk::PresentMode::SharedContinuousRefreshKHR);
+    }
+
+    bool isSharedPresentModeDesired() const
+    {
+        return (mDesiredSwapchainPresentMode == vk::PresentMode::SharedDemandRefreshKHR ||
+                mDesiredSwapchainPresentMode == vk::PresentMode::SharedContinuousRefreshKHR);
     }
 
     egl::Error lockSurface(const egl::Display *display,
@@ -322,7 +331,7 @@ class WindowSurfaceVk : public SurfaceVk
     VkResult acquireNextSwapchainImage(vk::Context *context);
     // This method is called when a swapchain image is presented.  It schedules
     // acquireNextSwapchainImage() to be called later.
-    void deferAcquireNextImage(const gl::Context *context);
+    void deferAcquireNextImage();
     // Called when a swapchain image whose acquisition was deferred must be acquired.  This method
     // will recreate the swapchain (if needed) and call the acquireNextSwapchainImage() method.
     angle::Result doDeferredAcquireNextImage(const gl::Context *context, bool presentOutOfDate);
