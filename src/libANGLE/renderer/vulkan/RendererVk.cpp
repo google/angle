ae971cfe57bd5ce5c6e1954e993543eeca56192f
Mohan Maiya <m.maiya@samsung.com>
2022-09-07 17:00:17 -0700

    Vulkan: Expose wide gamut and HDR EGL colorspace extensions
    Query the underlying Vulkan ICD for the list of supported colorspaces
    on platforms that support VK_GOOGLE_surfaceless_query extension.
    Expose corresponding EGL colorspace extensions.
    
    Bug: angleproject:7630
    Change-Id: If9843ee55100fff5a8cec4ecf2ff27c746c5b205
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3873767
    Reviewed-by: Trevor Black <vantablack@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: mohan maiya <m.maiya@samsung.com>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1548,8 +1548,10 @@ angle::Result RendererVk::initialize(DisplayVk *displayVk,
 
     if (ExtensionFound(VK_GOOGLE_SURFACELESS_QUERY_EXTENSION_NAME, instanceExtensionNames))
     {
+        // TODO: Validation layer has a bug when vkGetPhysicalDeviceSurfaceFormats2KHR is called
+        // on Mock ICD with surface handle set as VK_NULL_HANDLE. http://anglebug.com/7631
         mEnabledInstanceExtensions.push_back(VK_GOOGLE_SURFACELESS_QUERY_EXTENSION_NAME);
-        ANGLE_FEATURE_CONDITION(&mFeatures, supportsSurfacelessQueryExtension, true);
+        ANGLE_FEATURE_CONDITION(&mFeatures, supportsSurfacelessQueryExtension, !isMockICDEnabled());
     }
 
     // Verify the required extensions are in the extension names set. Fail if not.
