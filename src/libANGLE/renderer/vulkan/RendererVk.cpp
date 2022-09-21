41de8a02dcffaa023398891ee007052b20816a61
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-09-21 09:08:12 -0700

    Suppress VVL error about input attachment desc
    * Suppressed the following VVL:
      * UNASSIGNED-input-attachment-descriptor-not-in-subpass
    
    Bug: angleproject:7685
    Change-Id: I8a23f8d4ddbc91df791f5c98b4ce1e11278b7cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3910796
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Auto-Submit: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -217,6 +217,8 @@ constexpr const char *kSkippedMessages[] = {
     "UNASSIGNED-BestPractices-vkCmdBeginRenderPass-ClearValueWithoutLoadOpClear",
     // http://anglebug.com/7513
     "VUID-VkGraphicsPipelineCreateInfo-pStages-06896",
+    // http://anglebug.com/7685
+    "UNASSIGNED-input-attachment-descriptor-not-in-subpass",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
