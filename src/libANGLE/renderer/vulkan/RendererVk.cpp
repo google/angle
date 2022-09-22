0d34d4f8810aeb2297bf2814f8b1ca7f749decd9
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-09-21 18:51:04 -0700

    Suppress more VVL errors about vkCmdDraw*-None
    * Suppressed the following VVLs:
      * VUID-vkCmdDrawIndexed-None-02686
      * VUID-vkCmdDrawIndirect-None-02686
      * VUID-vkCmdDrawIndirectCount-None-02686
      * VUID-vkCmdDrawIndexedIndirect-None-02686
      * VUID-vkCmdDrawIndexedIndirectCount-None-02686
    
    Bug: angleproject:7685
    Change-Id: I41390ee2a2a01509367a6e98697da3ad7e3cc2e5
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3910174
    Auto-Submit: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -220,6 +220,11 @@ constexpr const char *kSkippedMessages[] = {
     // http://anglebug.com/7685
     "UNASSIGNED-input-attachment-descriptor-not-in-subpass",
     "VUID-vkCmdDraw-None-02686",
+    "VUID-vkCmdDrawIndexed-None-02686",
+    "VUID-vkCmdDrawIndirect-None-02686",
+    "VUID-vkCmdDrawIndirectCount-None-02686",
+    "VUID-vkCmdDrawIndexedIndirect-None-02686",
+    "VUID-vkCmdDrawIndexedIndirectCount-None-02686",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
