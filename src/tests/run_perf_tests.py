d0a024f9c750394213694fce66daea405b45c0ed
Jamie Madill <jmadill@chromium.org>
2022-10-09 16:47:33 -0400

    Lower perf tests resource usage further.
    This reduces the warmup trials from 2->1 and the trial time
    from 5s -> 4s.
    
    Old: (2 warmup + 3 trials) * 5s / trial ~= 25s per sample
    New: (1 warmup + 3 trials) * 4s / trial ~= 16s per sample
    
    The tests should run in about 2/3 the time as it took prior.
    
    A future change will likely have to split up the tests so we
    can run shorter shards in sequential groupings between tests.
    
    Bug: angleproject:7738
    Change-Id: Ic6c744be838174b61f4eb832189ccad14f89103d
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3939917
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Jamie Madill <jmadill@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- src/tests/run_perf_tests.py
+++ src/tests/run_perf_tests.py
@@ -44,8 +44,8 @@ DEFAULT_TRIALS = 3
 DEFAULT_MAX_ERRORS = 3
 
 # These parameters condition the test warmup to stabilize the scores across runs.
-DEFAULT_WARMUP_TRIALS = 2
-DEFAULT_TRIAL_TIME = 5
+DEFAULT_WARMUP_TRIALS = 1
+DEFAULT_TRIAL_TIME = 4
 
 # Test expectations
 FAIL = 'FAIL'
