046db15c02a4231c5cfccf29b6bcdff43149e1e4
Shahbaz Youssefi <syoussefi@chromium.org>
2023-01-16 15:25:33 -0500

    Vulkan: Remove redundant ImageState::sourceType
    It was the same as ImageState::target
    
    Bug: angleproject:7962
    Change-Id: I0ed95deaa23087abbe975a99304bae6ea515ab7f
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4168455
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/Image.h
+++ src/libANGLE/Image.h
@@ -148,7 +148,6 @@ struct ImageState : private angle::NonCopyable
     gl::Extents size;
     size_t samples;
     GLuint levelCount;
-    EGLenum sourceType;
     EGLenum colorspace;
     bool hasProtectedContent;
 
