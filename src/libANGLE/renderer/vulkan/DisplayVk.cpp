b925c43e9b4a9cc654bbb679f6ac921487079b2f
Yuly Novikov <ynovikov@chromium.org>
2023-02-03 21:12:48 +0000

    Revert "Vulkan: Remove platform restriction of EGL_ANDROID_native_fence_sync"
    This reverts commit f852187dfbb094a981331a584d65dc5a4069644e.
    
    Reason for revert: Suspect for crashes on Fuchsia
    https://chromium-review.googlesource.com/c/chromium/src/+/4221336
    https://cr-buildbucket.appspot.com/build/8790151118736391057
    
    Original change's description:
    > Vulkan: Remove platform restriction of EGL_ANDROID_native_fence_sync
    >
    > Remove platform restriction of EGL_ANDROID_native_fence_sync
    > extension except for Mac OS.
    >
    > Bug: angleproject:7834
    > Change-Id: I116f44d8baca9cdcdd90013ca173b04461d06ae1
    > Signed-off-by: Sungyong Choi <sywow.choi@samsung.com>
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4027707
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    > Reviewed-by: mohan maiya <m.maiya@samsung.com>
    > Commit-Queue: mohan maiya <m.maiya@samsung.com>
    
    Bug: angleproject:7834
    Change-Id: I2b7381fd1702be578014539cbc96bfa7ace9c295
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4218602
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- src/libANGLE/renderer/vulkan/DisplayVk.cpp
+++ src/libANGLE/renderer/vulkan/DisplayVk.cpp
@@ -504,8 +504,10 @@ void DisplayVk::generateExtensions(egl::DisplayExtensions *outExtensions) const
     outExtensions->contextPriority = !getRenderer()->getFeatures().allocateNonZeroMemory.enabled;
     outExtensions->noConfigContext = true;
 
+#if defined(ANGLE_PLATFORM_ANDROID)
     outExtensions->nativeFenceSyncANDROID =
         getRenderer()->getFeatures().supportsAndroidNativeFenceSync.enabled;
+#endif  // defined(ANGLE_PLATFORM_ANDROID)
 
 #if defined(ANGLE_PLATFORM_GGP)
     outExtensions->ggpStreamDescriptor = true;
--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -3650,9 +3650,10 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
         &mFeatures, supportsExternalFenceFd,
         ExtensionFound(VK_KHR_EXTERNAL_FENCE_FD_EXTENSION_NAME, deviceExtensionNames));
 
-    if (!IsMac() && ((mFeatures.supportsExternalFenceCapabilities.enabled &&
-                      mFeatures.supportsExternalSemaphoreCapabilities.enabled) ||
-                     mPhysicalDeviceProperties.apiVersion >= VK_MAKE_VERSION(1, 1, 0)))
+#if defined(ANGLE_PLATFORM_ANDROID)
+    if ((mFeatures.supportsExternalFenceCapabilities.enabled &&
+         mFeatures.supportsExternalSemaphoreCapabilities.enabled) ||
+        mPhysicalDeviceProperties.apiVersion >= VK_MAKE_VERSION(1, 1, 0))
     {
         VkExternalFenceProperties externalFenceProperties = {};
         externalFenceProperties.sType = VK_STRUCTURE_TYPE_EXTERNAL_FENCE_PROPERTIES;
@@ -3687,6 +3688,7 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
                                 (mFeatures.supportsExternalFenceFd.enabled &&
                                  mFeatures.supportsExternalSemaphoreFd.enabled));
     }
+#endif  // defined(ANGLE_PLATFORM_ANDROID)
 
     ANGLE_FEATURE_CONDITION(
         &mFeatures, supportsShaderStencilExport,
