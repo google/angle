995db1f66bcf87fc9e47d908fb2a885e810d2567
Dave Tapuska <dtapuska@chromium.org>
2023-02-08 10:40:08 -0500

    [ios] Fix build failure on iOS.
    In CL https://chromium-review.googlesource.com/c/angle/angle/+/4217712
    a switch was added that is not really supported on iOS but will cause
    an error with -Wswitch. Return MTLResourceStorageModeShared for that
    code path.
    
    Bug: angleproject:7975
    Change-Id: I68e326da4a733892d9282460c3f57e0aac58df08
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4233079
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>

--- src/libANGLE/renderer/metal/mtl_resources.mm
+++ src/libANGLE/renderer/metal/mtl_resources.mm
@@ -78,6 +78,12 @@ MTLResourceOptions resourceOptionsForStorageMode(MTLStorageMode storageMode)
             return MTLResourceStorageModePrivate;
         case MTLStorageModeMemoryless:
             return MTLResourceStorageModeMemoryless;
+#if TARGET_OS_SIMULATOR
+        default:
+            // TODO(http://anglebug.com/8012): Remove me once hacked SDKs are fixed.
+            UNREACHABLE();
+            return MTLResourceStorageModeShared;
+#endif
     }
 }
 
