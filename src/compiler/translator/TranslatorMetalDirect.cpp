7586f8c84a3ef50ecc2562c945021904efe02b99
Shahbaz Youssefi <syoussefi@chromium.org>
2023-02-20 22:10:55 -0500

    Translator: Validate correct op usage in nodes
    Bug: angleproject:2733
    Change-Id: Id555398cd6633ce38e1d4d900c7b739fd9990ad1
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4274045
    Auto-Submit: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/compiler/translator/TranslatorMetalDirect.cpp
+++ src/compiler/translator/TranslatorMetalDirect.cpp
@@ -255,7 +255,7 @@ TIntermSequence *GetMainSequence(TIntermBlock *root)
     sequence.push_back(builtinRef->deepCopy());
     TIntermAggregate *aggregate =
         TIntermAggregate::CreateConstructor(builtin->getType(), &sequence);
-    TIntermBinary *assignment = new TIntermBinary(EOpInitialize, flippedBuiltinRef, aggregate);
+    TIntermBinary *assignment = new TIntermBinary(EOpAssign, flippedBuiltinRef, aggregate);
 
     // Create an assignment to the replaced variable's .xy.
     TIntermSwizzle *correctedXY =
