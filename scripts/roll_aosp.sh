a073613509f1fa3195500e37462ecc4bff686092
Roman Lavrov <romanl@google.com>
2023-03-01 15:17:07 +0000

    Revert "Replace zlib from chrome by Android's zlib."
    This reverts commit 39f0eaf8095b303b09951ceb6ffda8254ed24c8d.
    
    Reason for revert: Broke roller https://crbug.com/skia/14155
    
    Original change's description:
    > Replace zlib from chrome by Android's zlib.
    >
    > Implements Elliott's https://r.android.com/2360448 in our codegen. The
    > only diffs are ordering and third_party_android_ndk_cpu_features.
    > We only seem to depend on cpufeatures for zlib.
    >
    > //third_party/zlib/google:compression_utils_portable is the root
    > dependency, so cutting it makes other zlib libs disappear from
    > Android.bp
    >
    > zlib_google_compression_utils_portable and libz_static
    > that this CL depends on were added in https://r.android.com/2359720
    >
    > Tested by running roll_aosp.sh in Android tree and building.
    > third_party/zlib got deleted locally so presumably it should
    > get deleted by the roller
    >
    > Bug: b/262603863
    > Change-Id: I2651cc21121d741526055fa2b0288d0dcfa5c62a
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4111891
    > Reviewed-by: Cody Northrop <cnorthrop@google.com>
    > Commit-Queue: Roman Lavrov <romanl@google.com>
    
    Bug: b/262603863
    Change-Id: Ie17cb5b63a2c86a7414230bf9b0e8ef16e8734bb
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4298280
    Commit-Queue: Roman Lavrov <romanl@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -119,6 +119,7 @@ third_party_deps=(
     "third_party/vulkan-deps/spirv-tools/src"
     "third_party/vulkan-deps/vulkan-headers/src"
     "third_party/vulkan_memory_allocator"
+    "third_party/zlib"
 )
 
 root_add_deps=(
@@ -155,7 +156,6 @@ unsupported_third_party_deps=(
    "third_party/llvm-build"
    "third_party/android_build_tools"
    "third_party/android_sdk"
-   "third_party/zlib"  # Replaced by Android's zlib
 )
 for unsupported_third_party_dep in "${unsupported_third_party_deps[@]}"; do
    rm -rf "$unsupported_third_party_dep"
