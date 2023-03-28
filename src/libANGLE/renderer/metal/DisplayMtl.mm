5fcd027edec5ae756c4484a7d6e32f8aad147984
Dean Jackson <dino@apple.com>
2023-03-29 08:54:34 +1100

    Fix the watchOS build for the mirror clamp to edge extension
    The mirror clamp to edge extension relies on
    MTLSamplerAddressModeMirrorClampToEdge, which is not available
    on watchOS. Disable the extension there for the moment.
    
    Bug: angleproject:8103
    Change-Id: I52b9d5217b39080cde6663e0d0fa839e53dacecf
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4379037
    Reviewed-by: Kenneth Russell <kbr@chromium.org>

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -940,10 +940,12 @@ void DisplayMtl::initializeExtensions() const
     mNativeExtensions.copyTextureCHROMIUM           = true;
     mNativeExtensions.copyCompressedTextureCHROMIUM = false;
 
+#if !defined(ANGLE_PLATFORM_WATCHOS) || !ANGLE_PLATFORM_WATCHOS
     if (@available(iOS 14.0, macOS 10.11, macCatalyst 14.0, tvOS 16.0, *))
     {
         mNativeExtensions.textureMirrorClampToEdgeEXT = true;
     }
+#endif
 
     if (ANGLE_APPLE_AVAILABLE_XCI(10.11, 11.0, 13.1))
     {
