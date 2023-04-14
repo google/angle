28edde6f2b7b306f37ce85527f90586f1ddd5015
Yiwei Zhang <zzyiwei@chromium.org>
2023-04-13 20:20:33 -0700

    Vulkan: enable enableCompressingPipelineCacheInThreadPool for venus
    Try again with aosp/2451163.
    
    Bug: b/246683126
    Change-Id: Idb66cfbace29349ddf5ed6a96146433098384689
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4426749
    Commit-Queue: Yiwei Zhang <zzyiwei@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4384,7 +4384,7 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
                                 (mFeatures.preferMonolithicPipelinesOverLibraries.enabled &&
                                  libraryBlobsAreReusedByMonolithicPipelines));
 
-    ANGLE_FEATURE_CONDITION(&mFeatures, enableAsyncPipelineCacheCompression, false);
+    ANGLE_FEATURE_CONDITION(&mFeatures, enableAsyncPipelineCacheCompression, isVenus);
 
     // Sync monolithic pipelines to the blob cache occasionally on platforms that would benefit from
     // it:
