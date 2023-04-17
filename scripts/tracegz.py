a636aa5c7439e0ba72470f163284740126357d1b
Roman Lavrov <romanl@google.com>
2023-04-17 16:03:10 -0400

    Fix win-trace broken by tracegz.py
    Gets invoked on win-trace resulting in errors like:
    
        FileNotFoundError: [Errno 2] No such file or directory: '..\\..\\src\\tests\\restricted_traces\\BindGeneratesResourceTest_Textures_ES2_Vulkan_SwiftShader\\BindGeneratesResourceTest_Textures_ES2_Vulkan_SwiftShader.json'
    
    https://ci.chromium.org/ui/p/angle/builders/ci/win-trace/3788/overview
    
    Bug: b/276742336
    Change-Id: I2d86cdbcbc0286746e535a4b19f4d4b547b5783f
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4436590
    Commit-Queue: Cody Northrop <cnorthrop@google.com>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>
    Auto-Submit: Roman Lavrov <romanl@google.com>

--- scripts/tracegz.py
+++ scripts/tracegz.py
@@ -3,9 +3,9 @@ import json
 import os
 import sys
 
-trace_dir = os.path.join('..', '..', 'src', 'tests', 'restricted_traces', sys.argv[1])
+trace, trace_dir = sys.argv[1:]
 
-with open(os.path.join(trace_dir, sys.argv[1] + '.json'), 'rb') as f:
+with open(os.path.join(trace_dir, trace + '.json'), 'rb') as f:
     trace_json = json.loads(f.read())
 
 # Concatenate trace cpp source into a single gz
