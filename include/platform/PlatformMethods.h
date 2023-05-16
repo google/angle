7a11f4415e37ffba18023eaf1722d3f568ee12ba
Geoff Lang <geofflang@chromium.org>
2023-05-16 11:14:44 -0400

    Don't export ANGLE platform methods.
    These functions are loaded through eglGetProcAddress. We don't need
    to export these extra symbols and increase our binary size.
    
    Bug: chromium:1445969
    Change-Id: I494bbcb86973f167e8245b56e6eb73da7eab69f5
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4538367
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- include/platform/PlatformMethods.h
+++ include/platform/PlatformMethods.h
@@ -313,15 +313,15 @@ extern "C" {
 // The application should set any platform methods it cares about on the returned pointer.
 // If display is not valid, behaviour is undefined.
 
-ANGLE_PLATFORM_EXPORT bool ANGLE_APIENTRY ANGLEGetDisplayPlatform(angle::EGLDisplayType display,
-                                                                  const char *const methodNames[],
-                                                                  unsigned int methodNameCount,
-                                                                  void *context,
-                                                                  void *platformMethodsOut);
+bool ANGLE_APIENTRY ANGLEGetDisplayPlatform(angle::EGLDisplayType display,
+                                            const char *const methodNames[],
+                                            unsigned int methodNameCount,
+                                            void *context,
+                                            void *platformMethodsOut);
 
 // Sets the platform methods back to their defaults.
 // If display is not valid, behaviour is undefined.
-ANGLE_PLATFORM_EXPORT void ANGLE_APIENTRY ANGLEResetDisplayPlatform(angle::EGLDisplayType display);
+void ANGLE_APIENTRY ANGLEResetDisplayPlatform(angle::EGLDisplayType display);
 }  // extern "C"
 
 namespace angle
