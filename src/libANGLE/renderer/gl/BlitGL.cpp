2a5c05df74278910f51c81c417e8121cd7b1df53
Sunny Sachanandani <sunnyps@chromium.org>
2023-07-05 14:24:54 -0700

    gl: Handle copyTexSubImage2D failures manually
    https://chromium-review.googlesource.com/c/angle/angle/+/4662093 was a
    previous attempt at graceful fallback from copyTexSubImage2D, but the
    approach in that CL doesn't quite work since it produces an error when
    returning from the function which makes the caller also propagate the
    error instead of falling back to blit via shader.
    
    This CL makes it so that we handle the error manually and return an
    angle::result::Continue with *copySucceededOut=false to indicate to the
    caller that it should try a fallback.
    
    Bug: chromium:1458040
    Change-Id: I3cd66dcaa6069d5527c3e73190d72ffbba98ae4b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4662393
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/gl/BlitGL.cpp
+++ src/libANGLE/renderer/gl/BlitGL.cpp
@@ -880,13 +880,17 @@ angle::Result BlitGL::copyTexSubImage(const gl::Context *context,
 
     mStateManager->bindTexture(dest->getType(), dest->getTextureID());
 
-    ANGLE_GL_TRY_ALWAYS_CHECK(
-        context, mFunctions->copyTexSubImage2D(ToGLenum(destTarget), static_cast<GLint>(destLevel),
-                                               destOffset.x, destOffset.y, sourceArea.x,
-                                               sourceArea.y, sourceArea.width, sourceArea.height));
+    // Handle GL errors during copyTexSubImage2D manually since this can fail for certain formats on
+    // Pixel 2 and 4 and we have fallback paths (blit via shader) in the caller.
+    ClearErrors(context, __FILE__, __FUNCTION__, __LINE__);
+    mFunctions->copyTexSubImage2D(ToGLenum(destTarget), static_cast<GLint>(destLevel), destOffset.x,
+                                  destOffset.y, sourceArea.x, sourceArea.y, sourceArea.width,
+                                  sourceArea.height);
+    const bool copySucceeded =
+        !IsError(CheckError(context, "copyTexSubImage2D", __FILE__, __FUNCTION__, __LINE__));
 
     ANGLE_TRY(UnbindAttachment(context, mFunctions, GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0));
-    *copySucceededOut = true;
+    *copySucceededOut = copySucceeded;
     return angle::Result::Continue;
 }
 
