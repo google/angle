c319f34c4c8cb9f4f969ddca3cc4f645c74e32b8
Roman Lavrov <romanl@google.com>
2023-07-28 12:30:18 -0400

    Add VUID-vkCmdDraw-None VUID-vkCmdDrawIndexed-None VUIDs
    08753 is now renamed to 09003
    https://github.com/KhronosGroup/Vulkan-ValidationLayers/pull/6215
    as it's being split into multiple cases
    
    Bug: angleproject:8242
    Change-Id: I64aa75736fdb6ca9664da39f642e64f9f674b696
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4727623
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Roman Lavrov <romanl@google.com>
    Auto-Submit: Roman Lavrov <romanl@google.com>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -246,6 +246,8 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdDrawIndexed-None-08608",
     "VUID-vkCmdDraw-None-08753",
     "VUID-vkCmdDrawIndexed-None-08753",
+    "VUID-vkCmdDraw-None-09003",
+    "VUID-vkCmdDrawIndexed-None-09003",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
