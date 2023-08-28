60b56591dee59bc0bc770577f43d90be4b18863c
Shahbaz Youssefi <syoussefi@chromium.org>
2023-08-28 15:40:26 -0400

    Verify shader link correctness after looking up in program cache
    Since the program cache takes shader sources into account, if the cache
    is hit the shaders should have been correct for link.
    
    Bug: angleproject:8297
    Change-Id: I115c7eb807ed620d30b9e30669c99fffb5c7912d
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4817830
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/Program.cpp
+++ src/libANGLE/Program.cpp
@@ -1201,13 +1201,6 @@ angle::Result Program::linkImpl(const Context *context)
 
     mState.mInfoLog.reset();
 
-    // Validate we have properly attached shaders before checking the cache.
-    if (!linkValidateShaders(context))
-    {
-        return angle::Result::Continue;
-    }
-    linkShaders();
-
     egl::BlobCache::Key programHash = {0};
     MemoryProgramCache *cache       = context->getMemoryProgramCache();
 
@@ -1238,7 +1231,14 @@ angle::Result Program::linkImpl(const Context *context)
     // Cache load failed, fall through to normal linking.
     unlink();
 
-    // Re-link shaders after the unlink call.
+    // Validate we have properly attached shaders after checking the cache.  Since the input to the
+    // shaders is part of the cache key, if there was a cache hit, the shaders would have linked
+    // correctly.
+    if (!linkValidateShaders(context))
+    {
+        return angle::Result::Continue;
+    }
+
     linkShaders();
 
     ProgramMergedVaryings mergedVaryings;
