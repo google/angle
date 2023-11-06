0584ffd8fbdd1e58dffceecea77c32989751def8
Roman Lavrov <romanl@google.com>
2023-11-06 16:34:13 -0500

    Unsuppress VUID-VkGraphicsPipelineCreateInfo-pStages-00739
    Possibly was due to a VVL bug
    https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/6717
    
    Bug: b/303219657
    Change-Id: Ic9a6e7b45fb42572af04f94382906cd6f0f512df
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5008042
    Auto-Submit: Roman Lavrov <romanl@google.com>
    Commit-Queue: Roman Lavrov <romanl@google.com>
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Amirali Abdolrashidi <abdolrashidi@google.com>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -226,7 +226,6 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkDestroySemaphore-semaphore-01137",
     // https://issuetracker.google.com/303219657
     "VUID-VkGraphicsPipelineCreateInfo-pStages-00738",
-    "VUID-VkGraphicsPipelineCreateInfo-pStages-00739",
     // http://anglebug.com/7861
     "VUID-vkCmdDraw-None-06887", "VUID-vkCmdDraw-None-06886", "VUID-vkCmdDrawIndexed-None-06887",
     // http://anglebug.com/8394
