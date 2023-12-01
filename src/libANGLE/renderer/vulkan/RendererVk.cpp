90767546353c4537d95994b571673ebff6f74f42
Charlie Lao <cclao@google.com>
2023-12-01 12:27:15 -0800

    Vulkan: Add test for __samplerExternal2DY2YEXT then swizzle
    Added a correctness test
    SourceYUVTextureTargetExternalRGBSampleYUVSampleWithSwizzle for applying
    swizzle after sampler.
    
    Also removed some bug workaround for VVL and drivers since they are
    fixed now.
    
    Bug: b/309480316
    Change-Id: If82b2251745a96335b535c67b6e0c0847268b25b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5080497
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: mohan maiya <m.maiya@samsung.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -269,8 +269,6 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-VkSamplerCreateInfo-pNext-pNext",
     // https://issuetracker.google.com/303441816
     "VUID-VkRenderPassBeginInfo-renderPass-00904",
-    // http://b/223456677 VK_ANDROID_external_format_resolve: remove once ARM/ VVL are fixed.
-    "VUID-VkImageViewCreateInfo-usage-08931", "VUID-VkImageCreateInfo-pNext-02397",
     // http://anglebug.com/8401
     "Undefined-Value-ShaderOutputNotConsumed"};
 
