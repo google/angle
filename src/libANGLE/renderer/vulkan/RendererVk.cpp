524bcb8e3704f33c78cbb5fd0e4fe349119d569f
Yiwei Zhang <zzyiwei@chromium.org>
2023-12-13 20:21:38 +0000

    Vulkan: avoid forcing preferSubmitAtFBOBoundary with Venus
    It turns out benefiting Venus atop ARM while hurting in a non-trivial
    way on old Gen Intel due to execbuf overhead.
    
    Bug: b/314847707
    Test: 2%+ gain for trex on ANV on CML
    Change-Id: I61eb18a05e94c97ee6a4d70f98ae30423155e632
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5118128
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Auto-Submit: Yiwei Zhang <zzyiwei@chromium.org>
    Reviewed-by: Charlie Lao <cclao@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4413,9 +4413,8 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
 
     // Testing shows that on ARM GPU, doing implicit flush at framebuffer boundary improves
     // performance. Most app traces shows frame time reduced and manhattan 3.1 offscreen score
-    // improves 7%. Enable for MESA Virtio-GPU Venus driver in virtualized environment as well.
-    ANGLE_FEATURE_CONDITION(&mFeatures, preferSubmitAtFBOBoundary,
-                            isARM || isSwiftShader || isVenus);
+    // improves 7%.
+    ANGLE_FEATURE_CONDITION(&mFeatures, preferSubmitAtFBOBoundary, isARM || isSwiftShader);
 
     // In order to support immutable samplers tied to external formats, we need to overallocate
     // descriptor counts for such immutable samplers
