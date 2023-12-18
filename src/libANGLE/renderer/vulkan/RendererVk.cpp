1e4c4c763bda46badedc2daaa7d3b443d72f9806
Yiwei Zhang <zzyiwei@chromium.org>
2023-12-18 19:35:13 +0000

    Vulkan: avoid disabling explicitlyCastMediumpFloatTo16Bit for venus
    We disabled it to work around below legacy skia cts:
    CtsSkQPTestCases org.skia.skqp.SkQPRunner#gles_gradient_many_stops
    
    The test no longer exists in newer skia cts. Meanwhile, we have to drop
    the workaround for other precision issue on Mali-G52.
    
    Bug: b/316474995
    Test: dEQP-GLES2.functional.shaders.algorithm.rgb_to_hsl_vertex
    Change-Id: I18394595632d05657e96f8b9c9675b4dd959d92b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5133372
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Auto-Submit: Yiwei Zhang <zzyiwei@chromium.org>
    Commit-Queue: Yiwei Zhang <zzyiwei@chromium.org>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4738,7 +4738,7 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
     // workaround, per-sample shading is inferred by ANGLE and explicitly enabled by the API.
     ANGLE_FEATURE_CONDITION(&mFeatures, explicitlyEnablePerSampleShading, isARM);
 
-    ANGLE_FEATURE_CONDITION(&mFeatures, explicitlyCastMediumpFloatTo16Bit, isARM && !isVenus);
+    ANGLE_FEATURE_CONDITION(&mFeatures, explicitlyCastMediumpFloatTo16Bit, isARM);
 
     // Force to create swapchain with continuous refresh on shared present. Disabled by default.
     // Only enable it on integrations without EGL_FRONT_BUFFER_AUTO_REFRESH_ANDROID passthrough.
