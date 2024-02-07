475784f5a635491cf9b0d44ce47499c349bbef10
Solti <solti@google.com>
2024-02-07 21:24:36 +0000

    suppress VUID-VkPipelineVertexInputStateCreateInfo-pNext-pNext
    Bug: angleproject:8522
    Bug: b/324243941
    Change-Id: I825986e7c98f50522d7ba25c0c78839802c42c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5277631
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>
    Commit-Queue: Solti Ho <solti@google.com>

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -292,6 +292,8 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdEndDebugUtilsLabelEXT-commandBuffer-01912",
     // https://anglebug.com/8516
     "VUID-VkGraphicsPipelineCreateInfo-dynamicRendering-06576",
+    // https://anglebug.com/8522
+    "VUID-VkPipelineVertexInputStateCreateInfo-pNext-pNext",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
