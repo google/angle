a69c56fa884e6f70f061a2f9c8137e7d4570516a
Yuly Novikov <ynovikov@chromium.org>
2024-03-21 16:45:11 +0000

    Pin build until Skia is able to build with ToT abseil.
    ANGLE pins abseil-cpp until https://skbug.com/330350366 is resolved.
    Therefore ANGLE's abseil-cpp uses old DEF files, which can't be kept in
    sync with new build, e.g. Win toolchain updates.
    
    Bug: angleproject:8555
    Change-Id: Ie5a8a1bb8b4286dfcf50b73cddd36b05bf1c5b15
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5385782
    Auto-Submit: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Roman Lavrov <romanl@google.com>
    Reviewed-by: Roman Lavrov <romanl@google.com>

--- scripts/roll_chromium_deps.py
+++ scripts/roll_chromium_deps.py
@@ -30,7 +30,9 @@ def FindSrcDirPath():
     return os.path.dirname(os.path.abspath(os.path.join(__file__, '..')))
 
 ANGLE_CHROMIUM_DEPS = [
-    'build',
+    # Pin build because abseil-cpp is pinned, so its DEF files can't be kept in sync with build.
+    # https://skbug.com/330350366
+    # 'build',
     'buildtools',
     'buildtools/linux64',
     'buildtools/mac',
