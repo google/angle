b64563343822b61a3a840ad192632c3d6c94da42
Geoff Lang <geofflang@chromium.org>
2024-04-02 15:03:17 -0400

    Reland: GL: Support KHR_blend_equation_advanced
    Combine blend equation setting when they are equal. It is invalid to
    set advanced blend equations for color/alpha separately.
    
    Emit blend equation layout qualifiers.
    
    This is an unchanged reland of
    https://chromium-review.googlesource.com/c/angle/angle/+/5415033
    and depends on a GL dispatch table fix in
    https://chromium-review.googlesource.com/c/angle/angle/+/5670963
    
    Bug: angleproject:42267098
    Change-Id: Id669cf98ec2b7d46eab174717964f8d6daf88b29
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5659200
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -2162,6 +2162,17 @@ void GenerateCaps(const FunctionsGL *functions,
     extensions->tiledRenderingQCOM = !features.disableTiledRendering.enabled &&
                                      functions->hasGLESExtension("GL_QCOM_tiled_rendering");
 
+    extensions->blendEquationAdvancedKHR =
+        functions->hasGLExtension("GL_NV_blend_equation_advanced") ||
+        functions->hasGLExtension("GL_KHR_blend_equation_advanced") ||
+        functions->isAtLeastGLES(gl::Version(3, 2)) ||
+        functions->hasGLESExtension("GL_KHR_blend_equation_advanced");
+    extensions->blendEquationAdvancedCoherentKHR =
+        functions->hasGLExtension("GL_NV_blend_equation_advanced_coherent") ||
+        functions->hasGLExtension("GL_KHR_blend_equation_advanced_coherent") ||
+        functions->isAtLeastGLES(gl::Version(3, 2)) ||
+        functions->hasGLESExtension("GL_KHR_blend_equation_advanced_coherent");
+
     // PVRTC1 textures must be squares on Apple platforms.
     if (IsApple())
     {
