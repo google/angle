646f5d19356453b643ae3d35d1677e8e8de7a925
Austin Annestrand <a.annestrand@samsung.com>
2024-04-03 15:28:40 -0500

    CL/VK: Implement eventVk routines
    Adding routines:
      clGetEventInfo [CL_EVENT_COMMAND_EXECUTION_STATUS]
      clSetEventCallback
      clWaitForEvents [USER_EVENT_STATUS]
    
    We also trigger event callback on exec-status update.
    
    Bug: angleproject:8574
    Change-Id: I470d9758de22a552bd9cf9565594b3049e21835d
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5421575
    Commit-Queue: Austin Annestrand <a.annestrand@samsung.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/vulkan/CLContextVk.cpp
+++ src/libANGLE/renderer/vulkan/CLContextVk.cpp
@@ -241,7 +241,7 @@ angle::Result CLContextVk::linkProgram(const cl::Program &program,
 
 angle::Result CLContextVk::createUserEvent(const cl::Event &event, CLEventImpl::Ptr *eventOut)
 {
-    *eventOut = CLEventImpl::Ptr(new (std::nothrow) CLEventVk(event, cl::EventPtrs{}));
+    *eventOut = CLEventImpl::Ptr(new (std::nothrow) CLEventVk(event));
     if (*eventOut == nullptr)
     {
         ANGLE_CL_RETURN_ERROR(CL_OUT_OF_HOST_MEMORY);
