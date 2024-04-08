0b776d32f69a932acb61963d9daad9e13f610944
Shahbaz Youssefi <syoussefi@chromium.org>
2024-04-08 10:14:45 -0400

    M124: SPIR-V: Fix const constructors with single scalar
    These constructors may be generated because of
    RemoveArrayLengthTraverser.
    
    Bug: chromium:332546345
    Change-Id: I2b2bf3728ef5bae148abc2a8518f8f3f42850025
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5462388
    Reviewed-by: Cody Northrop <cnorthrop@google.com>

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -1067,6 +1067,11 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
     {
         return false;
     }
+    // Fold the expressions again, because |RemoveArrayLengthMethod| can introduce new constants.
+    if (!FoldExpressions(this, root, &mDiagnostics))
+    {
+        return false;
+    }
 
     if (!RemoveUnreferencedVariables(this, root, &mSymbolTable))
     {
