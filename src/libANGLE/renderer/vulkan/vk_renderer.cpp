c74d2634e6e094865e2ad1c43e37f00910c119be
Charlie Lao <cclao@google.com>
2024-04-19 12:19:51 -0700

    Vulkan: Update mSurfaceCaps with per-present mode caps.
    Vulkan spec says "The per-present mode image counts may be less-than or
    greater-than the image counts returned when VkSurfacePresentModeEXT is
    not provided". Riht now we are using the generic surfaceCaps information
    which has smaller minImageCount. This triggers VVL error
    VUID-VkSwapchainCreateInfoKHR-presentMode-02839.. This CL updates
    mSurfaceCaps and mMinImageCount with per-present mode caps and create
    swapChain with proper minImageCount.
    
    Bug: angleproject:8672
    Change-Id: I6061d2db8e98032dcb2c3225f2976013a99336a9
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5467850
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Charlie Lao <cclao@google.com>

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -279,8 +279,6 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdDrawIndexed-format-07753",
     "VUID-vkCmdDraw-format-07753",
     "Undefined-Value-ShaderFragmentOutputMismatch",
-    // https://anglebug.com/8672
-    "VUID-VkSwapchainCreateInfoKHR-presentMode-02839",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
