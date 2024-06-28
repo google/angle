aee59926cd632e95915bde127f68257ed7a91c54
Kimmo Kinnunen <kkinnunen@apple.com>
2024-06-28 17:00:56 +0300

    Metal: Enable GL_ANGLE_reverse_row_order for Metal
    The feature is already implemented due to generic pack parameters
    handling and FramebufferMtl::setFlipY().
    
    Bug: angleproject:349980052
    Change-Id: I08cc81dcb26ae81e8d81b060d986e01479487dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5667597
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Kimmo Kinnunen <kkinnunen@apple.com>

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -1077,6 +1077,8 @@ void DisplayMtl::initializeExtensions() const
     // GL_NV_pixel_buffer_object
     mNativeExtensions.pixelBufferObjectNV = true;
 
+    mNativeExtensions.packReverseRowOrderANGLE = true;
+
     if (mFeatures.hasEvents.enabled)
     {
         // MTLSharedEvent is only available since Metal 2.1
