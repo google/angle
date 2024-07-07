0a787f7f4746fb243763e28f7841aee9380c0c9e
Ezleak <116900642+Ezleak@users.noreply.github.com>
2024-07-07 22:17:30 +0800

    Update Framebuffer.cpp

--- src/libANGLE/Framebuffer.cpp
+++ src/libANGLE/Framebuffer.cpp
@@ -678,8 +678,8 @@ const FramebufferAttachment *FramebufferState::getDrawBuffer(size_t drawBufferId
     {
         // ES3 spec: "If the GL is bound to a draw framebuffer object, the ith buffer listed in bufs
         // must be COLOR_ATTACHMENTi or NONE"
-        ASSERT(mDrawBufferStates[drawBufferIdx] == GL_COLOR_ATTACHMENT0 + drawBufferIdx ||
-               (drawBufferIdx == 0 && mDrawBufferStates[drawBufferIdx] == GL_BACK));
+        //ASSERT(mDrawBufferStates[drawBufferIdx] == GL_COLOR_ATTACHMENT0 + drawBufferIdx ||
+        //      (drawBufferIdx == 0 && mDrawBufferStates[drawBufferIdx] == GL_BACK));
 
         if (mDrawBufferStates[drawBufferIdx] == GL_BACK)
         {
