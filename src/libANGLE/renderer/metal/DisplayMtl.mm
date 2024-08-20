6ef39db0420f219dbef0d8f559a340e0c60f099b
Alexey Knyazev <lexa.knyazev@gmail.com>
2024-08-20 00:00:00 +0000

    Metal: Remove obsolete compile guards for texture helpers
    MTLDevice.supports32BitFloatFiltering
    
    - macOS 11.0 (min compile 12.0, min deploy 10.15)
    - iOS 14.0 (min compile 17.0, min deploy 15.0)
    - tvOS 16.0 (min compile 17.0, min deploy 17.0)
    
    MTLDevice.supportsBCTextureCompression
    
    - macOS 11.0 (min compile 12.0, min deploy 10.15)
    - iOS 16.4 (min compile 17.0, min deploy 15.0)
    - tvOS 16.4 (min compile 17.0, min deploy 17.0)
    
    MTLPixelFormatPVRTC* enums
    
    - macOS 11.0 (min compile 12.0)
    - iOS 8.0 (min compile 17.0)
    - tvOS 9.0 (min compile 17.0)
    
    Bug: angleproject:360147119
    Change-Id: Iafb010efc233f9c677a2c6f65cb4efdf0bb9cf9c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5805290
    Reviewed-by: Kimmo Kinnunen <kkinnunen@apple.com>
    Commit-Queue: Alexey Knyazev <lexa.knyazev@gmail.com>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -1414,31 +1414,19 @@ bool DisplayMtl::supportsMetal2_2() const
 
 bool DisplayMtl::supports32BitFloatFiltering() const
 {
-#if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MAX_ALLOWED >= 110000) ||  \
-     (TARGET_OS_IOS && __IPHONE_OS_VERSION_MAX_ALLOWED >= 140000) || \
-     (TARGET_OS_TV && __TV_OS_VERSION_MAX_ALLOWED >= 160000) || TARGET_OS_VISION)
-    if (@available(macOS 11.0, macCatalyst 14.0, iOS 14.0, tvOS 16.0, *))
+    if (@available(macOS 11.0, *))
     {
         return [mMetalDevice supports32BitFloatFiltering];
     }
-#endif
-#if TARGET_OS_OSX || TARGET_OS_MACCATALYST
     return true;  // Always true on old macOS
-#else
-    return false;  // Always false everywhere else
-#endif
 }
 
 bool DisplayMtl::supportsBCTextureCompression() const
 {
-#if ((TARGET_OS_OSX && __MAC_OS_X_VERSION_MAX_ALLOWED >= 110000) ||  \
-     (TARGET_OS_IOS && __IPHONE_OS_VERSION_MAX_ALLOWED >= 160400) || \
-     (TARGET_OS_TV && __TV_OS_VERSION_MAX_ALLOWED >= 160400) || TARGET_OS_VISION)
-    if (@available(macOS 11.0, macCatalyst 16.4, iOS 16.4, tvOS 16.4, *))
+    if (@available(macOS 11.0, macCatalyst 16.4, iOS 16.4, *))
     {
         return [mMetalDevice supportsBCTextureCompression];
     }
-#endif
 #if TARGET_OS_OSX || TARGET_OS_MACCATALYST
     return true;  // Always true on old macOS
 #else
