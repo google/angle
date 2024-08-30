4167a9a05a4df659d852944aab61ea2eac4d8b0d
Shahbaz Youssefi <syoussefi@chromium.org>
2024-08-29 23:38:05 -0400

    Don't lock the context mutex if no flush in eglClientWaitSync
    Bug: b/362604439
    Change-Id: Ic514bcb3824514b5fd82ebb14ab97286aeb6557c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5828262
    Reviewed-by: Igor Nazarov <i.nazarov@samsung.com>
    Reviewed-by: Charlie Lao <cclao@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- scripts/generate_entry_points.py
+++ scripts/generate_entry_points.py
@@ -1606,6 +1606,9 @@ EGL_PACKED_TYPES = {
     "EGLSyncKHR": "egl::SyncID",
 }
 
+EGL_CONTEXT_LOCK_PARAM_TYPES_FILTER = ["Thread *", "egl::Display *", "gl::ContextID"]
+EGL_CONTEXT_LOCK_PARAM_NAMES_FILTER = ["attribute", "flags"]
+
 CAPTURE_BLOCKLIST = ['eglGetProcAddress']
 
 
@@ -1923,9 +1926,10 @@ def format_entry_point_def(api, command_node, cmd_name, proto, params, cmd_packe
         has_errcode_ret = False
 
     internal_context_lock_params = [
-        just_the_name_packed(param, packed_enums) for param in params if just_the_type_packed(
-            param, packed_enums) in ["Thread *", "egl::Display *", "gl::ContextID"] or
-        just_the_name_packed(param, packed_enums) in ["attribute"]
+        just_the_name_packed(param, packed_enums)
+        for param in params
+        if just_the_type_packed(param, packed_enums) in EGL_CONTEXT_LOCK_PARAM_TYPES_FILTER or
+        just_the_name_packed(param, packed_enums) in EGL_CONTEXT_LOCK_PARAM_NAMES_FILTER
     ]
 
     packed_gl_enum_conversions = []
@@ -2180,9 +2184,8 @@ def get_context_lock_params(api, cmd_name, params, cmd_packed_gl_enums, packed_p
             just_the_type_packed(param, packed_gl_enums),
             just_the_name_packed(param, packed_gl_enums))
         for param in params
-        if just_the_type_packed(
-            param, packed_gl_enums) in ["Thread *", "egl::Display *", "gl::ContextID"] or
-        just_the_name_packed(param, packed_gl_enums) in ["attribute"]
+        if just_the_type_packed(param, packed_gl_enums) in EGL_CONTEXT_LOCK_PARAM_TYPES_FILTER or
+        just_the_name_packed(param, packed_gl_enums) in EGL_CONTEXT_LOCK_PARAM_NAMES_FILTER
     ])
 
 
