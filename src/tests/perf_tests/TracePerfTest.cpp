4dfba10af74454e1b2bb5e729ce747caf19f7da5
Yuly Novikov <ynovikov@chromium.org>
2024-08-30 15:05:44 +0000

    Skip passmark_complex on Linux Intel
    Bug: b/362801312
    Change-Id: I743261c7f5086d5aaf98d28c1b32fa494c0fc2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5822715
    Commit-Queue: Cody Northrop <cnorthrop@google.com>
    Reviewed-by: Cody Northrop <cnorthrop@google.com>
    Auto-Submit: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -1814,6 +1814,14 @@ TracePerfTest::TracePerfTest(std::unique_ptr<const TracePerfParams> params)
         addExtensionPrerequisite("GL_OES_framebuffer_object");
     }
 
+    if (traceNameIs("passmark_complex"))
+    {
+        if (isIntelLinuxNative)
+        {
+            skipTest("b/362801312 eglCreateContext fails on Mesa 23.2.1");
+        }
+    }
+
     if (IsGalaxyS22())
     {
         if (traceNameIs("cod_mobile") || traceNameIs("dota_underlords") ||
