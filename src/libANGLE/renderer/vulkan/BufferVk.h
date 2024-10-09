beaudie/vesc

Vulkan: Invalidate host visible non-coherent buffers on mapping
We can not trust the cache during CPU readback when the buffer
memory type is non-coherent.

Bug: b/366134076
Change-Id: I89920cfa468ee0be0feb607fea9d60bc0732191f

src/libANGLE/renderer/vulkan/BufferVk.h
@@ -176,6 +176,10 @@ class BufferVk : public BufferImpl
                                              VkDeviceSize offset,
                                              VkDeviceSize size,
                                              uint8_t **mapPtr);
+    angle::Result handleHostVisibleBufferMap(ContextVk *contextVk,
+                                             VkDeviceSize offset,
+                                             GLbitfield access,
+                                             uint8_t **mapPtr);
     angle::Result setDataImpl(ContextVk *contextVk,
                               size_t bufferSize,
                               const BufferDataSource &dataSource,

google/angle

Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5890707
Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
Reviewed-by: Charlie Lao <cclao@google.com>
Auto-Submit: Imran Ziad <imranziad@chromium.org>
(cherry picked from commit 0040cda1170f9d481135a95d2fbff616120f404b)
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5902541
Commit-Queue: Charlie Lao <cclao@google.com>
Commit-Queue: Imran Ziad <imranziad@chromium.org>

src/libANGLE/renderer/vulkan/BufferVk.h
@@ -263,6 +263,10 @@ class BufferVk : public BufferImpl
                                              VkDeviceSize offset,
                                              VkDeviceSize size,
                                              uint8_t **mapPtr);
+    angle::Result mapHostVisibleBuffer(ContextVk *contextVk,
+                                       VkDeviceSize offset,
+                                       GLbitfield access,
+                                       uint8_t **mapPtr);
     angle::Result setDataImpl(ContextVk *contextVk,
                               size_t bufferSize,
                               const BufferDataSource &dataSource,