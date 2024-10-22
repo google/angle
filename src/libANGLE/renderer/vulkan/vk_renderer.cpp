e869b4262c2ea793a8bceaf228253dcfb5966c5b
Cody Northrop <cnorthrop@google.com>
2024-10-22 10:04:06 -0600

    Revert "Removed checks for promoted extensions"
    This reverts commit c2219ef9ec03277b6f8ad18d6d1ade41ddf88ba6.
    
    Reason for revert: Possible cause of boot failures on cuttlefish
    
    Bug: angleproject:374975259
    
    Original change's description:
    > Removed checks for promoted extensions
    >
    > Removed checks for Vulkan extensions that have been promoted into Vulkan
    > 1.1, which is already the minimum required version for ANGLE. The
    > dependent features now work when the extensions are missing because the
    > extensions are no longer necessary.
    >
    > Bug: angleproject:374483183
    > Change-Id: Iad92b33a8e724eaa8f4bfcbe406f5b6b8536d6d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5944678
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    > Reviewed-by: Charlie Lao <cclao@google.com>
    > Commit-Queue: Charlie Lao <cclao@google.com>
    
    Bug: angleproject:374483183
    Change-Id: I92eb6681c6d1b8f7e2d0832bb4e1cbf4597bd1c6
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5952279
    Commit-Queue: Cody Northrop <cnorthrop@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -2733,18 +2733,26 @@ void Renderer::appendDeviceExtensionFeaturesPromotedTo11(
     vk::AddToPNextChain(deviceProperties, &mSubgroupProperties);
     vk::AddToPNextChain(deviceFeatures, &mProtectedMemoryFeatures);
 
-    // VK_KHR_sampler_ycbcr_conversion
-    vk::AddToPNextChain(deviceFeatures, &mSamplerYcbcrConversionFeatures);
+    if (ExtensionFound(VK_KHR_SAMPLER_YCBCR_CONVERSION_EXTENSION_NAME, deviceExtensionNames))
+    {
+        vk::AddToPNextChain(deviceFeatures, &mSamplerYcbcrConversionFeatures);
+    }
 
-    // VK_KHR_multiview
-    vk::AddToPNextChain(deviceFeatures, &mMultiviewFeatures);
-    vk::AddToPNextChain(deviceProperties, &mMultiviewProperties);
+    if (ExtensionFound(VK_KHR_MULTIVIEW_EXTENSION_NAME, deviceExtensionNames))
+    {
+        vk::AddToPNextChain(deviceFeatures, &mMultiviewFeatures);
+        vk::AddToPNextChain(deviceProperties, &mMultiviewProperties);
+    }
 
-    // VK_KHR_16bit_storage
-    vk::AddToPNextChain(deviceFeatures, &m16BitStorageFeatures);
+    if (ExtensionFound(VK_KHR_16BIT_STORAGE_EXTENSION_NAME, deviceExtensionNames))
+    {
+        vk::AddToPNextChain(deviceFeatures, &m16BitStorageFeatures);
+    }
 
-    // VK_KHR_variable_pointers
-    vk::AddToPNextChain(deviceFeatures, &mVariablePointersFeatures);
+    if (ExtensionFound(VK_KHR_VARIABLE_POINTERS_EXTENSION_NAME, deviceExtensionNames))
+    {
+        vk::AddToPNextChain(deviceFeatures, &mVariablePointersFeatures);
+    }
 }
 
 // The following features and properties used by ANGLE have been promoted to Vulkan 1.2:
