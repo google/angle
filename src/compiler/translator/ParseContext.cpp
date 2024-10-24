4a5b0284f38f2a5b5ace1b65b803a25eac72fef8
Kimmo Kinnunen <kkinnunen@apple.com>
2024-10-24 11:46:54 +0300

    Disallow discarded uniform block references
    Uniform block instances cannot be used by themselves, as there
    is no way to refer to their type. Disallow the only typeless
    access pattern, discarding via expression statement:
    
      uniform MyBlock { int x;  } b;
      void main() {
        b;
        0, b;
      }
    
    Explained as disallowed in newer GLSL 300:
    https://github.com/KhronosGroup/WebGL/issues/3644
    
    The comma expression form would ASSERT in CollectVariables.
    
    Bug: angleproject:42267026
    Change-Id: I6c8b835482fd551bd97576c1bd24f005874da6af
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5961498
    Reviewed-by: Alexey Knyazev <lexa.knyazev@gmail.com>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Kimmo Kinnunen <kkinnunen@apple.com>
    Auto-Submit: Kimmo Kinnunen <kkinnunen@apple.com>

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -859,6 +859,14 @@ bool TParseContext::checkIsAtGlobalLevel(const TSourceLoc &line, const char *tok
     return true;
 }
 
+void TParseContext::checkIsValidExpressionStatement(const TSourceLoc &line, TIntermTyped *expr)
+{
+    if (expr->isInterfaceBlock())
+    {
+        error(line, "expression statement is not allowed for interface blocks", "");
+    }
+}
+
 // ESSL 3.00.5 sections 3.8 and 3.9.
 // If it starts "gl_" or contains two consecutive underscores, it's reserved.
 // Also checks for "webgl_" and "_webgl_" reserved identifiers if parsing a webgl shader.
@@ -7257,6 +7265,10 @@ TIntermTyped *TParseContext::addComma(TIntermTyped *left,
         error(loc,
               "sequence operator is not allowed for void, arrays, or structs containing arrays",
               ",");
+    }
+    if (left->isInterfaceBlock() || right->isInterfaceBlock())
+    {
+        error(loc, "sequence operator is not allowed for interface blocks", ",");
     }
 
     TIntermBinary *commaNode = TIntermBinary::CreateComma(left, right, mShaderVersion);
