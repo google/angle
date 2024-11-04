3f312d98b9388384dcb552b4206b2903ec0ed48f
Shufen Ma <shufen.ma@arm.com>
2024-11-04 14:04:50 +0800

    ES31: GetTexLevelParameter{if} validation for TEXTURE_2D_MULTISAMPLE
    GL_INVALID_VALUE is generated if target is GL_TEXTURE_2D_MULTISAMPLE,
    GL_TEXTURE_2D_MULTISAMPLE_ARRAY, or GL_TEXTURE_BUFFER, and level is
    not zero.
    
    Bug: angleproject:376497150
    Change-Id: Ib05ef30a04699e590804d1059387a6920c38971c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5982239
    Reviewed-by: Alexey Knyazev <lexa.knyazev@gmail.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/validationES.cpp
+++ src/libANGLE/validationES.cpp
@@ -1110,10 +1110,6 @@ bool ValidMipLevel(const Context *context, TextureType type, GLint level)
     {
         case TextureType::_2D:
         case TextureType::_2DArray:
-        case TextureType::_2DMultisample:
-        case TextureType::_2DMultisampleArray:
-            // TODO(http://anglebug.com/42261478): It's a bit unclear what the "maximum allowable
-            // level-of-detail" for multisample textures should be. Could maybe make it zero.
             maxDimension = caps.max2DTextureSize;
             break;
 
@@ -1126,6 +1122,8 @@ bool ValidMipLevel(const Context *context, TextureType type, GLint level)
         case TextureType::Rectangle:
         case TextureType::VideoImage:
         case TextureType::Buffer:
+        case TextureType::_2DMultisample:
+        case TextureType::_2DMultisampleArray:
             return level == 0;
 
         case TextureType::_3D:
