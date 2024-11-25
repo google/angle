4aaeffd8b998c470f0c7299988276eac5449309a
Kimmo Kinnunen <kkinnunen@apple.com>
2024-11-25 11:33:30 +0200

    Metal: Limit simulator texture size to 8k
    Regressed in commit d44204893a1725a74cd20b332fbb6f595b0975c3.
    
    Bug: angleproject:380712807
    Change-Id: Ic5e298412b35451e87b76f70d5b1b253ed1406cb
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6039222
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Auto-Submit: Kimmo Kinnunen <kkinnunen@apple.com>
    Commit-Queue: Kimmo Kinnunen <kkinnunen@apple.com>

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -716,9 +716,13 @@ void DisplayMtl::ensureCapsInitialized() const
     // https://developer.apple.com/metal/Metal-Feature-Set-Tables.pdf
     mNativeCaps.maxElementIndex  = std::numeric_limits<GLuint>::max() - 1;
     mNativeCaps.max3DTextureSize = 2048;
-#if TARGET_OS_OSX || TARGET_OS_MACCATALYST || TARGET_OS_SIMULATOR
+#if TARGET_OS_OSX || TARGET_OS_MACCATALYST
     mNativeCaps.max2DTextureSize = 16384;
     // On macOS exclude [[position]] from maxVaryingVectors.
+    mNativeCaps.maxVaryingVectors         = 31 - 1;
+    mNativeCaps.maxVertexOutputComponents = mNativeCaps.maxFragmentInputComponents = 124 - 4;
+#elif TARGET_OS_SIMULATOR
+    mNativeCaps.max2DTextureSize          = 8192;
     mNativeCaps.maxVaryingVectors         = 31 - 1;
     mNativeCaps.maxVertexOutputComponents = mNativeCaps.maxFragmentInputComponents = 124 - 4;
 #else
