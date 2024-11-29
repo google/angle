f5196a27b9b6bdc358191717f2b5a3ba824d1e82
Alexey Knyazev <lexa.knyazev@gmail.com>
2024-11-29 00:00:00 +0000

    Fix normalizedToFloat for signed types
    * Ensured that INT_MIN is converted to -1.
    
    * Fixed max value computation when the
      input bit width is not type-aligned.
    
    * Cleaned up D24X8::ReadDepthStencil
      to match the updated semantics.
    
    * Added tests for all variants.
    
    Fixed: angleproject:352963106
    Change-Id: I8b9f60d3364331567ad3b55276666bb416c3c6ac
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6072233
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Alexey Knyazev <lexa.knyazev@gmail.com>

--- src/image_util/imageformats.cpp
+++ src/image_util/imageformats.cpp
@@ -1923,7 +1923,7 @@ void D16::WriteDepthStencil(D16 *dst, const DepthStencil *src)
 
 void D24X8::ReadDepthStencil(DepthStencil *dst, const D24X8 *src)
 {
-    dst->depth = gl::normalizedToFloat<24>(src->D & 0x00ffffff);
+    dst->depth = gl::normalizedToFloat<24>(src->D);
 }
 
 void D24X8::WriteDepthStencil(D24X8 *dst, const DepthStencil *src)
