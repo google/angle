421109ac5be0ffe422bf1ab0c7a688d8c1abbade
Roman Lavrov <romanl@google.com>
2025-02-27 16:14:40 -0500

    Unpin aosp builds from C++17
    As Yuxin pointed out upstream libcxx was updated so we shouldn't need to
    pin in Android any more. We still can't have C++20 code in ANGLE due to
    Skia, so some of (Chromium CI) bots will continue building as C++17.
    
    Bug: angleproject:330910097
    Change-Id: I41a9a1f727ae47ccb450853b8a5ed0f0f5edd26d
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6309328
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>

--- scripts/generate_android_bp.py
+++ scripts/generate_android_bp.py
@@ -782,8 +782,6 @@ def main():
         {
             'name':
                 'angle_common_library_cflags',
-            'cpp_std':
-                'gnu++17',  # TODO(b/330910097): std::popcount missing from external/libcxx
             'cflags': [
                 # Chrome and Android use different versions of Clang which support differnt warning options.
                 # Ignore errors about unrecognized warning flags.
