35356d952d21febf41ee9385775a425b6e242246
Andrew Grieve <agrieve@chromium.org>
2025-05-06 12:27:32 -0400

    Use a depfile rather than exec_script to list inputs for commit_id.py
    Bug: chromium:416002047
    Change-Id: Ia7ddd4e5e8ff7cc1e88cce6542f62a314c8e6430
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6511928
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>

--- src/commit_id.py
+++ src/commit_id.py
@@ -6,17 +6,14 @@
 # Generate commit.h with git commit hash.
 #
 
+import pathlib
 import subprocess as sp
 import sys
 import os
 
 usage = """\
-Usage: commit_id.py check                - check if git is present
-       commit_id.py get_git_dirs         - prints work-tree and common git directories
-       commit_id.py unpack <ref_file>    - check if <ref_file> exists, and if not
-                                           create it based on .git/packed-refs
-       commit_id.py position             - print commit position
-       commit_id.py gen <file_to_write>  - generate commit.h"""
+Usage: commit_id.py position               - print commit position
+       commit_id.py gen <output> [depfile] - generate commit.h"""
 
 
 def grab_output(command, cwd):
@@ -62,6 +59,24 @@ def unpack_ref(ref_file, ref_file_full_path, packed_refs_full_path):
         fout.write(git_hash + '\n')
 
 
+def get_git_inputs_and_maybe_unpack_ref(cwd):
+    # commit id should depend on angle's HEAD revision
+    git_dir = os.path.normpath(os.path.join(cwd, get_git_dir(cwd)))
+    head_file = os.path.join(git_dir, 'HEAD')
+    ret = [head_file]
+    git_common_dir = os.path.normpath(os.path.join(cwd, get_git_common_dir(cwd)))
+    result = pathlib.Path(head_file).read_text().split()
+    if result[0] == "ref:":
+        ref_file = result[1]
+        ref_file_full_path = os.path.join(git_common_dir, ref_file)
+
+        if not os.path.exists(ref_file_full_path):
+            packed_refs_full_path = os.path.join(git_common_dir, 'packed-refs')
+            unpack_ref(ref_file, ref_file_full_path, packed_refs_full_path)
+        ret.append(os.path.join(git_common_dir, ref_file))
+    return ret
+
+
 if len(sys.argv) < 2:
     sys.exit(usage)
 
@@ -73,31 +88,7 @@ aosp = os.path.exists(aosp_angle_path)
 cwd = aosp_angle_path if aosp else os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 git_dir_exists = does_git_dir_exist(cwd)
 
-if operation == 'check':
-    if git_dir_exists:
-        print("1")
-    else:
-        print("0")
-    sys.exit(0)
-elif operation == 'get_git_dirs':
-    print(get_git_dir(cwd))
-    print(get_git_common_dir(cwd))
-    sys.exit(0)
-elif operation == 'unpack':
-    if len(sys.argv) < 3:
-        sys.exit(usage)
-
-    ref_file = sys.argv[2]
-    git_common_dir = get_git_common_dir(cwd)
-    ref_file_full_path = os.path.join(cwd, git_common_dir, ref_file)
-    ref_file_exists = os.path.exists(ref_file_full_path)
-
-    if not ref_file_exists:
-        packed_refs_full_path = os.path.join(cwd, git_common_dir, 'packed-refs')
-        unpack_ref(ref_file, ref_file_full_path, packed_refs_full_path)
-
-    sys.exit(0)
-elif operation == 'position':
+if operation == 'position':
     if git_dir_exists:
         print(get_commit_position(cwd))
     else:
@@ -108,6 +99,7 @@ if len(sys.argv) < 3 or operation != 'gen':
     sys.exit(usage)
 
 output_file = sys.argv[2]
+depfile = sys.argv[3] if len(sys.argv) == 4 else None
 commit_id_size = 12
 commit_date = 'unknown date'
 commit_position = '0'
@@ -126,12 +118,18 @@ if git_dir_exists and not commit_id:
     except:
         pass
 
-hfile = open(output_file, 'w')
+with open(output_file, 'w') as hfile:
+    hfile.write('#define ANGLE_COMMIT_HASH "%s"\n' % (commit_id or "unknown hash"))
+    hfile.write('#define ANGLE_COMMIT_HASH_SIZE %d\n' % commit_id_size)
+    hfile.write('#define ANGLE_COMMIT_DATE "%s"\n' % commit_date)
+    hfile.write('#define ANGLE_COMMIT_POSITION %s\n' % commit_position)
 
-hfile.write('#define ANGLE_COMMIT_HASH "%s"\n' % (commit_id or "unknown hash"))
-hfile.write('#define ANGLE_COMMIT_HASH_SIZE %d\n' % commit_id_size)
-hfile.write('#define ANGLE_COMMIT_DATE "%s"\n' % commit_date)
-hfile.write('#define ANGLE_COMMIT_POSITION %s\n' % commit_position)
-
-
-hfile.close()
+if depfile:
+    inputs = []
+    if git_dir_exists:
+        inputs = get_git_inputs_and_maybe_unpack_ref(cwd)
+    with open(depfile, 'w') as f:
+        f.write(output_file)
+        f.write(': ')
+        f.write(' '.join(os.path.relpath(p) for p in inputs))
+        f.write('\n')
