96492b317e27ba4106ed00f5faa4534cfeaa0b8f
Yuxin Hu <yuxinhu@google.com>
2025-08-13 13:35:02 -0700

    [M139] Do not sort ANGLE internal uniforms or built-in uniforms
    On Mac, for webGL app on chrome, if the uniforms added by ANGLE
    are not placed at the top of the ANGLE_UserUniforms struct, the user
    defined uniforms in the same ANGLE_UserUniforms struct seem to be
    messed up and not read correctly by shader code. This change fixes
    it by moving the sortUniforms() function call to the place before we
    add ANGLE internal uniforms. That way ANGLE internal uniforms will
    be inserted later and stay on top of ANGLE_UserUniforms struct.
    
    Bug: chromium:437678149
    Bug: chromium:438986381
    Change-Id: I34a0d3dcb98c5fe30ad248381ecb5f7f78252275
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6847281
    Auto-Submit: Yuxin Hu <yuxinhu@google.com>
    Commit-Queue: Yuxin Hu <yuxinhu@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    (cherry picked from commit 1d8279a2b7b6ce99ec7697d97db4eee3268bc52a)
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6854783

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -1143,6 +1143,17 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
         }
     }
 
+    // https://crbug.com/437678149:
+    // On Mac, if ANGLE internal uniforms are not placed on the top of ANGLE_UserUniforms struct,
+    // the other user-defined uniforms are not intercepted correctly by the shader code.
+    // Sort user-defined uniforms first before adding ANGLE internal uniforms like
+    // angle_DrawID on top of them, so that the sort doesn't reorder the ANGLE internal uniforms
+    // and trigger the bug on Mac.
+    if (!sortUniforms(root))
+    {
+        return false;
+    }
+
     if (mShaderType == GL_VERTEX_SHADER &&
         IsExtensionEnabled(mExtensionBehavior, TExtension::ANGLE_multi_draw))
     {
@@ -1307,10 +1318,6 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
     }
 
     ASSERT(!mVariablesCollected);
-    if (!sortUniforms(root))
-    {
-        return false;
-    }
     CollectVariables(root, &mAttributes, &mOutputVariables, &mUniforms, &mInputVaryings,
                      &mOutputVaryings, &mSharedVariables, &mUniformBlocks, &mShaderStorageBlocks,
                      mResources.HashFunction, &mSymbolTable, mShaderType, mExtensionBehavior,
