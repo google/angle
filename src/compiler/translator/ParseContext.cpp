ef9c5ce43110f4f8ba56124bdee927944ce3ddd1
Chris Dalton <chris@rive.app>
2025-09-07 17:50:37 -0600

    Add a "noncoherent" layout qualifier to PLS
    Raster ordering can be expensive. If an app knows the geometry in a draw
    does not overlap, it can mark the PLS planes as noncoherent and opt for
    using barriers instead.
    
    Bug: angleproject:40096838
    Change-Id: I2d686c2e64f51940face186637a9fcadcc251bef
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6923300
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Chris Dalton <chris@rive.app>

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -1480,7 +1480,8 @@ bool TParseContext::declareVariable(const TSourceLoc &line,
 
     if (!((identifier.beginsWith("gl_LastFragData") || type->getQualifier() == EvqFragmentInOut) &&
           (isExtensionEnabled(TExtension::EXT_shader_framebuffer_fetch) ||
-           isExtensionEnabled(TExtension::EXT_shader_framebuffer_fetch_non_coherent))))
+           isExtensionEnabled(TExtension::EXT_shader_framebuffer_fetch_non_coherent))) &&
+        !(type->isPixelLocal() && isExtensionEnabled(TExtension::ANGLE_shader_pixel_local_storage)))
     {
         checkNoncoherentIsNotSpecified(line, type->getLayoutQualifier().noncoherent);
     }
@@ -2439,8 +2440,8 @@ void TParseContext::checkNoncoherentIsNotSpecified(const TSourceLoc &location, b
     if (noncoherent != false)
     {
         error(location,
-              "invalid layout qualifier: only valid when used with 'gl_LastFragData' or the "
-              "variable decorated with 'inout' in a fragment shader",
+              "invalid layout qualifier: only valid when used with 'gl_LastFragData', the "
+              "variable decorated with 'inout' in a fragment shader, or pixel local storage",
               "noncoherent");
     }
 }
@@ -5944,10 +5945,10 @@ TLayoutQualifier TParseContext::parseLayoutQualifier(const ImmutableString &qual
         if (qualifierType == "noncoherent")
         {
             if (checkCanUseOneOfExtensions(
-                    qualifierTypeLine,
-                    std::array<TExtension, 2u>{
-                        {TExtension::EXT_shader_framebuffer_fetch,
-                         TExtension::EXT_shader_framebuffer_fetch_non_coherent}}))
+                    qualifierTypeLine, std::array<TExtension, 3u>{
+                                           {TExtension::EXT_shader_framebuffer_fetch,
+                                            TExtension::EXT_shader_framebuffer_fetch_non_coherent,
+                                            TExtension::ANGLE_shader_pixel_local_storage}}))
             {
                 checkLayoutQualifierSupported(qualifierTypeLine, qualifierType, 100);
                 qualifier.noncoherent = true;
--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -3764,6 +3764,7 @@ void Context::setExtensionEnabled(const char *name, bool enabled)
             enableIfRequestable("GL_EXT_draw_buffers_indexed");
             enableIfRequestable("GL_EXT_color_buffer_float");
             enableIfRequestable("GL_EXT_color_buffer_half_float");
+            enableIfRequestable("GL_EXT_shader_framebuffer_fetch_non_coherent");
             enableIfRequestable("GL_ANGLE_shader_pixel_local_storage_coherent");
             enableIfRequestable("GL_ANGLE_shader_pixel_local_storage");
         }
@@ -8993,11 +8994,6 @@ bool Context::areBlobCacheFuncsSet() const
 
 void Context::pixelLocalStorageBarrier()
 {
-    if (getExtensions().shaderPixelLocalStorageCoherentANGLE)
-    {
-        return;
-    }
-
     Framebuffer *framebuffer = mState.getDrawFramebuffer();
     ASSERT(framebuffer);
     PixelLocalStorage &pls = framebuffer->getPixelLocalStorage(this);
