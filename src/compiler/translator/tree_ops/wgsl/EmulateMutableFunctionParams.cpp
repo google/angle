c452d6ffa4b3c8231b1a07f5e333b6d45843f5da
Matthew Denton <mpdenton@chromium.org>
2025-09-10 17:09:50 -0700

    WGSL: Introduce temporaries for written-to func params
    WGSL's function parameters are always immutable, while GLSL's are
    immutable. Until https://github.com/gpuweb/gpuweb/issues/4113 is
    resolved, work around this with an AST pass.
    
    EmulateMutableFunctionParams: If function parameters are modified
    in the function body, they are replaced with temporary copies.
    
    Example:
    
    vec4 doFoo(Foo foo, float zw)
    {
        foo.x = foo.y;
        return vec4(foo.x, foo.y, zw, zw);
    }
    
    Result:
    
    vec4 doFoo(Foo foo, float zw)
    {
        Foo sbc7 = foo;
        sbc7.x = sbc7.y;
        return vec4(sbc7.x, sbc7.y, zw, zw);
    }
    
    Bug: angleproject:42267100
    Change-Id: Id86275156dabd309d527e7ef3ade1ec058dd8b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/6935273
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>

---
+++ src/compiler/translator/tree_ops/wgsl/EmulateMutableFunctionParams.cpp
@@ -0,0 +1,113 @@
+//
+// Copyright 2025 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// EmulateMutableFunctionParams: if any function params are written to, replace
+// them with a temp variable initialized at the start of the function with the value of the params,
+// because params are immutable in WGSL, for now.
+//
+
+#include "compiler/translator/tree_ops/wgsl/EmulateMutableFunctionParams.h"
+
+#include "common/log_utils.h"
+#include "compiler/translator/BaseTypes.h"
+#include "compiler/translator/Compiler.h"
+#include "compiler/translator/ImmutableStringBuilder.h"
+#include "compiler/translator/IntermNode.h"
+#include "compiler/translator/Operator_autogen.h"
+#include "compiler/translator/SymbolUniqueId.h"
+#include "compiler/translator/tree_util/DriverUniform.h"
+#include "compiler/translator/tree_util/IntermNode_util.h"
+#include "compiler/translator/tree_util/IntermTraverse.h"
+#include "compiler/translator/tree_util/Visit.h"
+
+namespace sh
+{
+namespace
+{
+
+bool IsParam(const TVariable *var)
+{
+    TQualifier q = var->getType().getQualifier();
+    return q == EvqParamIn || q == EvqParamInOut || q == EvqParamOut || q == EvqParamConst;
+}
+
+class EmulateMutableFunctionParamsTraverser : public TLValueTrackingTraverser
+{
+  public:
+    EmulateMutableFunctionParamsTraverser(TSymbolTable *symbolTable)
+        : TLValueTrackingTraverser(true, false, false, symbolTable)
+    {}
+
+    bool visitFunctionDefinition(Visit visit, TIntermFunctionDefinition *func) override
+    {
+        mCurrentFunc = func;
+        return true;
+    }
+
+    void visitSymbol(TIntermSymbol *node) override
+    {
+        const TVariable *paramVar = &node->variable();
+        // Only looking at params used within functions.
+        if (mInGlobalScope || !IsParam(paramVar))
+        {
+            return;
+        }
+
+        Param param{mCurrentFunc, paramVar};
+
+        // Keeps track of each param (TFunctionDefinition*, TVariable*) that is possibly written to.
+        if (isLValueRequiredHere())
+        {
+            mParamsWrittenTo.insert(param);
+        }
+
+        // Keep track of all param usage in the function, so later in traversal the param is written
+        // to and therefore will be replaced with a temporary, the traverser can replace this usage
+        // with a usage of the temporary.
+        mParamUsages[param].push_back({node, getParentNode()});
+    }
+
+    bool update(TCompiler *compiler, TIntermBlock *root)
+    {
+        for (const Param &param : mParamsWrittenTo)
+        {
+            // Declare the temporary and initialize it with the parameter.
+            TIntermDeclaration *tempVarDecl = nullptr;
+            TVariable *tempVar = DeclareTempVariable(mSymbolTable, new TIntermSymbol(param.second),
+                                                     EvqTemporary, &tempVarDecl);
+
+            // Put the declaration at the top of the function body.
+            insertStatementsInBlockAtPosition(param.first->getBody(), 0, {tempVarDecl}, {});
+
+            // Replace all the references to the parameter with references to the temp var.
+            for (const ParamUsageWithParent &paramUse : mParamUsages[param])
+            {
+                queueReplacementWithParent(paramUse.second, paramUse.first,
+                                           new TIntermSymbol(tempVar), OriginalNode::IS_DROPPED);
+            }
+        }
+
+        // Apply updates and validate
+        return updateTree(compiler, root);
+    }
+
+  private:
+    using Param                = std::pair<const TIntermFunctionDefinition *, const TVariable *>;
+    using ParamUsageWithParent = std::pair<TIntermSymbol *, TIntermNode *>;
+    TMap<Param, TVector<ParamUsageWithParent>> mParamUsages;
+    TSet<Param> mParamsWrittenTo;
+
+    TIntermFunctionDefinition *mCurrentFunc;
+};
+
+}  // anonymous namespace
+
+bool EmulateMutableFunctionParams(TCompiler *compiler, TIntermBlock *root)
+{
+    EmulateMutableFunctionParamsTraverser traverser(&compiler->getSymbolTable());
+    root->traverse(&traverser);
+    return traverser.update(compiler, root);
+}
+}  // namespace sh
