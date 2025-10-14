4b5f4da636b7e167481161761b1ede34513ae93c
Yuly Novikov <ynovikov@chromium.org>
2025-10-14 12:31:26 +0000

    Don't roll CLSPV binary blobs into Android.
    Bug: angleproject:440837006
    Change-Id: I2c0027503c08317d1027f46f683e037f3cef72a0
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7036480
    Auto-Submit: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -177,6 +177,7 @@ delete_after_codegen_paths=(
    "third_party/android_system_sdk"
    "third_party/android_toolchain"
    "third_party/bazel"
+   "third_party/clspv/gn"
    "third_party/colorama"
    "third_party/jdk/current"  # subdirs only to keep third_party/jdk/BUILD.gn (not pulled by gclient as it comes from ANGLE repo)
    "third_party/jdk/extras"
