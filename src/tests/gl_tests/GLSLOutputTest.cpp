19f5cb187fba22a800c3fadf63316886b6d27d8b
Shahbaz Youssefi <syoussefi@chromium.org>
2025-10-28 16:30:35 -0400

    Port gl_FragColor compiler output tests to end2end
    This change introduces GLSLOutputTest, where per-backend output checks
    can be done more easily (later subsuming WGSLOutput and MSLOutput tests
    for exmaple).
    
    Bug: angleproject:454531087
    Change-Id: Ied9fb341e73350de3e3a2d17402160bd9ddb1131
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7091935
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

---
+++ src/tests/gl_tests/GLSLOutputTest.cpp
@@ -0,0 +1,94 @@
+//
+// Copyright 2025 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#ifdef UNSAFE_BUFFERS_BUILD
+#    pragma allow_unsafe_buffers
+#endif
+
+#include "test_utils/CompilerTest.h"
+
+#include "test_utils/angle_test_configs.h"
+#include "test_utils/gl_raii.h"
+
+using namespace angle;
+
+namespace
+{
+class GLSLOutputTest : public CompilerTest
+{
+  protected:
+    GLSLOutputTest() {}
+
+    // Helper to create a shader
+    void compileShader(GLenum shaderType, const char *shaderSource)
+    {
+        const CompiledShader &shader = compile(shaderType, shaderSource);
+        EXPECT_TRUE(shader.success());
+    }
+
+    // Check that the output contains (or not) the expected substring.
+    void verifyIsInTranslation(GLenum shaderType, const char *expect)
+    {
+        EXPECT_TRUE(getCompiledShader(shaderType).verifyInTranslatedSource(expect)) << expect;
+    }
+    void verifyIsNotInTranslation(GLenum shaderType, const char *expect)
+    {
+        EXPECT_TRUE(getCompiledShader(shaderType).verifyNotInTranslatedSource(expect)) << expect;
+    }
+};
+
+class GLSLOutputGLSLTest : public GLSLOutputTest
+{};
+
+// Verifies that without explicitly enabling GL_EXT_draw_buffers extension in the shader, no
+// broadcast emulation.
+TEST_P(GLSLOutputGLSLTest, FragColorNoBroadcast)
+{
+    constexpr char kFS[] = R"(void main()
+{
+    gl_FragColor = vec4(1, 0, 0, 0);
+})";
+    compileShader(GL_FRAGMENT_SHADER, kFS);
+    verifyIsInTranslation(GL_FRAGMENT_SHADER, "gl_FragColor");
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[0]");
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[1]");
+}
+
+// Verifies that with explicitly enabling GL_EXT_draw_buffers extension
+// in the shader, broadcast is emualted by replacing gl_FragColor with gl_FragData.
+TEST_P(GLSLOutputGLSLTest, FragColorBroadcast)
+{
+    ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_EXT_draw_buffers"));
+
+    constexpr char kFS[] = R"(#extension GL_EXT_draw_buffers : require
+void main()
+{
+    gl_FragColor = vec4(1, 0, 0, 0);
+})";
+    compileShader(GL_FRAGMENT_SHADER, kFS);
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragColor");
+    verifyIsInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[0]");
+    verifyIsInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[1]");
+}
+
+// Verifies that with explicitly enabling GL_EXT_draw_buffers extension
+// in the shader with an empty main(), nothing happens.
+TEST_P(GLSLOutputGLSLTest, EmptyMain)
+{
+    ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_EXT_draw_buffers"));
+
+    constexpr char kFS[] = R"(#extension GL_EXT_draw_buffers : require
+void main()
+{
+})";
+    compileShader(GL_FRAGMENT_SHADER, kFS);
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragColor");
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[0]");
+    verifyIsNotInTranslation(GL_FRAGMENT_SHADER, "gl_FragData[1]");
+}
+}  // namespace
+
+ANGLE_INSTANTIATE_TEST(GLSLOutputGLSLTest, ES2_OPENGL(), ES2_OPENGLES());
