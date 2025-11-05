7da12a27312fabf95c54ff3156f08c9259628b84
Cody Northrop <cnorthrop@google.com>
2025-11-05 15:49:46 -0700

    Vulkan: Enable dynamic rendering on newer PowerVR drivers
    Test: Trace performance
    Bug: b/372273294
    Change-Id: I0dd92c815b1cde27ef0004ee26a3dbee2e2d8a4f
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7125873
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Commit-Queue: Cody Northrop <cnorthrop@google.com>

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -5197,6 +5197,11 @@ void Renderer::initFeatures(const vk::ExtensionNameList &deviceExtensionNames,
         driverVersion =
             angle::ParseSamsungVulkanDriverVersion(mPhysicalDeviceProperties.driverVersion);
     }
+    else if (isPowerVR)
+    {
+        driverVersion =
+            angle::ParseImaginationVulkanDriverVersion(mPhysicalDeviceProperties.driverVersion);
+    }
 
     // Classify devices based on general architecture:
     //
@@ -6445,7 +6450,7 @@ void Renderer::initFeatures(const vk::ExtensionNameList &deviceExtensionNames,
     // Use of dynamic rendering is disabled on older Qualcomm drivers due to driver bugs
     // (http://crbug.com/415738891).
     //
-    // Use of dynamic rendering on PowerVR devices is disabled for performance reasons
+    // Use of dynamic rendering is disabled on older PowerVR devices for performance reasons
     // (http://issuetracker.google.com/372273294).
     const bool hasLegacyDitheringV1 =
         mFeatures.supportsLegacyDithering.enabled &&
@@ -6460,7 +6465,7 @@ void Renderer::initFeatures(const vk::ExtensionNameList &deviceExtensionNames,
             !emulatesMultisampledRenderToTexture &&
             !(isARMProprietary && driverVersion < angle::VersionTriple(52, 0, 0)) &&
             !(isQualcommProprietary && driverVersion < angle::VersionTriple(512, 801, 0)) &&
-            !isPowerVR);
+            !(isPowerVR && driverVersion < angle::VersionTriple(1, 634, 0)));
 
     // On tile-based renderers, breaking the render pass is costly.  Changing into and out of
     // framebuffer fetch causes the render pass to break so that the layout of the color attachments
