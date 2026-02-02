48739e490fbcc602e4c86dac545e9fd32d0f9e6c
Gowtham Tammana <g.tammana@samsung.com>
2026-02-02 16:20:51 -0600

    CL/Vulkan: Arrange the device caps into categories
    Not all of the CLDevice caps can be translated to a Vulkan feature
    query. Organizing the cpas in sections as below
    
    - Ones that have corresponding Vulkan queries
    - Ones that are queryable through any vendor specific extensions
    - Ones that are Vulkan backend implementation details
    - Ones that are not supported by current implementation
    - Ones that are currently hard-coded to some known good values
    
    Bug: angleproject:505313376
    Tests-Passing: ocl_cts.test_api get_device_info
    Change-Id: I0777774e32972eac22857df52f24e629997e89bd
    Signed-off-by: Gowtham Tammana <g.tammana@samsung.com>
    Signed-off-by: Austin Annestrand <a.annestrand@samsung.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8252917
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>

--- src/libANGLE/renderer/vulkan/CLDeviceVk.cpp
+++ src/libANGLE/renderer/vulkan/CLDeviceVk.cpp
@@ -219,37 +219,40 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
         {cl::DeviceInfo::Profile, std::string("FULL_PROFILE")},
         {cl::DeviceInfo::OpenCL_C_Version, std::string("OpenCL C 1.2 ")},
         {cl::DeviceInfo::LatestConformanceVersionPassed, std::string("FIXME")}};
+
     mInfoSizeT = {
+        // Below caps are retrieved from Vulkan queries
         {cl::DeviceInfo::MaxWorkGroupSize, props.limits.maxComputeWorkGroupInvocations},
+        {cl::DeviceInfo::ProfilingTimerResolution, props.limits.timestampPeriod},
+
+        // Below caps are retrieved from vendor specific extensions if present
+        {cl::DeviceInfo::PreferredWorkGroupSizeMultiple, getWorkGroupSizeMultiple()},
+
+        // Below caps are not supported by current implementation
         {cl::DeviceInfo::MaxGlobalVariableSize, 0},
         {cl::DeviceInfo::GlobalVariablePreferredTotalSize, 0},
 
+        // Below caps are set to some known good values
         // TODO(aannestrand) Update these hardcoded platform/device queries
         // http://anglebug.com/42266935
         {cl::DeviceInfo::MaxParameterSize, 1024},
-        {cl::DeviceInfo::ProfilingTimerResolution, props.limits.timestampPeriod},
         {cl::DeviceInfo::PrintfBufferSize, 1024 * 1024},
-        {cl::DeviceInfo::PreferredWorkGroupSizeMultiple, getWorkGroupSizeMultiple()},
     };
 
     mInfoULong = {
+        // Below caps are retrieved from Vulkan queries
         {cl::DeviceInfo::MaxMemAllocSize, getMaxMemAllocSize()},
+        {cl::DeviceInfo::GlobalMemSize, getGlobalMemSize()},
         {cl::DeviceInfo::LocalMemSize, props.limits.maxComputeSharedMemorySize},
-        {cl::DeviceInfo::SVM_Capabilities, 0ULL},
-        {cl::DeviceInfo::QueueOnDeviceProperties, 0ULL},
-        {cl::DeviceInfo::PartitionAffinityDomain, 0ULL},
-        {cl::DeviceInfo::DeviceEnqueueCapabilities, 0ULL},
-        {cl::DeviceInfo::QueueOnHostProperties, CL_QUEUE_PROFILING_ENABLE},
 
-        // TODO(aannestrand) Update these hardcoded platform/device queries
-        // http://anglebug.com/42266935
+        // Below caps are retrieved from vendor specific extensions if present
+        {cl::DeviceInfo::SingleFpConfig, getSingleFpConfig()},
         {cl::DeviceInfo::HalfFpConfig, getHalfFpConfig()},
         {cl::DeviceInfo::DoubleFpConfig, getDoubleFpConfig()},
         {cl::DeviceInfo::GlobalMemCacheSize, getCacheSize()},
-        {cl::DeviceInfo::GlobalMemSize, getGlobalMemSize()},
-        // Constant buffer size is same as global variable size in SGPU
-        {cl::DeviceInfo::MaxConstantBufferSize, 1024 * 1024 * 1024ULL},
-        {cl::DeviceInfo::SingleFpConfig, getSingleFpConfig()},
+
+        // Below are Vulkan backend implementation details
+        {cl::DeviceInfo::QueueOnHostProperties, CL_QUEUE_PROFILING_ENABLE},
         {cl::DeviceInfo::AtomicMemoryCapabilities,
          CL_DEVICE_ATOMIC_ORDER_RELAXED | CL_DEVICE_ATOMIC_SCOPE_WORK_GROUP |
              CL_DEVICE_ATOMIC_ORDER_ACQ_REL | CL_DEVICE_ATOMIC_SCOPE_DEVICE |
@@ -260,6 +263,18 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
                                                       CL_DEVICE_ATOMIC_SCOPE_WORK_GROUP |
                                                       // non-mandatory
                                                       CL_DEVICE_ATOMIC_SCOPE_WORK_ITEM},
+
+        // Below caps are not supported by current implementation
+        {cl::DeviceInfo::SVM_Capabilities, 0ULL},
+        {cl::DeviceInfo::QueueOnDeviceProperties, 0ULL},
+        {cl::DeviceInfo::PartitionAffinityDomain, 0ULL},
+        {cl::DeviceInfo::DeviceEnqueueCapabilities, 0ULL},
+
+        // Below caps are set to some known good values
+        // TODO(aannestrand) Update these hardcoded platform/device queries
+        // http://anglebug.com/42266935
+        // Constant buffer size is same as global variable size in SGPU
+        {cl::DeviceInfo::MaxConstantBufferSize, 1024 * 1024 * 1024ULL},
     };
 
     cl_uint maxNumSubGroups = 0u;
@@ -272,15 +287,42 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
     }
 
     mInfoUInt = {
+        // Below caps are retrieved from Vulkan queries
         {cl::DeviceInfo::VendorID, props.vendorID},
+        {cl::DeviceInfo::GlobalMemCachelineSize,
+         static_cast<cl_uint>(props.limits.nonCoherentAtomSize)},
+        {cl::DeviceInfo::MaxNumSubGroups, maxNumSubGroups},
+        {cl::DeviceInfo::SubGroupIndependentForwardProgress,
+         maxNumSubGroups > 0 ? CL_TRUE : CL_FALSE},
+        {cl::DeviceInfo::AddressBits,
+         mRenderer->getFeatures().supportsBufferDeviceAddress.enabled ? 64 : 32},
+
+        // Below caps are retrieved from vendor specific extensions if present
+        {cl::DeviceInfo::MaxComputeUnits, getNumComputeUnits()},
+        // Frequency is reported in MHz
+        {cl::DeviceInfo::MaxClockFrequency, 555},
+        // Report the number of CU's as max sub devices for now
+        {cl::DeviceInfo::PartitionMaxSubDevices, getNumComputeUnits()},
+
+        // Below are currently setup as Vulkan backend implementation details
         {cl::DeviceInfo::MaxReadImageArgs, cl::IMPLEMENTATION_MAX_READ_IMAGES},
         {cl::DeviceInfo::MaxWriteImageArgs, cl::IMPLEMENTATION_MAX_WRITE_IMAGES},
         {cl::DeviceInfo::MaxReadWriteImageArgs, cl::IMPLEMENTATION_MAX_WRITE_IMAGES},
-        {cl::DeviceInfo::GlobalMemCachelineSize,
-         static_cast<cl_uint>(props.limits.nonCoherentAtomSize)},
         {cl::DeviceInfo::Available, CL_TRUE},
         {cl::DeviceInfo::LinkerAvailable, CL_TRUE},
         {cl::DeviceInfo::CompilerAvailable, CL_TRUE},
+        {cl::DeviceInfo::ExecutionCapabilities, CL_EXEC_KERNEL},
+        {cl::DeviceInfo::PreferredInteropUserSync, CL_TRUE},
+        {cl::DeviceInfo::GlobalMemCacheType, CL_READ_WRITE_CACHE},
+        {cl::DeviceInfo::HostUnifiedMemory, CL_TRUE},
+        // TODO(aannestrand) Update these hardcoded platform/device queries
+        // http://anglebug.com/42266935
+        {cl::DeviceInfo::EndianLittle, CL_TRUE},
+        {cl::DeviceInfo::LocalMemType, CL_LOCAL},
+        {cl::DeviceInfo::MaxWorkItemDimensions, 3},
+        {cl::DeviceInfo::NonUniformWorkGroupSupport, CL_TRUE},
+
+        // Below caps are not supported by current implementation
         {cl::DeviceInfo::MaxOnDeviceQueues, 0},
         {cl::DeviceInfo::MaxOnDeviceEvents, 0},
         {cl::DeviceInfo::QueueOnDeviceMaxSize, 0},
@@ -290,27 +332,15 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
         {cl::DeviceInfo::PipeSupport, CL_FALSE},
         {cl::DeviceInfo::PipeMaxActiveReservations, 0},
         {cl::DeviceInfo::ErrorCorrectionSupport, CL_FALSE},
-        {cl::DeviceInfo::PreferredInteropUserSync, CL_TRUE},
-        {cl::DeviceInfo::ExecutionCapabilities, CL_EXEC_KERNEL},
+        {cl::DeviceInfo::GenericAddressSpaceSupport, CL_FALSE},
+        {cl::DeviceInfo::WorkGroupCollectiveFunctionsSupport, CL_FALSE},
 
-        // TODO(aannestrand) Update these hardcoded platform/device queries
-        // http://anglebug.com/42266935
-        {cl::DeviceInfo::AddressBits,
-         mRenderer->getFeatures().supportsBufferDeviceAddress.enabled ? 64 : 32},
-        {cl::DeviceInfo::EndianLittle, CL_TRUE},
-        {cl::DeviceInfo::LocalMemType, CL_LOCAL},
+        // Below caps are set to some known good values
         // TODO (http://anglebug.com/379669750) Vulkan reports a big sampler count number, we dont
         // need that many and set it to minimum req for now.
         {cl::DeviceInfo::MaxSamplers, 16u},
         {cl::DeviceInfo::MaxConstantArgs, 8},
-        {cl::DeviceInfo::MaxNumSubGroups, maxNumSubGroups},
-        {cl::DeviceInfo::MaxComputeUnits, getNumComputeUnits()},
-        // Frequency is reported in MHz
-        {cl::DeviceInfo::MaxClockFrequency, 555},
-        {cl::DeviceInfo::MaxWorkItemDimensions, 3},
         {cl::DeviceInfo::MinDataTypeAlignSize, 128},
-        {cl::DeviceInfo::GlobalMemCacheType, CL_READ_WRITE_CACHE},
-        {cl::DeviceInfo::HostUnifiedMemory, CL_TRUE},
         {cl::DeviceInfo::NativeVectorWidthChar, 4},
         {cl::DeviceInfo::NativeVectorWidthShort, 2},
         {cl::DeviceInfo::NativeVectorWidthInt, 1},
@@ -318,8 +348,6 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
         {cl::DeviceInfo::NativeVectorWidthFloat, 1},
         {cl::DeviceInfo::NativeVectorWidthDouble, mRenderer->getNativeVectorWidthDouble()},
         {cl::DeviceInfo::NativeVectorWidthHalf, mRenderer->getNativeVectorWidthHalf()},
-        // Report the number of CU's as max sub devices for now
-        {cl::DeviceInfo::PartitionMaxSubDevices, getNumComputeUnits()},
         {cl::DeviceInfo::PreferredVectorWidthChar, 4},
         {cl::DeviceInfo::PreferredVectorWidthShort, 8},
         {cl::DeviceInfo::PreferredVectorWidthInt, 1},
@@ -330,11 +358,6 @@ CLDeviceVk::CLDeviceVk(const cl::Device &device, vk::Renderer *renderer)
         {cl::DeviceInfo::PreferredLocalAtomicAlignment, 0},
         {cl::DeviceInfo::PreferredGlobalAtomicAlignment, 0},
         {cl::DeviceInfo::PreferredPlatformAtomicAlignment, 0},
-        {cl::DeviceInfo::NonUniformWorkGroupSupport, CL_TRUE},
-        {cl::DeviceInfo::GenericAddressSpaceSupport, CL_FALSE},
-        {cl::DeviceInfo::SubGroupIndependentForwardProgress,
-         maxNumSubGroups > 0 ? CL_TRUE : CL_FALSE},
-        {cl::DeviceInfo::WorkGroupCollectiveFunctionsSupport, CL_FALSE},
     };
 }
 
