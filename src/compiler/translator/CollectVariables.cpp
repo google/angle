8807d22fb5c5cb62eb4c8233abfb0d5c6af1e999
Shahbaz Youssefi <syoussefi@chromium.org>
2026-02-27 13:44:02 -0500

    Translator: Mark all fields of blocks as active or none
    Previously, the translator would identify exactly which fields of a
    UBO or SSBO is active.  This is rather useless, since the inactive
    fields cannot be removed or otherwise ignored.
    
    This change simplifies variable collection by making any access to any
    field of a block mark all the fields as active.  For the IR, this
    prevents it from having to track specifically which fields are active.
    
    Bug: angleproject:349994211
    Change-Id: I6d114aca50d29c3eef791df0193ea28eeb17bae6
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7617591
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/compiler/translator/CollectVariables.cpp
+++ src/compiler/translator/CollectVariables.cpp
@@ -1278,9 +1278,7 @@ bool CollectVariablesTraverser::visitBinary(Visit, TIntermBinary *binaryNode)
         // NOTE: we do not determine static use / activeness for individual blocks of an array.
         TIntermTyped *blockNode = binaryNode->getLeft()->getAsTyped();
         ASSERT(blockNode);
-
-        TIntermConstantUnion *constantUnion = binaryNode->getRight()->getAsConstantUnion();
-        ASSERT(constantUnion);
+        ASSERT(binaryNode->getRight()->getAsConstantUnion());
 
         InterfaceBlock *namedBlock = nullptr;
 
@@ -1333,13 +1331,18 @@ bool CollectVariablesTraverser::visitBinary(Visit, TIntermBinary *binaryNode)
             ASSERT(namedBlock);
             ASSERT(namedBlock->staticUse);
             namedBlock->active      = true;
-            unsigned int fieldIndex = static_cast<unsigned int>(constantUnion->getIConst(0));
-            ASSERT(fieldIndex < namedBlock->fields.size());
-            // TODO(oetuaho): Would be nicer to record static use of fields of named interface
-            // blocks more accurately at parse time - now we only mark the fields statically used if
-            // they are active. http://anglebug.com/42261150 We need to mark this field and all of
-            // its sub-fields, as static/active
-            MarkActive(&namedBlock->fields[fieldIndex]);
+            // Even though a single field is identified to be active, mark all the fields active.
+            // The layout of the block cannot be changed by the translator based on which fields are
+            // active, and there isn't anything else that ANGLE or the application would
+            // realistically do with a knowledge that some fields may be inactive.
+            for (size_t fieldIndex = 0; fieldIndex < namedBlock->fields.size(); ++fieldIndex)
+            {
+                // TODO(oetuaho): Would be nicer to record static use of fields of named interface
+                // blocks more accurately at parse time - now we only mark the fields statically
+                // used if they are active. http://anglebug.com/42261150 We need to mark this field
+                // and all of its sub-fields, as static/active
+                MarkActive(&namedBlock->fields[fieldIndex]);
+            }
         }
 
         if (traverseIndexExpression)
