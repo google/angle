google/angle

commit 90fb0aa48cd07da57b5d34310f8aaae0cdf40456

IR: Validate that if conditions are boolean
Bug: angleproject:349994211
Change-Id: I7663a79095e4ef8e590d4068a6792d6c60828e0f
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7727804
Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
Reviewed-by: Yuxin Hu <yuxinhu@google.com>

src/compiler/translator/ir/src/validator.rs
@@ -32,6 +32,7 @@
 //
 // Instructions:
 //   - Access to struct fields are in bounds: validate_struct_field_in_bounds()
+//   - If conditions are boolean: validate_if_condition_is_bool()
 
 // TODO(http://anglebug.com/349994211): to validate:
 //   - Referenced IDs are not dead-code-eliminated: Checking against max_*_count can be paird with a
@@ -48,7 +49,6 @@
 //   - Pointers only valid in the left arg of load/store/access/call
 //   - Loop blocks ends in the appropriate instructions.
 //   - Do blocks end in DoLoop (unless already terminated by something else, like Return)
-//   - If condition is a bool.
 //   - Maximum one default case for Switch instructions.
 //   - No pointer->pointer type.
 //   - Interface variables with NameSource::Internal are unique.
@@ -352,7 +352,7 @@ impl<'a> Validator<'a> {
     fn validate_all_ids_in_a_constant_are_present(&self, constant_id: u32, constant: &Constant) {
         if constant.type_id.id >= self.max_type_count {
             self.on_error(format_args!(
-                "Constant id {constant_id} has invalid type id {}",
+                "constant id {constant_id} has invalid type id {}",
                 constant.type_id.id
             ));
         }
@@ -363,7 +363,7 @@ impl<'a> Validator<'a> {
         // Check type_id
         if variable.type_id.id >= self.max_type_count {
             self.on_error(format_args!(
-                "Variable id {variable_id} has invalid type id {}",
+                "variable id {variable_id} has invalid type id {}",
                 variable.type_id.id
             ));
         }
@@ -372,7 +372,7 @@ impl<'a> Validator<'a> {
             && valid_initializer.id >= self.max_constant_count
         {
             self.on_error(format_args!(
-                "Variable id {variable_id} has invalid constant initializer {}",
+                "variable id {variable_id} has invalid constant initializer {}",
                 valid_initializer.id
             ));
         }
@@ -383,7 +383,7 @@ impl<'a> Validator<'a> {
         // Check return type
         if function.return_type_id.id >= self.max_type_count {
             self.on_error(format_args!(
-                "Function id {function_id} has invalid return type {}",
+                "function id {function_id} has invalid return type {}",
                 function.return_type_id.id
             ));
         }
@@ -392,7 +392,7 @@ impl<'a> Validator<'a> {
         for param in &function.params {
             if param.variable_id.id >= self.max_variable_count {
                 self.on_error(format_args!(
-                    "Function id {function_id} has invalid parameters {}",
+                    "function id {function_id} has invalid parameters {}",
                     param.variable_id.id
                 ));
             }
@@ -759,7 +759,7 @@ impl<'a> Validator<'a> {
                     }
                     TypedIdValidationCategory::RegisterDeclared => {
                         let declared_register_tracker = declared_registers.expect(
-                            "Expecting valid DeclaredRegisterTracker provided for \
+                            "expecting valid DeclaredRegisterTracker provided for \
                              RegisterDeclared category",
                         );
                         if !declared_register_tracker.is_declared(register_id) {
@@ -802,7 +802,7 @@ impl<'a> Validator<'a> {
 
                 TypedIdValidationCategory::VariableDeclared => {
                     let declared_variables_tracker = declared_variables.expect(
-                        "Expecting valid DeclaredVarTracker provided for VariableDeclared category",
+                        "expecting valid DeclaredVarTracker provided for VariableDeclared category",
                     );
                     if !declared_variables_tracker.is_variable_declared(variable_id) {
                         self.on_error(format_args!(
@@ -998,7 +998,7 @@ impl<'a> Validator<'a> {
             }
             if !seen_types.insert(ir_type) {
                 self.on_error(format_args!(
-                    "Identical type {:?} found with different IDs",
+                    "identical type {:?} found with different IDs",
                     ir_type
                 ));
             }
@@ -1338,7 +1338,7 @@ impl<'a> Validator<'a> {
             }
             OpCode::LoopIf(_) if !parent_block_meta_data.is_loop => {
                 self.on_error(format_args!(
-                    "The block ends with OpCode::LoopIf must be immediate child of the loop that \
+                    "the block ends with OpCode::LoopIf must be immediate child of the loop that \
                      ends with OpCode::Loop"
                 ));
             }
@@ -1533,6 +1533,7 @@ impl<'a> Validator<'a> {
             &|_, instruction| {
                 let (opcode, _result) = instruction.get_op_and_result(&self.ir.meta);
                 self.validate_struct_field_in_bounds(opcode);
+                self.validate_if_condition_is_bool(opcode);
             },
         );
     }
@@ -1557,5 +1558,19 @@ impl<'a> Validator<'a> {
                 struct_type, field_index
             ));
         }
+    }
+
+    fn validate_if_condition_is_bool(&self, opcode: &OpCode) {
+        match *opcode {
+            OpCode::If(condition) | OpCode::LoopIf(condition)
+                if condition.type_id != TYPE_ID_BOOL =>
+            {
+                self.on_error(format_args!(
+                    "invalid If/LoopIf instruction: {:?}, consition is not a boolean: {:?}",
+                    opcode, condition.type_id
+                ));
+            }
+            _ => (),
+        };
     }
 }

commit 859bc83bd251464714c0632408b07695f6f98835

fix(ir/validator): fix crash + typos in validator.rs
1. BUG FIX (crash): validate_no_branch_in_the_middle_of_block_instructions() calls
   block.instructions[0..block.instructions.len() - 1] which panics in debug mode
   (integer underflow) when block.instructions is empty. Fixed by using
   .saturating_sub(1) so that an empty slice is returned for empty instruction lists.

2. fix typo: "consition" -> "condition" in validate_if_condition_is_bool error message
   (introduced in commit 90fb0aa)

3. fix typo: "variabls" -> "variables" in validate_all_ids_are_present_in_block error
   message

4. fix typo: "at lease 1" -> "at least 1" in OpCode::Switch validation error message

5. fix typo: "paird" -> "paired" in TODO comment

src/compiler/translator/ir/src/validator.rs
@@ -35,7 +35,7 @@
 //   - If conditions are boolean: validate_if_condition_is_bool()
 
 // TODO(http://anglebug.com/349994211): to validate:
-//   - Referenced IDs are not dead-code-eliminated: Checking against max_*_count can be paird with a
+//   - Referenced IDs are not dead-code-eliminated: Checking against max_*_count can be paired with a
 //     look up and checking that !is_dead_code_eliminated
 //   - If there's a cached "has side effect", that it's correct.
 //   - No operations should have entirely constant arguments, that should be folded (and
@@ -408,7 +408,7 @@ impl<'a> Validator<'a> {
         for variable in &block.variables {
             if variable.id >= self.max_variable_count {
                 self.on_error(format_args!(
-                    "invalid variable id {} found in block variabls",
+                    "invalid variable id {} found in block variables",
                     variable.id
                 ));
             }
@@ -1286,7 +1286,7 @@ impl<'a> Validator<'a> {
                 if current_block.case_blocks.is_empty() {
                     self.on_error(format_args!(
                         "OpCode::Switch is missing a valid target. Current block should contain \
-                         at lease 1 case_block"
+                         at least 1 case_block"
                     ));
                 }
                 if current_block.merge_block.is_none() {
@@ -1452,7 +1452,7 @@ impl<'a> Validator<'a> {
     }
 
     fn validate_no_branch_in_the_middle_of_block_instructions(&self, block: &Block) {
-        for instruction in &block.instructions[0..block.instructions.len() - 1] {
+        for instruction in &block.instructions[0..block.instructions.len().saturating_sub(1)] {
             if instruction.is_branch() {
                 self.on_error(format_args!(
                     "branch instruction is only allowed in the last instruction in the block"
@@ -1566,7 +1566,7 @@ impl<'a> Validator<'a> {
                 if condition.type_id != TYPE_ID_BOOL =>
             {
                 self.on_error(format_args!(
-                    "invalid If/LoopIf instruction: {:?}, consition is not a boolean: {:?}",
+                    "invalid If/LoopIf instruction: {:?}, condition is not a boolean: {:?}",
                     opcode, condition.type_id
                 ));
             }