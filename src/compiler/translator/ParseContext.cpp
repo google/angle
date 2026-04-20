2e0c61b40a8bf74945368f0e58bd8c965ef73a19
Shahbaz Youssefi <syoussefi@chromium.org>
2026-04-20 11:37:22 -0400

    M148: Translator: Avoid setting initializer for too-large variable
    Test credit kbr@chromium.org / gemini.
    
    Fixed: chromium:498400132
    Change-Id: Id5cd5f7d05e22b631f6142483de5d1ad62b6bc53
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7818823
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -1706,7 +1706,7 @@ void TParseContext::checkDeclarationIsValidArraySize(const TSourceLoc &line,
     }
 }
 
-void TParseContext::checkVariableSize(const TSourceLoc &line,
+bool TParseContext::checkVariableSize(const TSourceLoc &line,
                                       const ImmutableString &identifier,
                                       const TType *type)
 {
@@ -1724,7 +1724,7 @@ void TParseContext::checkVariableSize(const TSourceLoc &line,
     if (!mCompileOptions.rejectWebglShadersWithLargeVariables || numErrors() > 0 ||
         (mShaderType != GL_VERTEX_SHADER && mShaderType != GL_FRAGMENT_SHADER))
     {
-        return;
+        return true;
     }
 
     // Note: the only allowed interface block in webgl shaders is UBOs in std140 mode, so the size
@@ -1737,7 +1737,7 @@ void TParseContext::checkVariableSize(const TSourceLoc &line,
     if (variableSize > kWebGLMaxVariableSizeInBytes)
     {
         error(line, "Size of declared variable exceeds implementation-defined limit", identifier);
-        return;
+        return false;
     }
 
     switch (type->getQualifier())
@@ -1783,13 +1783,14 @@ void TParseContext::checkVariableSize(const TSourceLoc &line,
                 error(line,
                       "Size of declared private variable exceeds implementation-defined limit",
                       identifier);
-                return;
+                return false;
             }
             mTotalPrivateVariablesSize += variableSize;
             break;
         default:
             break;
     }
+    return true;
 }
 
 void TParseContext::checkVaryingLocations(const TSourceLoc &line, const TVariable *variable)
@@ -2145,7 +2146,10 @@ bool TParseContext::declareVariable(const TSourceLoc &line,
         return false;
     }
 
-    checkVariableSize(line, identifier, type);
+    if (!checkVariableSize(line, identifier, type))
+    {
+        return false;
+    }
     checkVariableLocations(line, *variable);
 
     // Declare the variable in IR
