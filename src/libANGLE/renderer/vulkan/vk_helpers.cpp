6a8a23a2ec210585cb2e92a7a286b26e6e5cc0b5
Shahbaz Youssefi <syoussefi@chromium.org>
2026-05-04 14:14:49 -0400

    M148: Reland "Vulkan: Use 64-bit resource serials"
    This is a reland of commit d0875fb5c481dfc471ab0a6b4911dbc50c20987a
    
    Original change's description:
    > Vulkan: Use 64-bit resource serials
    >
    > This makes serial wraparound impossible until the next species evolves.
    > If an instance of ANGLE is running till then, it might crash.
    >
    > So you know, remember to restart every million years.
    >
    > Bug: chromium:498804020
    > Change-Id: I1baf152196fed0f228f40deaa452abca19224bbf
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7812906
    > Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    > Auto-Submit: Shahbaz Youssefi <syoussefi@chromium.org>
    > Reviewed-by: Charlie Lao <cclao@google.com>
    
    Bug: chromium:498804020
    Change-Id: I6ee09b5ebdedd4881e628fdae30940d84d992b60
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7871948
    Reviewed-by: Yuxin Hu <yuxinhu@google.com>

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -12785,7 +12785,7 @@ ImageOrBufferViewSubresourceSerial ImageViewHelper::getSubresourceSerialForColor
 {
     ASSERT(mImageViewSerial.valid());
 
-    ImageOrBufferViewSubresourceSerial serial;
+    ImageOrBufferViewSubresourceSerial serial = {};
     serial.viewSerial  = mImageViewSerial;
     serial.subresource = MakeImageSubresourceReadRange(levelGL, levelCount, layer, layerMode,
                                                        readColorspace, mWriteColorspace);
