067cceb8c9cf3b4c9a12b16385aa728b0e840941
kylechar <kylechar@google.com>
2026-05-12 16:09:36 -0400

    [M149] Track default attribute invalidation
    VertexArrayVk caches pointers to BufferHelper for default attributes
    which persist when the VAO is unbound and then rebound. If the
    BufferHelper for the default attribute is reallocated when a different
    VAO is bound the cached pointers are dangling. Ensure that cached
    pointers are reset before they are dereferenced once VAO is rebound.
    
    Bug: chromium:502812366
    Change-Id: Idfed3e962b4dab17afdaa8d1a7a8239d108ce78b
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7822999
    Reviewed-by: Charlie Lao <cclao@google.com>
    Auto-Submit: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Charlie Lao <cclao@google.com>
    (cherry picked from commit 21bfbfcae6d59268eda14f33e89120d7bb828376)
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7872095
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -828,6 +828,7 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, vk::Rendere
       mFlipViewportForDrawFramebuffer(false),
       mFlipViewportForReadFramebuffer(false),
       mIsAnyHostVisibleBufferWritten(false),
+      mDefaultAttribsGeneration(mDefaultAttribsGenerationFactory.generate()),
       mImageWithTileMemory(nullptr),
       mCurrentQueueSerialIndex(kInvalidQueueSerialIndex),
       mInitialContextPriority(renderer->getDriverPriority(GetContextPriority(state))),
@@ -2129,12 +2130,20 @@ angle::Result ContextVk::handleDirtyEventLogImpl(CommandBufferT *commandBuffer)
 angle::Result ContextVk::handleDirtyGraphicsDefaultAttribs(DirtyBits::Iterator *dirtyBitsIterator,
                                                            DirtyBits dirtyBitMask)
 {
-    ASSERT(mDirtyDefaultAttribsMask.any());
     VertexArrayVk *vertexArrayVk = getVertexArray();
 
-    gl::AttributesMask attribsMask = mDirtyDefaultAttribsMask;
-    attribsMask &= ~vertexArrayVk->getCurrentEnabledAttribsMask();
-    attribsMask &= mState.getProgramExecutable()->getAttributesMask();
+    gl::AttributesMask attribsMask;
+    if (vertexArrayVk->getDefaultAttribsGeneration() != mDefaultAttribsGeneration)
+    {
+        vertexArrayVk->setDefaultAttribsGeneration(mDefaultAttribsGeneration);
+        attribsMask = ~vertexArrayVk->getCurrentEnabledAttribsMask();
+    }
+    else
+    {
+        attribsMask = mDirtyDefaultAttribsMask;
+        attribsMask &= ~vertexArrayVk->getCurrentEnabledAttribsMask();
+        attribsMask &= mState.getProgramExecutable()->getAttributesMask();
+    }
 
     for (size_t attribIndex : attribsMask)
     {
@@ -5750,6 +5759,10 @@ angle::Result ContextVk::syncState(const gl::Context *context,
                 break;
             case gl::state::DIRTY_BIT_VERTEX_ARRAY_BINDING:
             {
+                if (vertexArrayVk->getDefaultAttribsGeneration() != mDefaultAttribsGeneration)
+                {
+                    mGraphicsDirtyBits.set(DIRTY_BIT_DEFAULT_ATTRIBS);
+                }
                 invalidateDefaultAttributes(context->getActiveDefaultAttribsMask());
                 ANGLE_TRY(onVertexArrayChange(vertexArrayVk->getCurrentEnabledAttribsMask()));
                 ANGLE_TRY(onIndexBufferChange(vertexArrayVk->getCurrentElementArrayBuffer()));
