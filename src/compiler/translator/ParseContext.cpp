c5ceb2bed276e2f178424046583de265dbe36c32
Shahbaz Youssefi <syoussefi@chromium.org>
2026-05-15 09:27:48 -0400

    [M144-LTS] Translator: Disallow structs-with-sampelrs in constructors
    Bug: chromium:513001247
    Change-Id: I7bf0ed6d7a3a12588ca523348531679dcbc6ad89
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7852696
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    (cherry picked from commit 77daa7a3be822e5446122a9500cf61c2b9eaf38b)
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8051305
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -1142,6 +1142,12 @@ bool TParseContext::checkConstructorArguments(const TSourceLoc &line,
             error(line, reason.c_str(), "constructor");
             return false;
         }
+        else if (argTyped->getType().isStructureContainingSamplers())
+        {
+            std::string reason("cannot convert a variable with struct type containing samplers");
+            error(line, reason.c_str(), "constructor");
+            return false;
+        }
         else if (argTyped->getMemoryQualifier().writeonly)
         {
             error(line, "cannot convert a variable with writeonly", "constructor");
