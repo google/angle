c112b4eef2a22fc068aceab8b1d4d40b3923c6a4
Kenneth Russell <kbr@chromium.org>
2026-05-19 10:39:42 -0700

    Revert "Make "packed" and "shared" reserved keywords in WebGL shaders."
    This reverts commit 5f0e794041ecec196ea148d3ee08db5ed4c2fc02.
    
    Reason for revert: broke miro.com boards in crbug.com/513076578 .
    
    Original change's description:
    > Make "packed" and "shared" reserved keywords in WebGL shaders.
    >
    > WebGL only supports std140 layout, and using the "packed" and "shared"
    > keywords as identifiers causes invalid code to be generated in various
    > backends.
    >
    > Co-authored with jetski-cli.
    >
    > Fixed: chromium:507987648
    > Change-Id: Ib004f5310e07c0f197d924637cd7a1c70037072e
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7810112
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    > Auto-Submit: Kenneth Russell <kbr@chromium.org>
    > Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    
    Bug: chromium:507987648
    Bug: chromium:513076578
    Change-Id: Ic9bbd6f4105f221e80e1345ec35b9b7712b89fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7860429
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/compiler/translator/glslang_lex_autogen.cpp
+++ src/compiler/translator/glslang_lex_autogen.cpp
@@ -1507,10 +1507,6 @@ YY_DECL
                 case 29:
                     YY_RULE_SETUP
                     {
-                        if (IsWebGLBasedSpec(context->getShaderSpec()))
-                        {
-                            return reserved_word(yyscanner);
-                        }
                         return ES3_1_keyword(context, SHARED);
                     }
                     YY_BREAK
@@ -2213,8 +2209,7 @@ YY_DECL
                 case 151:
                     YY_RULE_SETUP
                     {
-                        if (context->getShaderVersion() >= 300 &&
-                            !IsWebGLBasedSpec(context->getShaderSpec()))
+                        if (context->getShaderVersion() >= 300)
                         {
                             yylval->lex.string = AllocatePoolCharArray(yytext, yyleng);
                             return check_type(yyscanner);
