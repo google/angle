31c01933e70803f97263bd007b2a0fff0fa2b21c
Cody Northrop <cnorthrop@google.com>
2026-08-04 21:15:36 -0600

    Scripts: Switch roll_aosp.sh to vpython3
    gclient now requires a hermetic version of python3.
    Switch to vpython3 (which is available in depot_tools).
    
    Test: scripts/roll_aosp.sh
    Bug: b/542790095
    Change-Id: I28ffcafbb2aee319e0cb9e8cc981bb3972e9e82c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8199724
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Cody Northrop <cnorthrop@google.com>

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -110,7 +110,7 @@ function generate_Android_bp_file() {
         gn desc ${GN_OUTPUT_DIRECTORY} --format=json "*" > ${GN_OUTPUT_DIRECTORY}/desc.$abi.json
     done
 
-    python3 scripts/generate_android_bp.py \
+    vpython3 scripts/generate_android_bp.py \
         --gn_json_arm=${GN_OUTPUT_DIRECTORY}/desc.arm.json \
         --gn_json_arm64=${GN_OUTPUT_DIRECTORY}/desc.arm64.json \
         --gn_json_x86=${GN_OUTPUT_DIRECTORY}/desc.x86.json \
@@ -129,7 +129,7 @@ function generate_angle_commit_file() {
     # variable is set to {rolling_to} git hash, and that can be used by below
     # script commit_id.py as the ANGLE_COMMIT_HASH written to the angle_commit.h.
     # See b/348044346.
-    python3 src/commit_id.py \
+    vpython3 src/commit_id.py \
         gen \
         angle_commit.h
 }
@@ -333,7 +333,7 @@ find third_party -wholename "*/_gclient_*" -delete
 rm -rf "third_party/zlib"
 
 # Sync all of ANGLE's deps so that 'gn gen' works
-python3 scripts/bootstrap.py
+vpython3 scripts/bootstrap.py
 gclient sync --reset --force --delete_unversioned_trees
 
 # Delete outdir to ensure a clean gn run.
