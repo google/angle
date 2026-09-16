cd26970bfada33ab7d869868441cc8665555fdf8
Mikael Nilsson <mikaeln@google.com>
2026-09-16 11:21:59 +0000

    Vulkan: Fix VMA vkGetPhysicalDeviceProperties2KHR check
    Commit 80d793462953 (crrev.com/c/8120970) added an initialization for
    vkGetPhysicalDeviceProperties2KHR guarded by VMA_VERSION.
    
    However, VMA_VERSION expands via VK_MAKE_API_VERSION (or
    VK_MAKE_VERSION), which contains C-style (uint32_t) casts. These casts
    cause preprocessor syntax errors (#if token '(' is not valid in
    preprocessor subexpressions) when compiling against certain Vulkan
    headers.
    
    Guard funcs.vkGetPhysicalDeviceProperties2KHR using
    VMA_GET_PHYSICAL_DEVICE_PROPERTIES2 instead, which is the macro VMA
    itself defines (starting in VMA 3.4.0) to declare the struct member.
    
    Bug: b/536094756
    Change-Id: I8061a9688f1ad49712e0dcf5430704432a543ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8409248
    Commit-Queue: Amirali Abdolrashidi <abdolrashidi@google.com>
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>

--- src/libANGLE/renderer/vulkan/vk_mem_alloc_wrapper.cpp
+++ src/libANGLE/renderer/vulkan/vk_mem_alloc_wrapper.cpp
@@ -51,12 +51,10 @@ VkResult InitAllocator(VkPhysicalDevice physicalDevice,
         funcs.vkBindImageMemory2KHR                   = vkBindImageMemory2;
         funcs.vkGetPhysicalDeviceMemoryProperties2KHR = vkGetPhysicalDeviceMemoryProperties2;
 
-        // vkGetPhysicalDeviceProperties2KHR is introduced in VMA 3.4.  VMA uses Vulkan's versioning
-        // scheme (VK_MAKE_API_VERSION) and sets major and minor versions.  VK_MAKE_API_VERSION
-        // cannot directly be used in the preprocessor |#if| check because it contains casts to
-        // |uint32_t|.  VK_MAKE_API_VERSION shifts the major number by 22 and the minor number by
-        // 12.
-#if defined(VMA_VERSION) && VMA_VERSION >= (3 << 22 | 4 << 12)
+        // vkGetPhysicalDeviceProperties2KHR was introduced in VMA 3.4 and is guarded by
+        // VMA_GET_PHYSICAL_DEVICE_PROPERTIES2. VMA_VERSION cannot be used in a preprocessor #if
+        // check because VK_MAKE_API_VERSION contains (uint32_t) casts.
+#if VMA_GET_PHYSICAL_DEVICE_PROPERTIES2
         funcs.vkGetPhysicalDeviceProperties2KHR = vkGetPhysicalDeviceProperties2;
 #endif
     }
