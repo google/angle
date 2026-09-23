7710c268735628636d1eca84b639ad6391ebe94b
Amirali Abdolrashidi <abdolrashidi@google.com>
2026-09-23 13:20:10 -0700

    Vulkan: Replace deprecated syncval layer setting
      Recent tests show the message that the following layer setting has
    been deprecated: `syncval_submit_time_validation` (even if disabled)
    The message also suggests using `syncval_full_validation` instead.
    
    * Updated layerSettings during renderer initialization to replace
      `syncval_submit_time_validation` with `syncval_full_validation`.
    
    * Removed outdated comment regarding the behavior setting_validate_sync
      with respect to VkEvents.
    
    Bug: b/565449106
    Change-Id: I74fae4ab481ab71660608cb603ea50d6442e85e4
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8451288
    Commit-Queue: Amirali Abdolrashidi <abdolrashidi@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Charlie Lao <cclao@google.com>

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -2628,24 +2628,21 @@ angle::Result Renderer::initialize(vk::ErrorContext *context,
     // Fine grain control of validation layer features
     const char *name                     = "VK_LAYER_KHRONOS_validation";
     const VkBool32 setting_validate_core = VK_TRUE;
-    // SyncVal is very slow (https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/7285)
-    // for VkEvent which causes a few tests fail on the bots. Disable syncVal if VkEvent is enabled
-    // for now.
     const VkBool32 setting_validate_sync = IsAndroid() ? VK_FALSE : VK_TRUE;
     const VkBool32 setting_thread_safety = VK_TRUE;
     // http://anglebug.com/42265520 - Shader validation caching is broken on Android
     const VkBool32 setting_check_shaders = IsAndroid() ? VK_FALSE : VK_TRUE;
     // http://b/316013423 Disable QueueSubmit Synchronization Validation. Lots of failures and some
     // test timeout due to https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/7285
-    const VkBool32 setting_syncval_submit_time_validation   = VK_FALSE;
+    const VkBool32 setting_syncval_full_validation          = VK_FALSE;
     const VkBool32 setting_syncval_message_extra_properties = VK_TRUE;
     const VkLayerSettingEXT layerSettings[]                 = {
         {name, "validate_core", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &setting_validate_core},
         {name, "validate_sync", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &setting_validate_sync},
         {name, "thread_safety", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &setting_thread_safety},
         {name, "check_shaders", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1, &setting_check_shaders},
-        {name, "syncval_submit_time_validation", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1,
-         &setting_syncval_submit_time_validation},
+        {name, "syncval_full_validation", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1,
+         &setting_syncval_full_validation},
         {name, "syncval_message_extra_properties", VK_LAYER_SETTING_TYPE_BOOL32_EXT, 1,
          &setting_syncval_message_extra_properties},
     };
