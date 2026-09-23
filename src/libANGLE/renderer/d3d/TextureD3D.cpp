6c47c4b6dae794245a23a16b3826efc09ebdefd0
Quyen Le <lehoangquyen@google.com>
2026-09-22 23:18:57 -0700

    Revert "m152: D3D11: fixing robust init for self copy same mip lvl"
    This reverts commit e7f6ee33d17574a2ac344e2b74daa3dd6e5c7ec3.
    
    Reason for revert: break windows builds
    
    Failure Link: https://ci.chromium.org/ui/p/chromium-m152/builders/try/win-libfuzzer-asan-rel/2715/overview
    
    Original change's description:
    > m152: D3D11: fixing robust init for self copy same mip lvl
    >
    > When performing a glCopyTexImage2D self-copy on the same mip level
    > with a source rectangle fully out of bounds of the framebuffer,
    > handleCopyImageSelfCopyRedefine checked ClipRectangle and returned
    > early after redefining the destination texture.
    >
    > This early return bypassed the call to initializeContents(), leaving
    > the newly resized destination texture uninitialized with stale or
    > undefined contents while the front-end marked the image initialized.
    >
    > This patch calls initializeContents() prior to the early return when
    > robust resource initialization or WebGL is enabled, ensuring that
    > fully out-of-bounds self-copies are properly zero-initialized. Also
    > removes the test skip expectation for the
    > Texture2DTestES3RobustInit.CopyTexImageOutOfBoundsSelf test on
    > Win D3D11.
    >
    > Bug: chromium:556789073
    > Fixed: chromium:561874521
    > Change-Id: I03b5f6c6f7fc15dfc4696808b35f9d5f8ed44195
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8387697
    > Auto-Submit: Quyen Le <lehoangquyen@google.com>
    > Commit-Queue: Quyen Le <lehoangquyen@google.com>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > (cherry picked from commit c2b3395143b5b0ae3f74bd120ad5e7f1fbd9318a)
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8439858
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    
    Bug: chromium:556789073
    Bug: chromium:561874521
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia59c581f9bf1bc5a894a76b0fd7ac7bec35e9abf
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/8450170
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>

--- src/libANGLE/renderer/d3d/TextureD3D.cpp
+++ src/libANGLE/renderer/d3d/TextureD3D.cpp
@@ -134,10 +134,6 @@ angle::Result TextureD3D::handleCopyImageSelfCopyRedefine(
     {
         // We won't be copying, but redefine the destination texture in case sourceArea is larger
         ANGLE_TRY(redefineDest(destExtents));
-        if (context->isWebGL() || context->isRobustResourceInitEnabled())
-        {
-            ANGLE_TRY(initializeContents(context, GL_NONE, destIndex));
-        }
         return angle::Result::Continue;
     }
 
